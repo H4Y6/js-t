d114edcccb5874c1af3fd8bbb04d071ce375ad39
h4y6 <h4y6@i.ua>
2023-10-23 15:42:36 +0300

    modified:   js/05-comparison-operators.js, js/07-branching.js

--- js/07-branching.js
+++ js/07-branching.js
@@ -66,3 +66,26 @@ const salary = 5000;
 
 //   console.log(a);
 // }
+
+// let type;
+const age = 20;
+
+// if (age >= 18) {
+//     type = "adult";
+// } else {
+//     type = "child";
+// }
+const type = age >= 18 ? 'adult' : 'child';
+console.log(type);
+
+const num1 = 5;
+const num2 = 10;
+// let biggerNumber;
+// if (num1 > num2) { biggerNumber = num1 }
+// else { biggerNumber = num2 }
+
+const biggerNumber = num1 > num2 ? num1 : num2;
+console.log(biggerNumber)
+
+
+
