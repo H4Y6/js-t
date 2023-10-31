a624d31e0bd0519d1286e89f8452dae187eea143
h4y6 <h4y6@i.ua>
2023-10-31 20:35:19 +0200

    new file:   js-2-1/.gitignore new file:   js-2-1/.prettierrc.json new file:   js-2-1/index.html new file:   js-2-1/js/01-basics.js new file:   js-2-1/js/02-payment.js new file:   js-2-1/js/03-even.js new file:   js-2-1/js/04-find-login.js new file:   js-2-1/js/05-smallest-number.js new file:   js-2-1/js/06-transform.js new file:   js-2-1/js/07-change-register.js new file:   js-2-1/js/08-slug.js new file:   js-2-1/js/09-array-sum.js new file:   js-2-1/js/10-cards.js

---
+++ js-2-1/js/05-smallest-number.js
@@ -0,0 +1,26 @@
+/*
+ * Напиши скрипт поиска самого маленького числа в массиве,
+ * при условии что числа уникальные (не повторяются).
+ */
+
+const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let smallestNumber = numbers[0];
+
+// for (const number of numbers) {
+//   if (number < smallestNumber) {
+//     smallestNumber = number;
+//   }
+// }
+
+// console.log('smallestNumber: ', smallestNumber);
+
+let max = numbers[0];
+
+for (const number of numbers) {
+  max = number > max ? number : max;
+}
+console.log({ max });
+
+console.dir(Math);
+
+console.log({ max: Math.max(...numbers) });
