59b02e3aa7676b708fa0478e9ef236db667a147e
h4y6 <h4y6@i.ua>
2023-11-05 18:45:08 +0200

    modified:   js-2-2/js/05-find-smallest-number.js modified:   js-2-2/js/06-change-case.js

--- js-2-2/js/05-find-smallest-number.js
+++ js-2-2/js/05-find-smallest-number.js
@@ -3,18 +3,27 @@
  * при условии что числа уникальные (не повторяются).
  */
 
-const findSmallesNumber = function (numbers) {
-  let smallestNumber = numbers[0];
+// const findSmallesNumber = function (numbers) {
+//   let smallestNumber = numbers[0];
 
-  for (const number of numbers) {
-    if (number < smallestNumber) {
-      smallestNumber = number;
-    }
+//   for (const number of numbers) {
+//     if (number < smallestNumber) {
+//       smallestNumber = number;
+//     }
+//   }
+
+//   return smallestNumber;
+// };
+
+const findSmallestNumber = function (args) {
+  let smallestNumber = 1 / 0;
+  // let smallestNumber = args[1];
+  for (const number of args) {
+    smallestNumber = number < smallestNumber ? number : smallestNumber;
   }
-
   return smallestNumber;
-};
+}
 
-console.log(findSmallesNumber([3, 8, 12, -2, 15])); // -2
-console.log(findSmallesNumber([100, 54, 8, 12, 47])); // 8
-console.log(findSmallesNumber([7, 21, 84, 15, 4])); // 4
+console.log(findSmallestNumber([3, 8, 12, -2, 15])); // -2
+console.log(findSmallestNumber([100, 54, 8, 12, 47])); // 8
+console.log(findSmallestNumber([7, 21, 84, 15, 4])); // 4
--- js-2-2/js/06-change-case.js
+++ js-2-2/js/06-change-case.js
@@ -4,21 +4,46 @@
  * Например, если строка «JavaScript», то на выходе должена быть строка «jAVAsCRIPT».
  */
 
-const changeCase = function (string) {
-  const letters = string.split('');
-  let invertedString = '';
+// const changeCase = function (string) {
+//   const letters = string.split('');
+//   let invertedString = '';
 
+//   for (const letter of letters) {
+//     const isInLowerCase = letter === letter.toLowerCase();
+
+//     invertedString += isInLowerCase
+//       ? letter.toUpperCase()
+//       : letter.toLowerCase();
+//   }
+
+//   return invertedString;
+// };
+
+// const invertString = function (string) {
+//   let invertedString = "";
+//   const letters = string.split("");
+//   for (const letter of letters) {
+//     if (letter === letter.toLowerCase()) {
+//       invertedString += letter.toUpperCase();
+//     } else {
+//       invertedString += letter.toLowerCase();
+//     }
+//   }
+//   return invertedString;
+// }
+
+
+
+const invertString = function (string) {
+  let invertedString = "";
+  const letters = string.split("");
   for (const letter of letters) {
-    const isInLowerCase = letter === letter.toLowerCase();
-
-    invertedString += isInLowerCase
-      ? letter.toUpperCase()
-      : letter.toLowerCase();
+    invertedString += letter === letter.toLowerCase() ? letter.toUpperCase() : letter.toLowerCase()
   }
-
   return invertedString;
-};
+}
 
-console.log(changeCase('qweRTY')); // QWErty
-console.log(changeCase('mAnGo')); // MaNgO
-console.log(changeCase('AjAx')); // aJaX
+console.log(invertString('qweRTY')); // QWErty
+console.log(invertString('mAnGo')); // MaNgO
+console.log(invertString('AjAx')); // aJaX
+
