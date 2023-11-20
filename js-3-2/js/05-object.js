90af956f0fedf0dea5552f71bda3cdf1fc36f9b0
h4y6 <h4y6@i.ua>
2023-11-20 22:58:34 +0200

    modified:   js-3-2/index.html new file:   js-3-2/js/05-object.js

---
+++ js-3-2/js/05-object.js
@@ -0,0 +1,25 @@
+{
+    const createUser = function (name, age) {
+        // return { name: name, age: age };
+        const eyeColor = 'green';
+
+        return { name, age, eyeColor };
+    }
+
+    const user = createUser('Smith', 33);
+    // console.log(user);
+}
+
+{
+    const user = { name: 'Peter', age: 32 };
+
+    const addProps = function ({ ...obj }, key, value) {
+        obj[key] = value;
+
+        return obj;
+    }
+    const developer = addProps(user, 'salary', 2222);
+    // const developer = addProps(user, 'rating', 4.2);
+
+    console.log(user, developer);
+}
