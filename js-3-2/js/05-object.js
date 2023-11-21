bca0fe0e3d93b71e33a6e68501d9f37669317966
h4y6 <h4y6@i.ua>
2023-11-21 19:12:05 +0200

    modified:   js-3-2/js/05-object.js

--- js-3-2/js/05-object.js
+++ js-3-2/js/05-object.js
@@ -21,19 +21,61 @@
     // const developer = addProps(user, 'salary', 2222);
     const developer = addProps(user, 'rating', 4.2);
 
-    console.log(user, developer);
+    // console.log(user);
+    // console.log(developer);
 }
 {
     const user = { name: 'Peter', age: 32 };
 
     const addProps = function (obj, key, value) {
-        obj
 
         return { ...obj, [key]: value };
     }
     let developer = addProps(user, 'salary', 2222);
-    console.log(user, developer);
+    // console.log(developer);
 
-    developer = addProps(user, 'rating', 4.2);
-    console.log(user, developer);
+    // developer = addProps(user, 'rating', 4.2);
+    // console.log(developer);
+    // console.log(user);
+}
+
+{
+    const employeeSam = { name: 'Sam', salary: 4300 };
+    const employeeTom = { name: 'Tom', salary: 2700 };
+
+    const company = {
+        employees: [],
+        totalSalaryFund: 7000,
+
+        addEmployee(employee) {
+            this.employees.push(employee);
+        },
+
+        estimateTotalSalary() {
+            let totalSalary = 0;
+
+            for (const employee of this.employees) {
+                totalSalary += employee.salary;
+            }
+
+            return totalSalary;
+        },
+
+        // calculateAllSalary() {
+        //     let sum = 0;
+
+        //     for (const employee of this.employees) {
+        //         sum += employee.salary;
+        //     }
+
+        //     return sum;
+        // },
+    };
+
+    company.addEmployee(employeeSam);
+    company.addEmployee(employeeTom);
+    console.log(company.employees);
+
+    console.log(company.estimateTotalSalary())
+    // console.log(company.calculateAllSalary())
 }
