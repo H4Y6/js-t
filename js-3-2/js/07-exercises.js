146de2fec680e6f557589a000ec6d4348385831a
h4y6 <h4y6@i.ua>
2023-11-29 18:18:18 +0200

    modified:   js-3-2/js/07-exercises.js

--- js-3-2/js/07-exercises.js
+++ js-3-2/js/07-exercises.js
@@ -1,11 +1,28 @@
-const copyInstance = function (instance) {
-    if (Array.isArray(instance)) {
-        return [...instance];
-    }
+{
+    const copyInstance = function (instance) {
+        if (Array.isArray(instance)) {
+            return [...instance];
+        }
 
-    return { ...instance };
-};
+        return { ...instance };
+    };
 
-const array1 = [1, 4, 6, 5]
-const newArray = copyInstance(array1);
-console.log(`Are they equal? ->  ${newArray === array1}`);
+    const array1 = [1, 4, 6, 5]
+    const newArray = copyInstance(array1);
+    // console.log(`Are they equal? ->  ${newArray === array1}`);
+}
+
+{
+    // Поверхнева копія масиву:
+    const users = [
+        { name: 'Pit' },
+        { name: 'Tit' },
+    ];
+
+    const newUsers = [...users];
+    newUsers[0].name = 'Sam';
+
+    console.log(users === newUsers)
+    console.log(users, '---users')
+    console.log(newUsers, '---newUsers')
+}
