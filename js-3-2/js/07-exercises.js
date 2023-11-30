da31e3e7b4829476932272b995c6291eb5e0d12f
h4y6 <h4y6@i.ua>
2023-11-30 19:19:05 +0200

    modified:   js-3-2/js/07-exercises.js

--- js-3-2/js/07-exercises.js
+++ js-3-2/js/07-exercises.js
@@ -78,6 +78,106 @@
 
     const mathFunctionResult = mathOperation(sign, 25, -40, 20);
     // const result = mathOperation(sign);
-    console.log(mathFunctionResult);
+    // console.log(mathFunctionResult);
 }
 
+{
+    const DEFAULT_USER_AVATAR = 'https://pict/avatar.jpg';
+    const DEFAULT_LOCALE = 'en_GB';
+
+    const generalData = {
+        user: {
+            name: 'Sam',
+            avatar: 'https://img/avatar.png',
+            age: 25,
+        },
+        locale: 'en_US',
+        cart: [
+            { productName: 'cup', price: 22 },
+            { productName: 'iron', price: 25 },
+            { productName: 'plate', price: 12 },
+        ],
+    };
+
+    const { user: { avatar = DEFAULT_USER_AVATAR, name: userName }, locale = DEFAULT_LOCALE, cart, } = generalData;
+    // const { avatar = DEFAULT_USER_AVATAR } = user;
+
+    // console.log(userName, ',', avatar);
+    // console.log(cart);
+    // console.log(locale);
+}
+
+{
+    const users = [
+        {
+            name: 'Pit',
+            age: 43,
+        },
+        {
+            name: 'Lee',
+            age: 18,
+        },
+        {
+            name: 'Anrew',
+            age: 29,
+        },
+
+    ]
+
+    const findYoungestUser = function (...args) {
+        if (args.length === 0) {
+            console.error('No user found');
+            return 'Error!';
+        }
+
+        let youngestUser = args[0];
+        let { age: youngestUserAge } = youngestUser;
+
+
+        for (const { name, age } of args) {
+            if (age > youngestUserAge) {
+                // if (age > youngestUser.age) {
+                continue;
+            }
+            youngestUser = { name, age };
+        }
+        // for (const user of args) {
+        //     if (user.age > youngestUser.age) {
+        //         continue;
+        //     }
+        //     youngestUser = user;
+        // }
+        return youngestUser;
+    }
+
+    const findOldestUser = function (...args) {
+        if (args.length === 0) {
+            console.error('No user found');
+            return 'Error!'
+        }
+
+        let oldestUser = args[0];
+        let { age: oldestUserAge } = oldestUser;
+
+        for (const { name, age } of args) {
+            if (age < oldestUserAge) {
+                // if (age < oldestUser.age) {
+                continue;
+            }
+            oldestUser = { name, age };
+        }
+        // for (const user of args) {
+        //     if (user.age < oldestUser.age) {
+        //         continue;
+        //     }
+        //     oldestUser = user;
+        // }
+        return oldestUser;
+    }
+
+    const youngestUser = findYoungestUser(...users);
+    const oldestUser = findOldestUser(...users);
+
+    console.log(youngestUser);
+    console.log(oldestUser);
+}
