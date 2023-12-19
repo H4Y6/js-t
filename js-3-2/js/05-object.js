7c8ca11cb0ba04af2cef8f19c4e5f60ce6a6d796
h4y6 <h4y6@i.ua>
2023-12-19 16:54:23 +0200

    modified:   js-3-2/index.html modified:   js-3-2/js/05-object.js

--- js-3-2/js/05-object.js
+++ js-3-2/js/05-object.js
@@ -282,3 +282,46 @@
     // atTheOldToad.updatePotionName("Dragon breath", "Polymorth")
     // console.log(atTheOldToad.getPotions());
 }
+
+{
+    const atTheOldToad = {
+        potions: [
+            { name: "Speed potion", price: 460 },
+            { name: "Dragon breath", price: 780 },
+            { name: "Stone skin", price: 520 },
+        ],
+        getPotions() {
+            return this.potions;
+        },
+        addPotion(newPotion) {
+            for ({ name } of this.potions) {
+                if (name === newPotion.name) {
+                    return `Error! Potion ${newPotion.name} is already in your inventory!`;
+                }
+                continue;
+            }
+
+            this.potions.push(newPotion);
+        },
+        removePotion(potionName) {
+            const potionIndex = this.potions.indexOf(potionName);
+
+            if (potionIndex === -1) {
+                return `Potion ${potionName} is not in inventory!`;
+            }
+
+            this.potions.splice(potionIndex, 1);
+        },
+        updatePotionName(oldName, newName) {
+            const potionIndex = this.potions.indexOf(oldName);
+
+            if (potionIndex === -1) {
+                return `Potion ${oldName} is not in inventory!`;
+            }
+
+            this.potions.splice(potionIndex, 1, newName);
+        },
+    };
+    console.log(atTheOldToad.addPotion({ name: "Dragon eye", price: 700 }))
+    console.log(atTheOldToad.getPotions())
+}
