16331fa7082c9d98dbb3a1c9dd527f6180b641c4
h4y6 <h4y6@i.ua>
2023-12-26 17:56:17 +0200

    modified:   js-4-1/js/03-arrow-fns.js

--- js-4-1/js/03-arrow-fns.js
+++ js-4-1/js/03-arrow-fns.js
@@ -86,18 +86,28 @@ const r3 = filter(fruits, fruit => fruit.quantity >= 120);
 console.log(r3);
 
 {
-    const setFiltreRule = (value, limitedValue) => value < limitedValue;
+    // const setFiltreRule = (value, limitedValue) => value <= limitedValue;
 
-    const filtre = (array, number, cb) => {
+    // const filtre = (array, number, cb) => {
+    //     const filtredArray = [];
+    //     for (const el of array) {
+    //         if (cb(el, number)) {
+    //             filtredArray.push(el);
+    //         }
+    //     }
+    //     return filtredArray;
+    // };
+
+    const filtre = (array, cb) => {
         const filtredArray = [];
         for (const el of array) {
-            if (cb(el, number)) {
+            if (cb(el)) {
                 filtredArray.push(el);
             }
         }
         return filtredArray;
-    }
+    };
 
-
-    console.log(filtre([3, 5, 2, 7, 8, 4], 4, setFiltreRule));
+    // console.log(filtre([3, 5, 2, 7, 8, 4], 4, setFiltreRule));
+    console.log(filtre([3, 5, 2, 7, 8, 4], item => item <= 5));
 }
