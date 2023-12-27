42273b885894d00d80f1fd7e1fab7537ef30b2a4
h4y6 <h4y6@i.ua>
2023-12-27 14:11:15 +0200

    modified:   js-4-1/js/04-light-tests.js

--- js-4-1/js/04-light-tests.js
+++ js-4-1/js/04-light-tests.js
@@ -10,16 +10,26 @@
         },
     };
     function makePizza(pizzaName) {
+        console.log(`Your order is accepted. Cooking pizza ${pizzaName}.`)
         return `Your order is accepted. Cooking pizza ${pizzaName}.`;
     }
 
     function onOrderError(pizzaName) {
+        console.log(`Error! There is no pizza with a name ${pizzaName} in the assortment.`)
         return `Error! There is no pizza with a name ${pizzaName} in the assortment.`;
     }
+    // pizzaPalace.order('Smoked', makePizza, onOrderError);
+    // pizzaPalace.order('Four meats', makePizza, onOrderError);
+    // pizzaPalace.order('Big Mike', makePizza, onOrderError);
+    // pizzaPalace.order('Vienna', makePizza, onOrderError);
+}
 
-    console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
-    console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
-    console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
-    console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+{
+    function calculateTotalPrice(orderedItems) {
+        let totalPrice = 0;
+        orderedItems.forEach(function (item) { totalPrice += item });
+        // orderedItems.forEach( item => totalPrice += item);
 
+        return totalPrice;
+    }
 }
