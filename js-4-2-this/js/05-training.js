c2e562464de791fb34e88b279313fbed7fd434ad
h4y6 <h4y6@i.ua>
2024-01-13 19:57:25 +0200

    modified:   js-4-2-this/index.html 	new file:   js-4-2-this/js/05-training.js

---
+++ js-4-2-this/js/05-training.js
@@ -0,0 +1,11 @@
+function printWordToScreen() {
+    const somewords = "This is an example."
+    console.log(somewords)
+};
+printWordToScreen();
+
+function helloWorld() {
+    const str = "Hello World!";
+    console.log(str);
+};
+helloWorld();
