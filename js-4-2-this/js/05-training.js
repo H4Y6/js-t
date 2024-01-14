2c30edefe11f9d34d829743665b172e231b5a109
h4y6 <h4y6@i.ua>
2024-01-14 23:38:22 +0200

    modified:   js-4-2-this/js/05-training.js

--- js-4-2-this/js/05-training.js
+++ js-4-2-this/js/05-training.js
@@ -2,13 +2,13 @@ function printWordToScreen() {
     const somewords = "This is an example."
     console.log(somewords)
 };
-printWordToScreen();
+// printWordToScreen();
 
 function helloWorld() {
     const str = "Hello World!";
     console.log(str);
 };
-helloWorld();
+// helloWorld();
 
 /**
 Write function bmi that calculates body mass index(bmi = weight / height2).
@@ -26,7 +26,7 @@ function bmi(weight, height) {
     if (bmi <= 30.0) return "Overweight";
     else return "Obese";
 };
-console.log(bmi(61.2, 1.8));
+// console.log(bmi(61.2, 1.8));
 
 /**  Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
  Input will always be valid, i.e. no negative integers. */
@@ -39,7 +39,7 @@ const countSheep = function (num) {
     return message;
 };
 
-console.log(countSheep(3));
+// console.log(countSheep(3));
 
 /**  Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
  Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and
@@ -53,7 +53,7 @@ const summation = function (num) {
     };
     return sum;
 };
-console.log(summation(5));
+// console.log(summation(5));
 
 /**  Can you find the needle in the haystack?
 Write a function findNeedle() that takes an array full of junk but containing one "needle"
@@ -64,7 +64,7 @@ function findNeedle(haystack) {
     const idx = haystack.indexOf('needle');
     return idx === -1 ? 'nothing`s found' : `found the needle at position ${idx}`;
 };
-console.log(findNeedle(['123124234', undefined, 'world', 'hay', 2, '3', true, 'needle', false]));
+// console.log(findNeedle(['123124234', undefined, 'world', 'hay', 2, '3', true, 'needle', false]));
 
 /**  Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times. */
 
@@ -76,19 +76,32 @@ function repeatStr(n, s) {
     };
     return result;
 };
-console.log(repeatStr(4, 'ter'));
+// console.log(repeatStr(4, 'ter'));
 
 /**   Write a function that removes the spaces from the string, then return the resultant string.   */
 
 function noSpace(x) {
     return x.split(' ').join('');
 };
-console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+// console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
 
 /**  This kata is about multiplying a given number by eight if it is an even number and by nine otherwise. */
 
 function simpleMultiplication(number) {
     return number % 2 === 0 ? number * 8 : number * 9;
 };
-console.log(simpleMultiplication(2));
-console.log(simpleMultiplication(3));
+// console.log(simpleMultiplication(2));
+// console.log(simpleMultiplication(3));
+
+/**  Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development 
+and functioning of living organisms.If you want to know more: http://en.wikipedia.org/wiki/DNA
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell);
+ you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+Example: (input --> output) */
+
+function dnaStrand(dna) {
+    return dna.replaceAll('T', 't').replaceAll('A', 'T').replaceAll('t', 'A')
+        .replaceAll('G', 'g').replaceAll('C', 'G').replaceAll('g', 'C');
+};
+console.log(dnaStrand("GTAT"));
