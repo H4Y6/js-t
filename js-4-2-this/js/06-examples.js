cf398abbef1b13101bc746e7c15e1d71984e67bb
h4y6 <h4y6@i.ua>
2024-01-19 18:16:59 +0200

    modified:   js-4-2-this/index.html modified:   js-4-2-this/js/06-examples.js

--- js-4-2-this/js/06-examples.js
+++ js-4-2-this/js/06-examples.js
@@ -18,15 +18,56 @@ const arr = [3, 7, 44, 25, 87, 2];
 
 const maxVal = Math.max.apply({}, arr);
 const maxValue = Math.max(...arr);
-const maxNum = Math.max.call({}, 2, 5, 7, 33, 61, 24, 47, 1);
+// const maxNum = Math.max.call({}, 2, 5, 7, 33, 61, 24, 47, 1);
 const maxNumber = Math.max(2, 5, 7, 33, 61, 24, 47, 1);
 
-// console.log(maxVal);
-// console.log(maxValue);
+console.log(maxVal);
+console.log(maxValue);
 // console.log(maxNum);
-// console.log(maxNumber);
+console.log(maxNumber);
 
-const admin = { name: 'Michaele' };
+const admin = { name: 'Michael', age: 33, };
 const fnA = function () { console.log(this.name) };
-const fnB = fnA.bind(admin);
-fnB();
+const koo = fnA.bind(admin);
+koo();
+
+{
+    const customer = {
+        firstName: 'Piter',
+        lastName: 'Colt',
+        getFullName() {
+            return `${this.firstName} ${this.lastName}`;
+        },
+    };
+
+    function makeMessage(cb) {
+        console.log(`${cb}! Your order will be ready in 5 minutes.`)
+    };
+
+    const foo = function () {
+        return `${this.firstName} ${this.lastName}`;
+    };
+
+    // makeMessage(customer.getFullName());
+
+    // makeMessage(customer.getFullName.bind(customer));
+    // makeMessage(foo.bind(customer));
+}
+
+{
+    const a = 7;
+
+    const logA = function () {
+        console.log('a:', a)
+        console.log(this)
+        console.log('this.a:', this.a)
+    };
+
+    const foo = () => {
+        const a = 11;
+        logA();
+    };
+
+    // logA();
+    // foo();
+}
