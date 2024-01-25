9e440e7298ff3f98660c7a9a6acc338a4068bcae
h4y6 <h4y6@i.ua>
2024-01-25 17:51:59 +0200

    modified:   tr-code-js/js/01-training.js

--- tr-code-js/js/01-training.js
+++ tr-code-js/js/01-training.js
@@ -286,5 +286,91 @@ String.prototype.toJadenCase = function () {
 
 const str = new String();
 
-console.log(str.toJadenCase());
+// console.log(str.toJadenCase());
 // console.log(String.prototype.toJadenCase());
+
+const atTheOldToad = {
+    potions: [
+        { name: "Speed potion", price: 460 },
+        { name: "Dragon breath", price: 780 },
+        { name: "Stone skin", price: 520 },
+    ],
+    getPotions() {
+        return this.potions;
+    },
+    addPotion(newPotion) {
+        for (const { name } of this.potions) {
+            if (name === newPotion.name) {
+                return `Error! Potion ${newPotion.name} is already in your inventory!`;
+            }
+        }
+        this.potions.push(newPotion);
+    },
+    removePotion(potionName) {
+        const potionIndex = this.potions.findIndex(p => p.name === potionName);
+        if (potionIndex === -1) {
+            return `Potion ${potionName} is not in inventory!`;
+        }
+        this.potions.splice(potionIndex, 1);
+    },
+    updatePotionName(oldName, newName) {
+        const potionIndex = this.potions.findIndex(p => p.name === oldName);
+        if (potionIndex === -1) {
+            return `Potion ${oldName} is not in inventory!`;
+        }
+        this.potions[potionIndex].name = newName;
+    },
+};
+
+// console.log(atTheOldToad.getPotions());
+// console.log(atTheOldToad.addPotion({ name: "Invisibility", price: 620 }));
+// console.log(atTheOldToad.removePotion("Dragon breath"));
+// console.log(atTheOldToad.updatePotionName("Dragon breath", "Idast"));
+
+function processCall(recipient, onAvailable, onNotAvailable) {
+    const isRecipientAvailable = Math.random() > 0.5;
+    if (!isRecipientAvailable) {
+        onNotAvailable(recipient);
+        return;
+    }
+    onAvailable(recipient);
+}
+
+function takeCall(name) {
+    console.log(`Соединяем с ${name}, ожидайте...`);
+}
+function activateAnsweringMachine(name) {
+    console.log(`Абонент ${name} недоступен, оставьте сообщение.`
+    );
+}
+
+function leaveHoloMessage(name) {
+    console.log(`Абонент ${name} недоступен, записываем голограмму.`
+    );
+}
+// processCall("Mango", takeCall, activateAnsweringMachine);
+// processCall("Poly", takeCall, leaveHoloMessage);
+
+const pizzaPalace = {
+    pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+    order(pizzaName, onSuccess, onError) {
+        if (!this.pizzas.includes(pizzaName)) {
+            return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+        };
+        return onSuccess(pizzaName);
+    },
+};
+
+function makePizza(pizzaName) {
+    return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+function onOrderError(error) {
+    return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+// console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
