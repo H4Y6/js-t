6c86583d1c54eddb7653beffcb3ff2c7698f0861
h4y6 <h4y6@i.ua>
2024-02-01 21:44:27 +0200

    modified:   js-5-proto&Object.create.js/js/2-constructors.js

--- js-5-proto&Object.create.js/js/2-constructors.js
+++ js-5-proto&Object.create.js/js/2-constructors.js
@@ -107,35 +107,124 @@
 const objOne = {};
 // console.log(objOne.__proto__ === Object.prototype, objOne);
 
+{
+  const Car = function ({ brand, model, price } = []) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
 
-const Car = function ({ brand, model, price } = []) {
-  this.brand = brand;
-  this.model = model;
-  this.price = price;
+    this.changePrice = function (newPrice) {
+      this.price = newPrice;
+    };
+  };
+  // console.log(Car.prototype);
+  Car.prototype.sayHello = function () {
+    console.log('Hello!');
+    console.log('Car.prototype.sayHello -> this:', this);
+  };
+  // Car.prototype.sayHello();
 
-  this.changePrice = function (newPrice) {
+  const myCar = new Car({
+    brand: 'Audi',
+    model: 'Q7',
+    price: 32000,
+  });
+  // console.log(myCar);
+
+  const myNewCar = new Car({
+    brand: 'Audi',
+    model: 'M1',
+    price: 25000,
+  });
+  // console.log(myNewCar);
+
+  const myNewestCar = new Car({
+    brand: 'VW',
+    model: 'D4',
+    price: 21000,
+  });
+
+  // console.log(myNewestCar);
+}
+
+{
+  const Bicycle = function ({ brand, model, price } = {}) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+
+  };
+
+  Bicycle.prototype.sayOk = function () {
+    // console.log('This.prototype.sayOk -> this:', this);
+    // console.log('Ok! 👍');
+  };
+
+  Bicycle.prototype.changePrice = function (newPrice) {
     this.price = newPrice;
   };
-};
 
-const myCar = new Car({
-  brand: 'Audi',
-  model: 'Q7',
-  price: 32000,
-});
-console.log(myCar);
+  // console.log(Bicycle.prototype);
 
-const myNewCar = new Car({
-  brand: 'Audi',
-  model: 'M1',
-  price: 25000,
-});
-console.log(myNewCar);
+  const myBycicle = new Bicycle({ brand: 'Mountain', model: 'A1', price: 122 });
+  // console.log(myBycicle);
+  // myBycicle.sayOk();
 
-const myNewestCar = new Car({
-  brand: 'VW',
-  model: 'D4',
-  price: 21000,
-});
+  const bicycleA = new Bicycle({ brand: 'Trackker', model: 'B1', price: 142 });
+  bicycleA.sayOk();
+  // console.log(bicycleA);
 
-console.log(myNewestCar);
+  const bicycleB = new Bicycle();
+  bicycleB.changePrice(221);
+  // console.log(bicycleB);
+}
+
+{
+  const String = function () { { } };
+  String.prototype.toJadenCase = function (s = "How can mirrors be real if our eyes aren't real") {
+    let result = '';
+    this.s = s;
+    this.s.split(' ').forEach(s => [(result += ' ' + s.replace(/^./, s[0].toUpperCase()))]);
+    return result.trim();
+  };
+  const str = new String();
+  // console.log(str.toJadenCase("How can mirrors be real if our eyes aren't real"));
+}
+
+{
+  const User = function ({ email, password } = {}) {
+    this.email = email;
+    this.password = password;
+  };
+
+  User.prototype.changeEmail = function (newEmail) {
+    this.email = newEmail;
+  };
+
+  const bingo = new User({ email: 'bingo@o.iy', password: 1234, });
+
+  bingo.changeEmail('bingo.new@ertop.iy');
+
+  console.log(bingo);
+
+  /**      Static properties       */
+
+  User.message = "It's a static property.";
+  console.log(User.message);
+
+  User.logInfo = function (obj) {
+    console.log("User.logInfo -> obj:", obj);
+    console.log("Email: ", obj.email);
+    console.log("Password: ", obj.password);
+  };
+
+  User.logInfo(bingo);
+
+}
+
+{
+  const objZ = { s: 2 };
+
+  console.log(objZ);
+  console.log(objZ.__proto__ === Object.prototype);
+}
