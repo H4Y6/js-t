b09eeffddc80595a8d95826a61c79a368fc34943
h4y6 <h4y6@i.ua>
2024-02-09 19:46:10 +0200

    modified:   tr-code-js/js/01-training.js

--- tr-code-js/js/01-training.js
+++ tr-code-js/js/01-training.js
@@ -445,11 +445,5 @@ console.log(squareSum([1, 2, 2]));
     }
 
     const builder = new StringBuilder(".");
-    console.log(builder.getValue()); // "."
-    builder.padStart("^");
-    console.log(builder.getValue()); // "^."
-    builder.padEnd("^");
-    console.log(builder.getValue()); // "^.^"
-    builder.padBoth("=");
-    console.log(builder.getValue()); // "=^.^="
+    // console.log(builder);
 }
