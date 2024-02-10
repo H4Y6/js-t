cf865f199d3cc5dbb5003a801b160b6689a56758
h4y6 <h4y6@i.ua>
2024-02-10 22:49:20 +0200

    modified:   tr-code-js/js/01-training.js

--- tr-code-js/js/01-training.js
+++ tr-code-js/js/01-training.js
@@ -548,6 +548,26 @@ function squareSum(numbers) {
 }
 
 {
+    class Car {
+        static #MAX_PRICE = 50000;
+        static checkPrice(price) {
+            return price > Car.#MAX_PRICE ? "Error! Price exceeds the maximum" : "Success! Price is within acceptable limits";
+        }
+
+        constructor({ price }) {
+            this.price = price;
+        }
+    }
+
+    const audi = new Car({ price: 36000 });
+    const bmw = new Car({ price: 64000 });
+
+    // console.log(Car.checkPrice(audi.price));    // "Success! Price is within acceptable limits"
+    // console.log(Car.checkPrice(bmw.price));     // "Error! Price exceeds the maximum"
+
+}
+
+{
     class Storage {
         #items;
 
