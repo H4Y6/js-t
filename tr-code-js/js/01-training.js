f36e72f7a43338e9877f11b1eb5346b7426a43c5
h4y6 <h4y6@i.ua>
2024-02-11 00:31:41 +0200

    modified:   tr-code-js/js/01-training.js

--- tr-code-js/js/01-training.js
+++ tr-code-js/js/01-training.js
@@ -599,25 +599,56 @@ function squareSum(numbers) {
 
 {
     class User {
+        // email;
+
         constructor(email) {
             this.email = email;
         }
 
-        get email() {
+        get _email() {
             return this.email;
         }
 
-        set email(newEmail) {
+        set _email(newEmail) {
             this.email = newEmail;
         }
     }
 
     class Admin extends User {
-        static AccessLevel = { BASIC: "basic", SUPERUSER: "superuser" };
 
-        constructor({ email, AccessLevel }) {
+        blacklistedEmails = [];
+
+
+        static AccessLevel = {
+            BASIC: "basic",
+            SUPERUSER: "superuser",
+        };
+
+        constructor({ email, accessLevel }) {
             super(email);
-            this.AccessLevel = AccessLevel;
+            this.accessLevel = accessLevel;
+        }
+
+        blacklist(email) {
+            this.blacklistedEmails.push(email);
+        }
+
+        isBlacklisted(email) {
+            return this.blacklistedEmails.includes(email);
         }
     }
+
+    const mango = new Admin({
+        email: "mango@mail.com",
+        accessLevel: Admin.AccessLevel.SUPERUSER,
+    });
+
+    console.log(mango.email); // "mango@mail.com"
+    console.log(mango.accessLevel); // "superuser"
+
+    mango.blacklist("poly@mail.com");
+    console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+    console.log(mango.isBlacklisted("mango@mail.com")); // false
+    console.log(mango.isBlacklisted("poly@mail.com")); // true
+
 }
