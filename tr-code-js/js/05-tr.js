0b4d2431802b7c393921b69c8090fcd330c624c6
h4y6 <h4y6@i.ua>
2024-02-11 22:58:16 +0200

    modified:   tr-code-js/js/05-tr.js

--- tr-code-js/js/05-tr.js
+++ tr-code-js/js/05-tr.js
@@ -8,18 +8,26 @@ const users = [
     { name: "Sheree Anthony", email: "sheree@kog.com", eyeColor: "brown", friends: ["Goldie Gentry", "Briana Decker"], isActive: true, balance: 2764, gender: "female" }
 ];
 
-const countBalance = (total, { balance }) => total + balance;
-const countTotalBalance = (array, cb, total) => array.reduce(cb, total);
+const getResult = (array, cb, total) => array.reduce(cb, total);
 
-console.log('total balance', countTotalBalance(users, countBalance, 0));
+const countBalance = (total, { balance }) => total + balance;
+console.log('total balance:', getResult(users, countBalance, 0));
+
+const countActive = (total, { isActive }) => total = isActive === true ? total + 1 : total;
+console.log('active quantity:', getResult(users, countActive, 0));
+
+const getActiveName = (string, el) => string = el.isActive === true ? string + el.name + '; ' : string;
+console.log('active names:', getResult(users, getActiveName, ''));
 
 const arrayReduce = (array, cb, initialValue) => {
     let acc = initialValue ?? array[0];
-    const idx = acc === undefined ? 1 : 0;
+    let i = acc === undefined ? 1 : 0;
 
-    for (let i = idx; i < array.length; i++) {
+    for (; i < array.length; i++) {
         acc = cb(acc, array[i], i, array);
     }
     return acc;
 };
 console.log('total balance >', arrayReduce(users, countBalance, 0));
+console.log('active quantity >', arrayReduce(users, countActive, 0));
+console.log('active names >', arrayReduce(users, getActiveName, 0));
