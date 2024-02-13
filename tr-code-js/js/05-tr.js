7d30a59fc5ba4b8265d2cd57f4250c09377b64ba
h4y6 <h4y6@i.ua>
2024-02-13 23:54:36 +0200

    modified:   tr-code-js/js/05-tr.js

--- tr-code-js/js/05-tr.js
+++ tr-code-js/js/05-tr.js
@@ -107,5 +107,24 @@ const arrayReduce = (array, cb, initialValue) => {
         }
     }
 
-    console.log(Kata.getVolumeOfCuboid(2, 3, 4));
+    // console.log(Kata.getVolumeOfCuboid(2, 3, 4));
+}
+
+{
+    // You have to write a function that accepts three parameters:
+
+    // cap is the amount of people the bus can hold excluding the driver.
+    // on is the number of people on the bus excluding the driver.
+    // wait is the number of people waiting to get on to the bus excluding the driver.
+    // If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+    function enough(cap, on, wait) {
+        return +(wait > cap - on && wait - cap + on);
+    }
+
+    // function enough(cap, on, wait) {
+    //     return on + wait - cap > 0 ? on + wait - cap : 0;
+    // }
+
+    console.log(enough(100, 0, 50));
 }
