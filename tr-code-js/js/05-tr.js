aba9f2eb01c7b2e5994a96b9c5df6897cb362c6e
h4y6 <h4y6@i.ua>
2024-02-15 19:53:23 +0200

    modified:   tr-code-js/js/05-tr.js

--- tr-code-js/js/05-tr.js
+++ tr-code-js/js/05-tr.js
@@ -232,20 +232,20 @@ const arrayReduce = (array, cb, initialValue) => {
         // }
         // return array.slice(-n).reduce((sum, el) => sum + el, 0);
 
-        const array = [];
-        let arrayLength = 0;
+        // const array = [];
+        // let arrayLength = 0;
 
-        for (let i = 1; i <= n; i++) {
-            arrayLength += i;
-        }
+        // for (let i = 1; i <= n; i++) {
+        //     arrayLength += i;
+        // }
 
-        for (let i = 0; i < arrayLength; i++) {
-            const element = i * 2 + 1;
-            array.push(element);
-        }
-        return array.slice(-n).reduce((sum, num) => sum + num, 0);
+        // for (let i = 0; i < arrayLength; i++) {
+        //     const element = i * 2 + 1;
+        //     array.push(element);
+        // }
+        // return array.slice(-n).reduce((sum, num) => sum + num, 0);
     }
-    console.log(rowSumOddNumbers(4));
+    // console.log(rowSumOddNumbers(4));
 }
 
 
@@ -270,5 +270,38 @@ n = 50 - 1 + 1 = 50
 
 const sum = array => array.reduce((acc, n) => acc + n, 0);
 
-console.log(sum([4, 5, 6, 7, 8, 9, 10, 11])); 4 * 15;
-console.log(4 * 15); 
+// console.log(sum([4, 5, 6, 7, 8, 9, 10, 11])); 4 * 15;
+// console.log(4 * 15); 
+
+{  /*
+    Grade book
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.          */
+
+    function getGrade(s1, s2, s3) {
+        const averageScore = (s1 + s2 + s3) / 3;
+
+        return averageScore < 60 ? 'F' : averageScore < 70 ? 'D' : averageScore < 80 ? 'C' : averageScore <= 90 ? 'B' : 'A';
+
+        // switch (true) {
+        //     case averageScore < 60:
+        //         return 'F';
+        //     case averageScore < 70:
+        //         return 'D';
+        //     case averageScore < 80:
+        //         return 'C';
+        //     case averageScore <= 90:
+        //         return 'B';
+
+        //     default: return 'A';
+        // }
+    }
+    // console.log(getGrade(92, 87, 94));
+}
