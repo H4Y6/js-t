26c250e700c961c7ead117a7a8477d0152736496
h4y6 <h4y6@i.ua>
2024-02-24 00:23:34 +0200

    modified:   tr-code-js/js/06-tr.js

--- tr-code-js/js/06-tr.js
+++ tr-code-js/js/06-tr.js
@@ -355,6 +355,54 @@ Return true if yes, false otherwise :)    */
 Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).   */
 
   function isValidWalk(walk) {
-    //insert brilliant code here
+    return walk.length === 10 && !walk.reduce((sum, d) => d === 'n' ? sum + 1 : d === 's' ? sum - 1 : sum, 0) && !walk.reduce((sum, d) => d === 'e' ? sum + 1 : d === 'w' ? sum - 1 : sum, 0);
+
+    // return walk.length === 10 && walk.reduce((sum, d) => d === 'n' ? sum + 1 : d === 's' ? sum - 1 : sum, 0) === 0 && walk.reduce((sum, d) => d === 'e' ? sum + 1 : d === 'w' ? sum - 1 : sum, 0) === 0;
+
+    // return walk.length === 10 && walk.reduce((sum, d) => sum + d.match(/[n]/) ? 1 : 0 - d.match(/[s]/) ? 1 : 0 + d.match(/[e]/) ? 1 : 0 - d.match(/[w]/) ? 1 : 0);
+
+    //   return walk.length === 10 && walk.filter(d => d.match(/[n]/)).length === walk.filter(d => d.match(/[s]/)).length && walk.filter(d => d.match(/[e]/)).length === walk.filter(d => d.match(/[w]/)).length;
   }
+  // console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']));
+}
+
+{  /** Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.   */
+
+
+  function comp(ar1, ar2) {
+    return (!Array.isArray(ar1) || !Array.isArray(ar2) || null) ? false : !([...ar1].sort((a, b) => a - b).some((e, i) => e ** 2 !== [...ar2].sort((a, b) => a - b)[i]));
+
+    // return (!Array.isArray(ar1) || !Array.isArray(ar2) || null) ? false : ([...ar1].sort((a, b) => a - b).every((e, i) => e ** 2 === [...ar2].sort((a, b) => a - b)[i]));
+
+    // if (!Array.isArray(ar1) || !Array.isArray(ar2) || null) return false;
+    // const arr1 = [...ar1].sort((a, b) => a - b);
+    // const arr2 = [...ar2].sort((a, b) => a - b);
+    // return (arr1.every((e, i) => e ** 2 === arr2[i]));
+  }
+  // console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 36100, 25921, 361, 20736, 361]));  /**  false   */
+  // console.log(comp([11, 19], [121, 361]));  /**  true   */
 }
