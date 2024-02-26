63edf8e1c9c1312b71960ff39049bc0c0a7e3760
h4y6 <h4y6@i.ua>
2024-02-26 20:16:34 +0200

    modified:   tr-code-js/js/06-tr.js

--- tr-code-js/js/06-tr.js
+++ tr-code-js/js/06-tr.js
@@ -561,6 +561,49 @@ P.S. The situation in this kata can be likened to the more-computer-science-rela
 
     }
   }
-  console.log(queueTime([1, 2, 3, 4, 5], 100));
-  console.log(queueTime([2, 2, 3, 3, 4, 4], 2));
+  // console.log(queueTime([1, 2, 3, 4, 5], 100));
+  // console.log(queueTime([2, 2, 3, 3, 4, 4], 2));
+}
+
+{    /** 8kyu  Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+"I love you"
+"a little"
+"a lot"
+"passionately"
+"madly"
+"not at all"
+If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.   */
+
+  function howMuchILoveYou(nbPetals) {
+    switch (nbPetals % 6) {
+      case 1:
+        return "I love you";
+      case 2:
+        return "a little";
+      case 3:
+        return "a lot";
+      case 4:
+        return "passionately";
+      case 5:
+        return "madly";
+      default: return "not at all";
+    }
+  }
+  // console.log(howMuchILoveYou(11));
+}
+
+{  /**  8kyu  Your task is to find the first element of an array that is not consecutive.
+By not consecutive we mean not exactly 1 larger than the previous element of the array.
+E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+If the whole array is consecutive then return null.
+
+The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!  */
+
+  function firstNonConsecutive(arr) {
+    return arr.find((el, idx, arr) => el - idx !== arr[0]) ?? null;
+  }
+  console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]));
 }
