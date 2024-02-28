08b7484e2f08346453eddf7042f558de68de6ecd
h4y6 <h4y6@i.ua>
2024-02-28 19:04:09 +0200

    modified:   tr-code-js/js/06-tr.js

--- tr-code-js/js/06-tr.js
+++ tr-code-js/js/06-tr.js
@@ -737,7 +737,18 @@ uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]                            */
 
   var uniqueInOrder = function (iterable) {
-    //your code here - remember iterable can be a string or an array
+    // return [...iterable].filter((el, i) => el !== iterable[i + 1]);
+
+    return Array.prototype.filter.call(iterable, ((el, i) => el !== iterable[i + 1]));
+
+    // const arr = [];
+    // for (let i = 0; i < iterable.length; i++) {
+
+    //   if (iterable[i + 1] !== iterable[i]) {
+    //     arr.push(iterable[i]);
+    //   };
+    // }
+    // return arr;
   };
-  // console.log(uniqueInOrder('ABBCcAD'));
+  console.log(uniqueInOrder('ABBBBBCcAD'));
 }
