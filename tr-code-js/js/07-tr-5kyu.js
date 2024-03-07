24b1107e47a01698464b4ce9f40308fd0a0c6a80
h4y6 <h4y6@i.ua>
2024-03-07 23:34:36 +0200

    modified:   tr-code-js/js/07-tr-5kyu.js

--- tr-code-js/js/07-tr-5kyu.js
+++ tr-code-js/js/07-tr-5kyu.js
@@ -527,7 +527,39 @@ Easy case is when the list is made up of only positive numbers and the maximum s
 Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.    */
 
   const maxSequence = arr => {
+    let res = 0;
+    if (arr.every(e => e < 0)) return res;
 
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[0] < 0) arr = arr.slice(1);
+      if (arr[arr.length - 1] < 0) arr = arr.slice(0, arr.length - 1);
+    }
+
+    if (arr.every(e => e >= 0)) return arr.reduce((sum, e) => sum + e, 0);
+
+    return res;
   };
+  // console.log(maxSequence([-2, -3, -1, -5]));
   // console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+  // console.log(maxSequence([-2, -1, 3, 4, 1, 2, 1, 5, -4]));
+}
+
+{  /**8 kyu  Grasshopper - Check for factor 
+This function should test if the factor is a factor of base.
+Return true if it is a factor or false if it is not.
+About factors
+Factors are numbers you can multiply together to get another number.
+2 and 3 are factors of 6 because: 2 * 3 = 6
+
+You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+You can use the mod operator (%) in most languages to check for a remainder
+For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+Note: base is a non-negative number, factor is a positive number.        */
+
+  function checkForFactor(base, factor) {
+    return !(base % factor);
+  }
+  // console.log(checkForFactor(24617, 3));
+  // console.log(checkForFactor(12, 3));
 }
