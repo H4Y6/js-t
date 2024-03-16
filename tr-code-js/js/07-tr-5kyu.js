184ebcc5dce0b07404e0110de8d5e8929a9498e0
h4y6 <h4y6@i.ua>
2024-03-16 18:49:52 +0200

    modified:   tr-code-js/js/07-tr-5kyu.js

--- tr-code-js/js/07-tr-5kyu.js
+++ tr-code-js/js/07-tr-5kyu.js
@@ -1351,3 +1351,120 @@ Examples (input --> output):
   // const numberToString = num => num + '';
   // console.log(numberToString('5715310'));
 }
+
+{  /**8 kyu  Beginner Series #4 Cockroach 
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+For example:
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.  */
+
+  function cockroachSpeed(s) {
+    // 100000cm / 3600s = 1 / .036;
+    return Math.floor(s / .036);
+  }
+  // console.log(cockroachSpeed(1.09));
+}
+
+{  /**8 kyu  Grasshopper - Terminal game move function  
+Terminal game move function
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+Example:
+move(3, 6) should equal 15 */
+
+  function move(position, roll) {
+    return position + 2 * roll;
+  }
+  // console.log(move(3, 6));
+}
+
+{  /** 7 kyu  Is this a triangle? 
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+(In this case, all triangles must have surface greater than 0 to be accepted).
+Examples:
+Input -> Output
+1,2,2 -> true
+4,2,3 -> true
+2,2,2 -> true
+1,2,3 -> false
+-5,1,3 -> false
+0,2,3 -> false
+1,2,9 -> false     */
+
+  function isTriangle(a, b, c) {
+    return a + b > c && a + c > b && b + c > a;
+    // return a + b + c - Math.max(a, b, c) - Math.min(a, b, c) > Math.max(a, b, c) - Math.min(a, b, c);
+  }
+  // console.log(isTriangle(2, 2, 3));
+}
+
+{  /** 8 kyu  Sum of positive 
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+Note: if there is nothing to sum, the sum is default to 0. */
+
+  function positiveSum(arr) {
+    return arr.reduce((sum, e) => e > 0 ? sum + e : sum, 0);
+  }
+  // console.log(positiveSum([-1, -2, 3, 4, -5]));
+}
+
+{  /** Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+number of hotdogs	price per unit (cents)
+n < 5	100
+n >= 5 and n < 10	95
+n >= 10	90  */
+
+  function saleHotdogs(n) {
+    return n < 5 ? 100 * n : n >= 10 ? 90 * n : 95 * n;
+  }
+  // console.log((saleHotdogs(22)));
+}
+
+{/** 8 kyu  What's the real floor? Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor (due to superstition).
+Write a function that given a floor in the american system returns the floor in the european system.
+With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. In case of above 13, they move down by two because there are two omitted numbers below them.
+Basements (negatives) stay the same as the universal level.
+More information here
+Examples
+1  =>  0 
+0  =>  0
+5  =>  4
+15  =>  13
+-3  =>  -3        */
+
+  function getRealFloor(n) {
+    // return n <= 0 ? n : n < 13 ? n - 1 : n - 2;
+    return n > 0 ? --n - (n > 13) : n;
+  }
+  // console.log(getRealFloor(21));
+}
+
+{  /**  6 kyu  Simple Encryption #1 - Alternating Split 
+Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+Examples:
+encrypt("012345", 1)  =>  "135024"
+encrypt("012345", 2)  =>  "135024"  ->  "304152"
+encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+encrypt("01234", 1)  =>  "13024"
+encrypt("01234", 2)  =>  "13024"  ->  "32104"
+encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+Together with the encryption function, you should also implement a decryption function which reverses the process.
+If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+This kata is part of the Simple Encryption Series:
+Simple Encryption #1 - Alternating Split
+Simple Encryption #2 - Index-Difference
+Simple Encryption #3 - Turn The Bits Around
+Simple Encryption #4 - Qwerty
+Have fun coding it and please don't forget to vote and rank this kata! :-)  */
+
+  function encrypt(text, n) {
+
+  }
+
+  function decrypt(encryptedText, n) {
+
+  }
+  console.log(encrypt("This is a test!", 3));
+  console.log(decrypt("s eT ashi tist!", 2));
+}
