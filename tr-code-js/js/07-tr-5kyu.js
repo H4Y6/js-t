164d9fa1c04090e253222d4dfafc3585a9c6c0ed
h4y6 <h4y6@i.ua>
2024-03-19 15:24:00 +0200

    modified:   tr-code-js/js/07-tr-5kyu.js

--- tr-code-js/js/07-tr-5kyu.js
+++ tr-code-js/js/07-tr-5kyu.js
@@ -1700,3 +1700,43 @@ isIsogram "aba" = false        */
   // console.log(isIsogram("abba"));
   // console.log(isIsogram("Dermatoglyphics"));
 }
+
+{  /**  8 kyu  Name Shuffler
+Write a function that returns a string in which firstname is swapped with last name.
+Example(Input --> Output)
+"john McClane" --> "McClane john"   */
+
+  const nameShuffler = str => str.split(' ').reverse().join(' ');
+  // const nameShuffler = str => str.split(' ')[1] + ' ' + str.split(' ')[0];
+  // const nameShuffler = str => {
+  //   const [a, b] = str.split(' ');
+  //   return [b, a].join(' ');
+  // };
+  // console.log(nameShuffler('tom jerry'));
+}
+
+{  /**7 kyu  Round up to the next multiple of 5 
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+Examples:
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5  etc.
+Input may be any positive or negative integer (including 0).
+You can assume that all inputs are valid integers.           */
+
+  const roundToNext5 = n => n % 5 ? roundToNext5(n + 1) : n;
+  // const roundToNext5 = n => 5 * Math.ceil(n / 5);
+  // const roundToNext5 = n => !(n % 5) ? n : (n > 0) ? 5 * parseInt(n / 5) + 5 : 5 * parseInt(n / 5);
+  // const roundToNext5 = n => {
+  // while (n % 5) {
+  // n++;
+  // } return n;
+  // };
+  console.log(roundToNext5(-11));
+}
