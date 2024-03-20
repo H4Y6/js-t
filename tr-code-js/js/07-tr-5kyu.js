005734e317b2f2e317dd0bd0d035cc01d6e020a4
h4y6 <h4y6@i.ua>
2024-03-20 20:09:12 +0200

    modified:   tr-code-js/js/07-tr-5kyu.js

--- tr-code-js/js/07-tr-5kyu.js
+++ tr-code-js/js/07-tr-5kyu.js
@@ -1832,3 +1832,18 @@ It's pretty straightforward. Your goal is to create a function that removes the 
   const removeChar = str => str.replace(/^\w|\w$/g, '');
   // console.log(removeChar('place'));
 }
+
+{  /**8 kyu  The Wide-Mouthed frog!   
+The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+When he meets the alligator, it then makes a tiny mouth.
+
+Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.  */
+
+  const mouthSize = animal => /alligator/i.test(animal) ? 'small' : 'wide';
+  // const mouthSize = animal => animal.match(/alligator/i) ? 'small' : 'wide';
+  // const mouthSize = animal => animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
+  console.log(mouthSize('alligator'));
+}
