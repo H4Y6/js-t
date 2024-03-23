1334c33c979d869af864e46bd0b01bcb2b23d8e8
h4y6 <h4y6@i.ua>
2024-03-23 23:57:52 +0200

    modified:   tr-code-js/js/07-tr-5kyu.js

--- tr-code-js/js/07-tr-5kyu.js
+++ tr-code-js/js/07-tr-5kyu.js
@@ -2151,4 +2151,19 @@ P.S. The situation in this kata can be likened to the more-computer-science-rela
   // console.log(queueTime([2, 2, 3, 3, 4, 4], 2));
 }
 
+{  /** 8 kyu  Find Multiples of a Number 
+In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6. */
+
+  const findMultiples = (integer, limit) => {
+    let arr = [];
+    for (let i = 1; i <= limit / integer; i++) {
+      const el = i * integer;
+      arr.push(el);
+    }
+    return arr;
+  };
+  // console.log(findMultiples(11, 54));
+}
+
 
