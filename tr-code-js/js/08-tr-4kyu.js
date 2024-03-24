25aec3060bed26db210dc08c856816a870dac127
h4y6 <h4y6@i.ua>
2024-03-24 21:40:39 +0200

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -39,3 +39,65 @@ Examples (input -> output)
   // const mergeArrays = (arr1, arr2) => [...arr1, ...arr2].filter((e, i, ar) => ar.indexOf(e) === i).sort((a, b) => a - b);
   // console.log(mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]));
 }
+
+{  /**  6 kyu  Mexican Wave
+The wave (known as the Mexican wave in the English-speaking world outside North America) is an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms. Immediately upon stretching to full height, the spectator returns to the usual seated position.
+The result is a wave of standing spectators that travels through the crowd, even though individual spectators never move away from their seats. In many large arenas the crowd is seated in a contiguous circuit all the way around the sport field, and so the wave is able to travel continuously around the arena; in discontiguous seating arrangements, the wave can instead reflect back and forth through the crowd. When the gap in seating is narrow, the wave can sometimes pass through it. Usually only one wave crest will be present at any given time in an arena, although simultaneous, counter-rotating waves have been produced. (Source Wikipedia)
+Task
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+Rules
+ 1.  The input string will always be lower case but maybe empty.
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+Good luck and enjoy!
+Kata Series
+If you enjoyed this, then please try one of my other Katas. Any feedback, translations and grading of beta Katas are greatly appreciated. Thank you.
+Rank 
+Maze Runner
+Rank 
+Scooby Doo Puzzle
+Rank 
+Driving License
+Rank 
+Connect 4
+Rank 
+Vending Machine
+Rank 
+Snakes and Ladders
+Rank 
+Mastermind
+Rank 
+Guess Who?
+Rank 
+Am I safe to drive?
+Rank 
+Mexican Wave
+Rank 
+Pigs in a Pen
+Rank 
+Hungry Hippos
+Rank 
+Plenty of Fish in the Pond
+Rank 
+Fruit Machine
+Rank 
+Car Park Escape     */
+
+  function wave(str, res = []) {
+    [...str].forEach((e, i) => (/\w/.test(e)) ? res.push(str.slice(0, i) + e.toUpperCase(e) + str.slice(i + 1)) : 0);
+    return res;
+  };
+  // function wave(str) {
+  //   let res = [];
+  //   str.split('').forEach((e, i) => {
+  //     let strS = str.slice(i).replace(/[a-z]/, e => e.toUpperCase());
+  //     let newStr = str.slice(0, i) + strS;
+  //     res.push(newStr);
+  //   });
+  //   return res.filter((e, i, ar) => ar.indexOf(e) === i).filter(e => e !== str);
+  // }
+  console.log(wave(' gap '));
+  console.log(wave("hello"));
+  console.log(wave(" two  words "));
+}
