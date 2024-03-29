548cb40de35a218e0d636302757e35cbb03e0214
h4y6 <h4y6@i.ua>
2024-03-29 22:47:38 +0200

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -409,3 +409,40 @@ This example will return the string Hello, John Smith! Welcome to Phoenix, Arizo
   }
   // console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'));
 }
+
+{  /**5 kyu  Integers: Recreation One
+1, 246, 2, 123, 3, 82, 6, 41 are the divisors of number 246. Squaring these divisors we get: 1, 60516, 4, 15129, 9, 6724, 36, 1681. The sum of these squares is 84100 which is 290 * 290.
+Task
+Find all integers between m and n (m and n integers with 1 <= m <= n) such that the sum of their squared divisors is itself a square.
+We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The subarrays (or tuples or Pairs) will have two elements: first the number the squared divisors of which is a square and then the sum of the squared divisors.
+Example:
+list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
+list_squared(42, 250) --> [[42, 2500], [246, 84100]]
+The form of the examples may change according to the language, see "Sample Tests".
+Note
+In Fortran - as in any other language - the returned string is not permitted to contain any redundant trailing whitespace: you can use dynamically allocated character strings.   */
+
+  // not ready yet
+  function listSquared(m, n) {
+    let res = [0, 0];
+    let sum = 0;
+    const arr = Array.from({ length: n }, (_, i) => 1 + i);
+    const ar = arr.slice(0).map(e => arr.filter(el => !(e % el)));
+    const a = ar.map(el => el.reduce((res = [0, 0], e) => {
+      sum += (e * e);
+      if (Math.sqrt(sum) % 1 === 0) {
+        res[0] = e;
+        res[1] = sum;
+        return res;
+      }
+    }, [0, 0]
+    )
+
+    );
+
+    return ar;
+  }
+  console.log(listSquared(250, 500));
+  // console.log(Math.sqrt(1 + 246 ** 2 + 4 + 123 ** 2 + 9 + 82 ** 2 + 36 + 41 ** 2));
+  // console.log(Math.sqrt(1 + 42 ** 2 + 4 + 21 ** 2 + 9 + 14 ** 2 + 36 + 7 ** 2));
+}
