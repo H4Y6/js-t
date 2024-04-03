d073ecd20b2bf61b81c6ac797c9ebf4d09053245
h4y6 <h4y6@i.ua>
2024-04-03 20:11:08 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -624,7 +624,16 @@ and return a string like this:
 {  /**  6 kyu  Delete occurrences of an element if it occurs more than n times
 Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
 For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
-With list [20,37,20,21] and number 1, the result would be [20,37,21].    */
+With list [20,37,20,21] and number 1, the result would be [20,37,21]. 
+
+const { assert } = require('chai');
+describe("Tests", () => {
+  it("test", () => {
+    assert.sameOrderedMembers(deleteNth([20,37,20,21], 1), [20,37,21])
+    assert.sameOrderedMembers(deleteNth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
+    assert.sameOrderedMembers(deleteNth([12,39,19,39,39,19,12], 1), [12, 39, 19])
+  });
+});              */
 
   const deleteNth = (arr, n) => {
     const newArr = [];
