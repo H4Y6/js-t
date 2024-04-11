6a650119de8bbdb924274dfa7b42fc838d94fb89
h4y6 <h4y6@i.ua>
2024-04-11 18:24:33 +0300

    modified:   js-2-1/js/11-arrays.js

--- js-2-1/js/11-arrays.js
+++ js-2-1/js/11-arrays.js
@@ -7,9 +7,51 @@
 // console.log(foo === bood);
 
 const fruits = ['kiwi', 'banana', 'apple', 'ananas', 'cucumber'];
-// console.log({ slicedFruits: fruits.slice(1, -1), fruits });
+console.log({ slicedFruits: fruits.slice(1, -1), fruits });
 
-// console.log({ splicedFruits: fruits.splice(-1, 1, 'cabbage'), fruits });
+console.log({ fruits, splicedFruits: fruits.splice(-3, 1, 'papaya') });
+// console.log({ fruits, splicedFruits: fruits.splice(-3) });
 
-console.log(fruits.pop(), fruits, fruits.push('pumpkin'), fruits.unshift('laster'), fruits.shift());
+// console.log(fruits.pop(), fruits, fruits.push('pumpkin'), fruits.unshift('laster'), fruits.shift());
 
+function checkStorage(a, o) {
+  if (o === 0) {
+    return 'empty';
+  }
+  else if (o > a) {
+    return 'too much';
+  }
+  return 'ok';
+}
+// console.log(checkStorage(4, 3));
+
+const numbers = [2, 4, 3, 8, 5, 9, 7];
+const findIndex = function (array, num) {
+  let idx = -1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === num) {
+      idx = i;
+      break;
+    }
+  }
+
+  return idx;
+};
+// console.log(findIndex(numbers, 5));
+
+const sumNumbers = function (arr, threshold) {
+  let sum = 0;
+  for (const num of arr) {
+    sum += num > threshold ? num : 0;
+  }
+
+  return sum;
+};
+// console.log(sumNumbers(numbers, 6));
+
+{
+  let a = 4;
+  const b = ++a;
+  // const b = a++;
+  // console.log(a, b);
+}
