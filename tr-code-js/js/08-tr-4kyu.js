06b7c43281e89ff3da6c8fc59cb94f58d416cf92
h4y6 <h4y6@i.ua>
2024-04-12 18:08:55 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -976,5 +976,33 @@ Examples
 "1,2"  =>  NULL        */
 
   const array = string => string.split(',').slice(1, -1).join(' ') || null;
-  console.log(array('A1,B2'));
+  // console.log(array('A1,B2'));
+}
+
+{  /**  8 kyu  Find Maximum and Minimum Values of a List Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+Notes
+You may consider that there will not be any empty arrays/vectors.     */
+
+  const min = function (list) {
+    return list.reduce((acc, el) => el < acc ? acc = el : acc, Infinity);
+  };
+
+  const max = function (list) {
+    return list.reduce((acc, el) => el > acc ? acc = el : acc, 0);
+  };
+  // console.log(min([4, 6, 2, 1, 9, 63, -134, 566]));
+  // console.log(min([-52, 56, 30, 29, -54, 0, -110, -1]));
+  // console.log(max([-52, 56, 30, 29, -54, 0, -110]));
+  // console.log(max([4, 6, 2, 1, 9, 63, -134, 566]));
+}
+
+{  /**  7 kyu  Factorial       Your task is to write function factorial.         */
+
+  // const factorial = n => n < 1 ? 1 : n * factorial(n - 1);
+  console.log(factorial(4));
 }
