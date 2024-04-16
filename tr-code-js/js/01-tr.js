50de88d687fc1d843a0e13ef131827b9fb28210d
h4y6 <h4y6@i.ua>
2024-04-16 23:59:22 +0300

    modified:   tr-code-js/js/01-tr.js

--- tr-code-js/js/01-tr.js
+++ tr-code-js/js/01-tr.js
@@ -391,3 +391,21 @@ function makeTask(data) {
     return matches;
   }
 }
+
+{
+  const bookShelf = {
+    books: ["The last kingdom", "The guardian of dreams"],
+    getBooks() {
+      return "Returning all books";
+    },
+    addBook(bookName) {
+      return `Adding book ${bookName}`;
+    },
+    removeBook(bookName) {
+      return `Deleting book ${bookName}`;
+    },
+    updateBook(oldName, newName) {
+      return `Updating book ${oldName} to ${newName}`;
+    },
+  };
+}
