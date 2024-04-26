53c6bfa88eee6bb4a76e253727ac0b8b78e1bf6c
h4y6 <h4y6@i.ua>
2024-04-26 23:54:30 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -1524,3 +1524,91 @@ Examples
   // console.log(isAnagram("dkgINQkXMCPViXY", "NIMPCXXkiVkdQg"));
   // console.log(isAnagram("IDaQbJBRvLFLzOviQr", "QzravBLivJaDQILObF"));
 }
+
+{  /**  7 kyu  Sum of numbers from 0 to N
+We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+Example:
+Input: > 6
+Output:  0+1+2+3+4+5+6 = 21
+
+Input:  > -15
+Output:  -15<0
+
+Input:  > 0
+Output:  0=0     */
+
+  const SequenceSum = (n) => {
+    if (n < 0) return `${n} < 0`;
+    let str = '';
+    let sum = 0;
+    for (let i = 0; i <= n; i++) {
+      str += i + '+';
+      sum += i;
+    }
+
+    return str.slice(0, -1) + ' = ' + sum;
+  };
+  // console.log(SequenceSum(-15));
+  // console.log(SequenceSum(6));
+}
+
+{  /**8 kyu  Multiply the number 
+Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+  3 -->    15  (  3 * 5¹)
+ 10 -->   250  ( 10 * 5²)
+200 --> 25000  (200 * 5³)
+  0 -->     0  (  0 * 5¹)
+ -3 -->   -15  ( -3 * 5¹)      */
+
+  const multiply = number => number * 5 ** (Math.abs(number) + '').length;
+
+  // console.log(multiply(10));
+}
+
+{  /** 7 kyu  Two fighters, one winner. 
+Create a function that returns the name of the winner in a fight between two fighters.
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+Example:
+  declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+  
+  Lew attacks Harry; Harry now has 3 health.
+  Harry attacks Lew; Lew now has 6 health.
+  Lew attacks Harry; Harry now has 1 health.
+  Harry attacks Lew; Lew now has 2 health.
+  Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.   */
+  function Fighter(name, health, damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+    this.toString = function () { return this.name; };
+  }
+
+  function declareWinner(fighter1, fighter2, firstAttacker) {
+
+    if (fighter1.name === firstAttacker) {
+
+      while (fighter1.health > 0) {
+        fighter2.health -= fighter1.damagePerAttack;
+        if (fighter2.health <= 0) return fighter1.name;
+        fighter1.health -= fighter2.damagePerAttack;
+        if (fighter1.health <= 0) return fighter2.name;
+      }
+    }
+
+    if (fighter2.name === firstAttacker) {
+
+      while (fighter1.health > 0) {
+        fighter1.health -= fighter2.damagePerAttack;
+        if (fighter1.health <= 0) return fighter2.name;
+        fighter2.health -= fighter1.damagePerAttack;
+        if (fighter2.health <= 0) return fighter1.name;
+      }
+    }
+  }
+  // console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"));
+  // console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"));
+}
