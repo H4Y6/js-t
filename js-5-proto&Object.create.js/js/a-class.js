2e4ee7b780bf7c4fa1996cdc518ba1ab38e6f1c5
h4y6 <h4y6@i.ua>
2024-04-27 19:29:55 +0300

    modified:   js-5-proto&Object.create.js/js/a-class.js

--- js-5-proto&Object.create.js/js/a-class.js
+++ js-5-proto&Object.create.js/js/a-class.js
@@ -32,49 +32,102 @@
  * Syntax, public properties and methods
  * Getters and setters
  */
+{
+    class Car {
+        static description = 'Class to descript cars';
 
-class Car {
-    static description = 'Class to descript cars';
+        static logInfo(carObj) {
+            console.log('Car.logInfo -> carObj', carObj);
+        }
 
-    static logInfo(carObj) {
-        console.log('Car.logInfo -> carObj', carObj);
+        constructor({ brand, model, price } = {}) {
+            this.brand = brand;
+            this._model = model;
+            this._price = price;
+        }
+
+        get price() {
+            return this._price;
+        }
+
+        set price(newPrice) {
+            this._price = newPrice;
+        }
+
+        get model() {
+            return this._model;
+        }
+
+        set model(newModel) {
+            this._model = newModel;
+        }
     }
 
-    constructor({ brand, model, price } = {}) {
-        this.brand = brand;
-        this._model = model;
-        this._price = price;
-    }
+    const carInstance = new Car({
+        brand: 'Audi',
+        model: 'Q3',
+        price: 35000,
+    });
 
-    get price() {
-        return this._price;
-    }
+    // console.log(carInstance.model);
+    carInstance.model = 'Q4';
+    // console.log(carInstance.model);
+    // 
+    // console.log(carInstance.price);
+    carInstance.price = 50000;
+    // console.log(carInstance.price);
 
-    set price(newPrice) {
-        this._price = newPrice;
-    }
-
-    get model() {
-        return this._model;
-    }
-
-    set model(newModel) {
-        this._model = newModel;
-    }
+    // console.log(carInstance);
 }
 
-const carInstance = new Car({
-    brand: 'Audi',
-    model: 'Q3',
-    price: 35000,
-});
+{
+    // const Car = function ({ brand, model, price } = {}) {
+    //     console.log('this->', this);
 
-console.log(carInstance.model);
-carInstance.model = 'Q4';
-console.log(carInstance.model);
+    //     this.brand = brand;
+    //     this.model = model;
+    //     this.price = price;
+    // };
 
-console.log(carInstance.price);
-carInstance.price = 50000;
-console.log(carInstance.price);
+    // Car.description = 'Method to descript a car instance';
 
-console.log(carInstance);
+    class Car {
+        static description = 'Class to descript a car instance';
+
+        static logInfo(carObj) { console.log('Car.logInfo -> carObj:', carObj); };
+        // static logInfo({ brand, model, price }) { console.log(`Brand: ${brand}, model: ${model}, price: ${price}`); };
+
+        constructor({ brand, model, price } = {}) {
+            console.log('this->', this);
+
+            this.brand = brand;
+            this._model = model;
+            this.price = price;
+        }
+
+        changePrice(newPrice) {
+            this.price = newPrice;
+        }
+
+        set model(newModel) {
+            this._model = newModel;
+        }
+
+        get model() {
+            return this._model;
+        }
+    }
+
+    const car = new Car({
+        brand: 'Audi',
+        model: 'G3',
+        price: 36222
+    });
+
+    car.changePrice(35555);
+    car.model = ('G5');
+    console.log('car', car);
+
+    Car.logInfo(car);
+    console.log(car.model);
+}
