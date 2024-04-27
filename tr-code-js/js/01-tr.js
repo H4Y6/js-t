5cdc0b461f3984db3cbd75e4fcef67055ca84d89
h4y6 <h4y6@i.ua>
2024-04-27 20:51:13 +0300

    modified:   tr-code-js/index.html modified:   tr-code-js/js/01-tr.js

--- tr-code-js/js/01-tr.js
+++ tr-code-js/js/01-tr.js
@@ -474,3 +474,145 @@ function makeTask(data) {
   };
 
 }
+
+{
+  const pizzaPalace = {
+    pizzas: ["Supercheese", "Smoked", "Four meats"],
+    checkPizza(pizzaName) {
+      return this.pizzas.includes(pizzaName);
+    },
+    order(pizzaName) {
+      const isPizzaAvailable = this.checkPizza(pizzaName);
+
+      if (!isPizzaAvailable) {
+        return `Sorry, there is no pizza named «${pizzaName}»`;
+      }
+
+      return `Order accepted, preparing «${pizzaName}» pizza`;
+    },
+  };
+
+  const customer = {
+    username: "Mango",
+    balance: 24000,
+    discount: 0.1,
+    orders: ["Burger", "Pizza", "Salad"],
+    getBalance() {
+      return this.balance;
+    },
+    getDiscount() {
+      return this.discount;
+    },
+    setDiscount(value) {
+      this.discount = value;
+    },
+    getOrders() {
+      return this.orders;
+    },
+    addOrder(cost, order) {
+      this.balance -= cost - cost * this.discount;
+      this.orders.push(order);
+    },
+  };
+
+  customer.setDiscount(0.15);
+  console.log(customer.getDiscount()); // 0.15
+  customer.addOrder(5000, "Steak");
+  console.log(customer.getBalance()); // 19750
+  console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+  const historyService = {
+    orders: [
+      { email: "jacob@hotmail.com", dish: "Burrito" },
+      { email: "solomon@topmail.net", dish: "Burger" },
+      { email: "artemis@coldmail.net", dish: "Pizza" },
+      { email: "solomon@topmail.net", dish: "Apple pie" },
+      { email: "jacob@hotmail.com", dish: "Taco" },
+    ],
+    getOrdersLog() {
+      return this.orders
+        .map(order => `email: ${order.email} dish: ${order.dish}`)
+        .join(" - ");
+    },
+    getEmails() {
+      const emails = this.orders.map(order => order.email);
+      const uniqueEmails = new Set(emails);
+      return [...uniqueEmails];
+    },
+    getOrdersByEmail(email) {
+      return this.orders.filter(order => order.email === email);
+    },
+  };
+
+  const ancestor = {
+    name: "Paul",
+    age: 83,
+    surname: "Dawson",
+    heritage: "Irish",
+  };
+
+  const parent = Object.create(ancestor);
+  parent.name = "Stacey";
+  parent.surname = "Moore";
+  parent.age = 54;
+
+  const child = Object.create(parent);
+  child.name = "Jason";
+  child.age = 27;
+  console.log('parent:', parent);
+  console.log('child:', child);
+
+  class Car {
+    constructor({ brand, model, price } = {}) {
+      this.brand = brand;
+      this.model = model;
+      this.price = price;
+    }
+    getPrice() { return this.price; }
+    changePrice(newPrice) { this.price = newPrice; }
+  }
+  const car = new Car({ brand: "Audi", model: "Q3", price: 36000 });
+  console.log('car:', car);
+
+  class Storage {
+    constructor(items) {
+      this.items = items;
+    }
+    getItems() { return this.items; }
+    addItem(newItem) { this.items.push(newItem); }
+    removeItem(itemToRemove) {
+      return this.items.splice(this.items.indexOf(itemToRemove), 1);
+    }
+  }
+  const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+  storage.addItem("Droid");
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+  console.log(storage.removeItem("Prolonger"));  /*  ['Prolonger']   */
+  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+  class StringBuilder {
+    constructor(initialValue) {
+      this.value = initialValue;
+    }
+    getValue() { return this.value; }
+    padEnd(str) {
+      this.value += str;
+    }
+    padStart(str) {
+      this.value = str + this.value;
+    }
+    padBoth(str) {
+      this.value = str + this.value + str;
+    }
+  }
+  const builder = new StringBuilder(".");
+  console.log(builder.getValue()); // "."
+  builder.padStart("^");
+  console.log(builder.getValue()); // "^."
+  builder.padEnd("^");
+  console.log(builder.getValue()); // "^.^"
+  builder.padBoth("=");
+  console.log(builder.getValue()); // "=^.^="
+
+}
