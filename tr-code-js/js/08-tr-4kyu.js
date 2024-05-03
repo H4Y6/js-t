3c59da6a01bbc64224d58235b318882761bca719
h4y6 <h4y6@i.ua>
2024-05-03 19:31:00 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -1941,7 +1941,7 @@ Note : The last element 2 is greater than the sum of its right elements (abstrac
   // console.log(arrayLeaders([-36, -12, -27]));
 }
 
-{  /**7 kyu  Maximum Gap (Array Series #4)  
+{  /**  7 kyu  Maximum Gap (Array Series #4)  
 Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
 
 Notes
@@ -1972,4 +1972,92 @@ Note : Regardless the sign of negativity .        */
   }
   // console.log(maxGap([24, 299, 131, 14, 26, 25]));
   // console.log(maxGap([-3, -27, -4, -2]));
+}
+
+{  /**  regExp   */
+
+  var str = "ABABCDEababcde";
+  const [a, b, c] = str.match(/ab/i);
+  // console.log(str.match(/ab/i).input);
+  // console.log(str.match(/ab/ig).length);
+  // console.log(str.match(/ab/).index);
+
+
+  function count2(str, char) {
+    return str.length - str.replace(new RegExp(char, "g"), "").length;
+  }
+  function count3(str, char) {
+    return str.split(char).length;
+  }
+  // console.log(count2('aaaaa', 'a'));
+  // console.log(count3("aaaaa", "b"));
+}
+
+{/**7 kyu  Product Array (Array Series #5)
+Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+Notes
+Array/list size is at least 2 .
+Array/list's numbers Will be only Positives
+Repetition of numbers in the array/list could occur.
+Input >> Output Examples
+productArray ({12,20}) ==>  return {20,12}
+Explanation:
+The first element in prod [] array 20 is the product of all array's elements except the first element
+The second element 12 is the product of all array's elements except the second element .
+productArray ({1,5,2}) ==> return {10,2,5}
+Explanation:
+The first element 10 is the product of all array's elements except the first element 1
+The second element 2 is the product of all array's elements except the second element 5
+The Third element 5 is the product of all array's elements except the Third element 2.
+productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
+Explanation:
+The first element 180 is the product of all array's elements except the first element 10
+The second element 600 is the product of all array's elements except the second element 3
+The Third element 360 is the product of all array's elements except the third element 5
+The Fourth element 300 is the product of all array's elements except the fourth element 6
+Finally ,The Fifth element 900 is the product of all array's elements except the fifth element 2        */
+
+  function productArray(numbers) {
+    const product = numbers.reduce((total, e) => total * e);
+    return numbers.map(e => product / e);
+  }
+  // console.log(productArray([16, 17, 4, 3, 5, 2]));
+}
+
+{  /** 7 kyu  Minimum Steps (Array Series #6) 
+Given an array of N integers, you have to find how many times you have to add up the smallest numbers in the array until their Sum becomes greater or equal to K.
+Notes:
+List size is at least 3.
+All numbers will be positive.
+Numbers could occur more than once , (Duplications may exist).
+Threshold K will always be reachable.
+Input >> Output Examples
+minimumSteps({1, 10, 12, 9, 2, 3}, 6)  ==>  return (2)
+Explanation:
+We add two smallest elements (1 + 2), their sum is 3 .
+Then we add the next smallest number to it (3 + 3) , so the sum becomes 6 .
+Now the result is greater or equal to 6 , Hence the output is (2) i.e (2) operations are required to do this .
+minimumSteps({8 , 9, 4, 2}, 23)  ==> return (3)
+Explanation:
+We add two smallest elements (4 + 2), their sum is 6 .
+Then we add the next smallest number to it (6 + 8) , so the sum becomes 14 .
+Now we add the next smallest number (14 + 9) , so the sum becomes 23 .
+Now the result is greater or equal to 23 , Hence the output is (3) i.e (3) operations are required to do this .
+minimumSteps({19,98,69,28,75,45,17,98,67}, 464)  ==>  return (8)
+Explanation:
+We add two smallest elements (19 + 17), their sum is 36 .
+Then we add the next smallest number to it (36 + 28) , so the sum becomes 64 .
+We need to keep doing this until the sum becomes greater or equal to K (464 in this case), which will require 8 Steps .     */
+
+  const minimumSteps = (numbers, value) => [...numbers].sort((a, b) => a - b).filter(e => (value -= e) > 0, 0).length;
+
+  // function minimumSteps(numbers, value) {
+  //   const arr = [...numbers].sort((a, b) => a - b);
+  //   for (let sum = 0, i = 0; i < arr.length; i++) {
+  //     sum += arr[i];
+  //     if (sum >= value) return i;
+  //   }
+  // }
+  // console.log(minimumSteps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464));
+  // console.log(minimumSteps([8, 9, 10, 4, 33, 2], 23));
 }
