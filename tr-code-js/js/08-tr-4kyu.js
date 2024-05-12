799d5c0578e08e4768c68c23b0b06376b26e1120
h4y6 <h4y6@i.ua>
2024-05-12 21:04:17 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -2421,3 +2421,15 @@ Examples: (Input --> output)
 
   // console.log(sumCubes(3));
 }
+
+{  /**  8 kyu  Grasshopper - Combine strings
+Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+Example:
+combineNames('James', 'Stevens')
+returns:
+'James Stevens'  */
+
+  const combineNames = (firstName, lastName) => [firstName, lastName].join(' ');
+  // console.log(combineNames('James', 'Stevens'));
+}
