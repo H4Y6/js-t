460b3c191c31ae1d8f3b9994b70ccac082861680
h4y6 <h4y6@i.ua>
2024-05-15 19:32:29 +0300

    modified:   js-6-1/js/01-foreach.js

--- js-6-1/js/01-foreach.js
+++ js-6-1/js/01-foreach.js
@@ -5,10 +5,23 @@
  * - Заменяет классический for, если не нужно прерывать цикл
  */
 
-const numbers = [5, 10, 15, 20, 25];
+/**
+ *  This is an array iteration method that is used as a replacement for the for and for...of loops when working with a data collection.
 
-numbers.forEach(function (number) {
-  console.log('number', number);
+array.forEach(function callback(element, index, array) {
+  // Callback body
+});
+
+* - Iterates over an array element by element.
+* - Calls a callback function for each element of the array.
+* - Returns nothing.
+Callback arguments are the value of the current element element, its index index and the original array. You can declare only those parameters that are required – most often this is an element – but never forget about their order. */
+
+const numbers = [0, 5, 10, 15, 20, 25];
+
+numbers.forEach(function (number, i, array) {
+  console.log(`Index ${i}, number ${number}, value ${numbers[i]}`);
+  numbers[i] += 1;  /** the way to change an array value */
 });
 
 console.log(numbers);
