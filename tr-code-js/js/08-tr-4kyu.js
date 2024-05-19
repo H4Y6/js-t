3f29f7b49d456b07e76480e285ed20834d3b17c4
h4y6 <h4y6@i.ua>
2024-05-19 23:52:10 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -2561,7 +2561,8 @@ isPowerOfTwo(4096) // -> true
 isPowerOfTwo(333)  // -> false
 Beware of certain edge cases - for example, 1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.      */
 
-  const isPowerOfTwo = n => n < 1 ? false : n === 1 ? true : n % 2 ? false : isPowerOfTwo(n /= 2);
+  const isPowerOfTwo = n => Number.isInteger(Math.log2(n));
+  // const isPowerOfTwo = n => n < 1 ? false : n === 1 ? true : n % 2 ? false : isPowerOfTwo(n /= 2);
 
   // function isPowerOfTwo(n) {
   // if (n === 1) return true;
@@ -2572,6 +2573,6 @@ Beware of certain edge cases - for example, 1 is a power of 2 since 2^0 = 1 and 
   // return isPowerOfTwo(n /= 2);
 
   // }
-  console.log(isPowerOfTwo(4096));
-  console.log(isPowerOfTwo(0));
+  // console.log(isPowerOfTwo(4096));
+  // console.log(isPowerOfTwo(0));
 }
