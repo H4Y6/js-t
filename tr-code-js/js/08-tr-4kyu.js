616467bb00ac8727e3ca390e8d4a0693c3d93265
h4y6 <h4y6@i.ua>
2024-05-19 23:48:15 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -2548,5 +2548,30 @@ Given a non-empty array of integers, return the result of multiplying the values
 
   const grow = x => eval(x.join('*'));
   // const grow = x => x.reduce((res, el) => res * el, 1);
-  console.log(grow([1, 2, 3]));
+  // console.log(grow([1, 2, 3]));
+}
+
+{  /** 7 kyu  Power of two 
+Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+You may assume the input is always valid.
+Examples
+isPowerOfTwo(1024) // -> true
+isPowerOfTwo(4096) // -> true
+isPowerOfTwo(333)  // -> false
+Beware of certain edge cases - for example, 1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.      */
+
+  const isPowerOfTwo = n => n < 1 ? false : n === 1 ? true : n % 2 ? false : isPowerOfTwo(n /= 2);
+
+  // function isPowerOfTwo(n) {
+  // if (n === 1) return true;
+  // if (n % 2) {
+  //   return false;
+  // }
+
+  // return isPowerOfTwo(n /= 2);
+
+  // }
+  console.log(isPowerOfTwo(4096));
+  console.log(isPowerOfTwo(0));
 }
