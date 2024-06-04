8af7d962e5ecf3ee5adbdf0c84c780176c0d5b9d
h4y6 <h4y6@i.ua>
2024-06-04 23:24:12 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -2852,17 +2852,16 @@ P. S. You can use \n in string to jump to the next line.  */
   Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
 Example:
-Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"     */
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"    /test failed */
 
 
-  String.prototype.toJadenCase = function (str) {
-    this.s = str;
+  String.prototype.toJadenCase = function () {
+    this.s = "How can mirrors be real if our eyes aren't real";
     const subStrs = this.s.split(' ');
     let newStr = '';
-    subStrs.forEach(s => [(newStr += ' ' + s.replace(/^./, s[0].toUpperCase()))]);
+    subStrs.forEach(s => [(newStr += ' ' + s.replace(s[0], s[0].toUpperCase()))]);
     return newStr.trim();
   };
 
-  const str = new String();
-  console.log(str.toJadenCase("How can mirrors be real if our eyes aren't real"));
+  console.log(String.prototype.toJadenCase("How can mirrors be real if our eyes aren't real"));
 }
