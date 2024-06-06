8f89f38e5b565d9b8f03acb3d50da332e11c8fe4
h4y6 <h4y6@i.ua>
2024-06-06 19:20:15 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -2873,5 +2873,22 @@ If two numbers in the argument array have the same number of digits, return the 
   function findLongest(array) {
     return array.reduce((res, el) => res = (el + '').length > (res + '').length ? el : res, 0);
   }
-  console.log(findLongest([8, 900, 500]));
+  // console.log(findLongest([8, 900, 500]));
+}
+
+{  /**  7 kyu  Sort the Gift Code
+  Happy Holidays fellow Code Warriors!
+Santa's senior gift organizer Elf developed a way to represent up to 26 gifts by assigning a unique alphabetical character to each gift. After each gift was assigned a character, the gift organizer Elf then joined the characters to form the gift ordering code.
+Santa asked his organizer to order the characters in alphabetical order, but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+Sort the Gift Code
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+Examples (Input -- => Output):
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"      */
+
+  function sortGiftCode(code) {
+    return code.split('').sort().join('');
+  }
+  // console.log(sortGiftCode('abcdef'));
 }
