c4878de99f11d605c1a39b23a566998c5d4acedc
h4y6 <h4y6@i.ua>
2024-06-09 20:15:15 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -3036,3 +3036,36 @@ The exponent will never be 1, and neither number will ever be 0  */
   }
   // console.log(derive(7, 9));
 }
+
+{  /**  7 kyu  Check the exa
+  The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+If the score < 0, return 0.
+For example:
+    Correct answer    |    Student's answer   |   Result         
+ ---------------------|-----------------------|-----------
+ ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
+ ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
+ ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
+ ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0            */
+
+  function checkExam(array1, array2) {
+    return Math.max(array2.reduce((res, el, i) => {
+      if (el === "") return res + 0;
+      if (el === array1[i]) return res + 4;
+      if (el !== array1[i]) return res - 1;
+    }, 0), 0);
+  }
+  // function checkExam(array1, array2) {
+  //   return array2.reduce((res, el, i) => {
+  //     if (el === "") return res + 0;
+  //     if (el === array1[i]) return res + 4;
+  //     if (el !== array1[i]) return res - 1;
+  //   }, 0) < 0 ? 0 : array2.reduce((res, el, i) => {
+  //     if (el === "") return res + 0;
+  //     if (el === array1[i]) return res + 4;
+  //     if (el !== array1[i]) return res - 1;
+  //   }, 0);
+  // }
+  console.log(checkExam(["a", "a", "b", "b"], ["", "c", "o", "d"]));
+}
