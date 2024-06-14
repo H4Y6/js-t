371ed5bb49cb957a27423997d7a770145c4367d7
h4y6 <h4y6@i.ua>
2024-06-14 21:58:15 +0300

    odified:   js-06-1-dom/js/01-query-selectors.js

--- js-06-1-dom/js/01-query-selectors.js
+++ js-06-1-dom/js/01-query-selectors.js
@@ -3,9 +3,13 @@ const magicBtn = document.querySelector('.js-magic-btn');
 /* document.querySelector(selector) & document.querySelectorAll(selector) */
 
 const navEl = document.querySelector('.site-nav');
-console.log('navEl', navEl);
+// console.log('navEl', navEl);
 
 const navLinksEl = document.querySelectorAll('.site-nav__link');
-console.log('navLinksEl', navLinksEl);
-
+// console.log('navLinksEl', navLinksEl);
 /* Document.querySelector* & Element.querySelector*  */
+
+magicBtn.addEventListener('click', () => {
+  console.log('navEl', navEl);
+  console.log('navLinksEl', navLinksEl);
+});
