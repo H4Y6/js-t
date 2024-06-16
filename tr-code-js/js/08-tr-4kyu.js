af752f5c027439898a518f3bdc3c92aaf75f91bf
h4y6 <h4y6@i.ua>
2024-06-16 20:31:46 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -3250,3 +3250,18 @@ Invalid arrays
   // console.log(upArray([9, 19]));
   // console.log(upArray([9, -1]));
 }
+
+{  /** 8 kyu  Grasshopper - Messi goals function
+  Messi goals function
+Messi is a soccer player with goals in three leagues:
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+Note: the input will always be valid.
+For example:
+5, 10, 2  -->  17      */
+
+  const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  // console.log(goals(12, 4, 7));
+}
