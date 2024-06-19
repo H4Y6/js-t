3278620fe93de67eb86dce945d2d15e0c4bb8146
h4y6 <h4y6@i.ua>
2024-06-20 01:32:10 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -3450,3 +3450,30 @@ For the sake of simplicity, you can assume that any numbers passed into the func
     // console.log(decode('h2ll4'));
   }
 }
+
+{  /** 8 kyu  Pythagorean Triple
+  Given an unsorted array of 3 positive integers [ n1, n2, n3 ], determine if it is possible to form a Pythagorean Triple using those 3 integers.
+A Pythagorean Triple consists of arranging 3 integers, such that:
+a2 + b2 = c2
+Examples
+[5, 3, 4] : it is possible to form a Pythagorean Triple using these 3 integers: 32 + 42 = 52
+[3, 4, 5] : it is possible to form a Pythagorean Triple using these 3 integers: 32 + 42 = 52
+[13, 12, 5] : it is possible to form a Pythagorean Triple using these 3 integers: 52 + 122 = 132
+[100, 3, 999] : it is NOT possible to form a Pythagorean Triple using these 3 integers - no matter how you arrange them, you will never find a way to satisfy the equation a2 + b2 = c2
+Return Values
+For Python: return True or False
+For JavaScript: return true or false
+Other languages: return 1 or 0 or refer to Sample Tests.    */
+
+  const isPythagoreanTriple = (integers) => {
+    const [x, y, max] = integers.sort((a, b) => a - b);
+    return x ** 2 + y ** 2 === max ** 2;
+  };
+
+  // const isPythagoreanTriple = (integers) => {
+  //   const max = Math.max(...integers);
+  //   const [x, y] = integers.filter(e => e !== max);
+  //   return x ** 2 + y ** 2 === max ** 2;
+  // };
+  // console.log(isPythagoreanTriple([3, 4, 5]));
+}
