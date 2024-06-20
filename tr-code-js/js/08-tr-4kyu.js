badf49a6c1490dd63f13401d2b8c395b5754c2bd
h4y6 <h4y6@i.ua>
2024-06-20 17:06:41 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -3477,3 +3477,41 @@ Other languages: return 1 or 0 or refer to Sample Tests.    */
   // };
   // console.log(isPythagoreanTriple([3, 4, 5]));
 }
+
+{  /** 7 kyu  Automorphic Number (Special Numbers Series #6)
+  Definition
+A number is called Automorphic number if and only if its square ends in the same digits as the number itself.
+Task
+Given a number determine if it Automorphic or not .
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+The number passed to the function is positive
+Input >> Output Examples
+autoMorphic (25) -->> return "Automorphic" 
+Explanation:
+25 squared is 625 , Ends with the same number's digits which are 25 .
+autoMorphic (13) -->> return "Not!!"
+Explanation:
+13 squared is 169 , Not ending with the same number's digits which are 69 .
+autoMorphic (76) -->> return "Automorphic"
+Explanation:
+76 squared is 5776 , Ends with the same number's digits which are 76 .
+autoMorphic (225) -->> return "Not!!"
+Explanation:
+225 squared is 50625 , Not ending with the same number's digits which are 225 .
+autoMorphic (625) -->> return "Automorphic"
+Explanation:
+625 squared is 390625 , Ends with the same number's digits which are 625 .
+autoMorphic (1) -->> return "Automorphic"
+Explanation:
+1 squared is 1 , Ends with the same number's digits which are 1 .
+autoMorphic (6) -->> return "Automorphic"
+Explanation:
+6 squared is 36 , Ends with the same number's digits which are 6    */
+
+  const automorphic = n => RegExp(`${n}$`).test(n * n) ? 'Automorphic' : 'Not!!';
+  // const automorphic = n => `${n * n}`.slice(-('' + n).length) === '' + n ? 'Automorphic' : 'Not!!';
+
+  console.log(automorphic(625));
+};
