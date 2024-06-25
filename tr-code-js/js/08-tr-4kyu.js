187915df04b42b0c659341269ae3c6dd5d427236
h4y6 <h4y6@i.ua>
2024-06-25 21:38:49 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -3700,8 +3700,31 @@ or
 a = ["az", "toto", "picaro", "zone", "kiwi"] -->
 "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"        */
 
-  function partlist(arr) {
-    return arr.map((e, i) => [arr.slice(0, i + 1).join(' '), arr.slice(i + 1).join(' ')]).slice(0, -1);
-  }
+  // function partlist(arr) {
+  //   return arr.map((e, i) => [arr.slice(0, i + 1).join(' '), arr.slice(i + 1).join(' ')]).slice(0, -1);
+  // }
+  const partlist = arr => arr.map((_, i) => [arr.slice(0, i).join(' '), arr.slice(i).join(' ')]).slice(1);
+
   // console.log(partlist(["az", "toto", "picaro", "zone", "kiwi"]));
+}
+
+{ /** 5 kyu  What's a Perfect Power anyway?
+  A perfect power is a classification of positive integers:
+In mathematics, a perfect power is a positive integer that can be expressed as an integer power of another positive integer. More formally, n is a perfect power if there exist natural numbers m > 1, and k > 1 such that mk = n.
+Your task is to check wheter a given integer is a perfect power. If it is a perfect power, return a pair m and k with mk = n as a proof. Otherwise return Nothing, Nil, null, NULL, None or your language's equivalent.
+Note: For a perfect power, there might be several pairs. For example 81 = 3^4 = 9^2, so (3,4) and (9,2) are valid solutions. However, the tests take care of this, so if a number is a perfect power, return any pair that proves it.
+
+Examples
+Test.describe("perfect powers", function(){
+  Test.it("should work for some examples",function(){
+    Test.assertSimilar(isPP(4), [2,2], "4 = 2^2");
+    Test.assertSimilar(isPP(9), [3,2], "9 = 3^2");
+    Test.assertEquals( isPP(5), null, "5 isn't a perfect number");
+  });
+});  */
+
+  const isPP = (n, q = 2) => n ** (1 / q) % 1 === 0 || n ** (1 / q) % 1 > .999999 || n ** (1 / q) % 1 < 10 ** -11
+    ? [(Math.round(n ** (1 / q))), q] : n ** (1 / q) < 2 ? null : isPP(n, q += 1);
+  // console.log(isPP(121));
+  // console.log(isPP(228669389707));
 }
