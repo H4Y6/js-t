8ab97065d9a6b04a70be56e8dabd0290acd23fed
h4y6 <h4y6@i.ua>
2024-06-25 22:54:23 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -3729,3 +3729,13 @@ Test.describe("perfect powers", function(){
   // console.log(isPP(121));
   // console.log(isPP(228669389707));
 }
+
+{  /** 7 kyu  Ordered Count of Characters
+  Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
+Consult the solution set-up for the exact data structure implementation depending on your language.
+Example:
+orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]    */
+
+  const orderedCount = text => Array(...new Set(text)).reduce((res, e) => res = [...res, [e, [...text].filter(el => el === e).length]], []);
+  // console.log(orderedCount('abracadabra'));
+}
