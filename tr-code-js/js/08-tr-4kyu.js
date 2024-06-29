90bc8701c1bdcd714b71731b47b56bcc743f62eb
h4y6 <h4y6@i.ua>
2024-06-29 22:58:29 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -3919,7 +3919,24 @@ movie(100, 10, 0.95) should return 24
 
   const parseF = s => isNaN(parseFloat(s)) ? null : parseFloat(s);
   // const parseF = s => parseFloat(s) === +s ? parseFloat(s) : null;
-  console.log(parseF('js'));
-  console.log(parseF(true));
-  console.log(parseF('3.002'));
+  // console.log(parseF('js'));
+  // console.log(parseF(true));
+  // console.log(parseF('3.002'));
+}
+
+{ /**8 kyu  ES6 string addition 
+  It is easy to join two strings together like this string1 + string2.
+
+Another way to get the desired result would be with string1.concat(string2)
+
+ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+
++ , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!
+
+If one of the arguments is a number your code must coerce it into being a string. */
+
+  function joinStrings(string1, string2) {
+    return `${string1} ${string2}`;
+  }
+  // console.log(joinStrings('aslop', 7));
 }
