782838477688773fdde2f14a2eaaa3894669790a
h4y6 <h4y6@i.ua>
2024-07-01 18:46:11 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4156,3 +4156,24 @@ You must not mutate the original array   */
   // console.log(multiplyAll([1, 2, 3])(4));
   // console.log(multiplyAll([1, 2])(11));
 }
+
+{ /** 6 kyu  Meeting
+  John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.             */
+
+  function meeting(s) {
+    return s.split(';').map(e => e.split(':').reverse().join(' ').toUpperCase()).sort().map(e => e.replace(e, `(${e})`)).join('').replace(/\s/g, ', ');
+  }
+  // console.log(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn"));
+}
