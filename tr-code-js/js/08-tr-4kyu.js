ee5f47599a8d05f25a1f7cb0d417cebf1f4e1e08
h4y6 <h4y6@i.ua>
2024-07-01 15:31:35 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -3999,5 +3999,37 @@ Note2: the input data can be: boolean array, array of objects, array of string a
     return array.join();
     // return array.toString();
   }
-  console.log(printArray(["h", "o", "l", "a"]));
+  // console.log(printArray(["h", "o", "l", "a"]));
+}
+
+{ /** 8 kyu
+  Training JS #3: Basic data types--String  */
+  var a1 = "A", a2 = "a", b1 = "B", b2 = "b", c1 = "C", c2 = "c", d1 = "D", d2 = "d", e1 = "E", e2 = "e", n1 = "N", n2 = "n";
+  function Dad() {
+    return d1 + a2 + d2;
+  }
+  function Bee() {
+    return b1 + e2.repeat(2);
+  }
+  function banana() {
+    return b2 + a2 + (n2 + a2).repeat(2);
+  }
+
+  //answer some questions if you finished works above
+  function answer1() {
+    //the answer should be "yes" or "no"
+    return "no";
+  }
+  function answer2() {
+    //the answer should be "yes" or "no"
+    return "no";
+  }
+  function answer3() {
+    //the answer should be "yes" or "no"
+    return "yes";
+  }
+  console.log(Dad());
+  console.log(Bee());
+  console.log(banana());
+
 }
