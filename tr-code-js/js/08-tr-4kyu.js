f1762d6fbec414943509a449a5b4a8860f2e6019
h4y6 <h4y6@i.ua>
2024-07-02 18:33:50 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4239,3 +4239,16 @@ sum of all digits to the right of the middle digit(s) -> 20
   // console.log(balancedNum(5845));
   // console.log(balancedNum(56239814));
 }
+
+{ /** 8 kyu  Fuel Calculator: Total Cost 
+  In this kata you will have to write a function that takes litres and pricePerLitre (in dollar) as arguments.
+Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total discount per litre cannot be more than 25 cents. Return the total cost rounded to 2 decimal places. Also you can guess that there will not be negative or non-numeric inputs.         */
+
+  function fuelPrice(litres, pricePerLitre) {
+    let discount = ~~(litres / 2) * .05;
+    // let discount = Math.floor(litres / 2) * .05;
+    if (discount > .25) discount = .25;
+    return +((pricePerLitre - discount) * litres).toFixed(2);
+  }
+  console.log(fuelPrice(55, 1.23));
+}
