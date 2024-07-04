3fde288ae5b57444f827adec3b7747252738b0bf
h4y6 <h4y6@i.ua>
2024-07-04 10:05:20 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4352,3 +4352,28 @@ For an example list of 1, 3, 5, 7
   const findAverage = nums => nums.reduce((sum, e) => sum + e, 0) / nums.length;
   // console.log(findAverage([1, 3, 5, 7]));
 }
+
+{ /** 8 kyu  Remove the time
+  You're re-designing a blog, and the blog's posts have the Weekday Month Day, time format for showing the date and time when a post was made, e.g., Friday May 2, 7pm.
+You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+Write a function that takes the website date/time in its original string format and returns the shortened format.
+Input
+Input will always be a string, e.g., "Friday May 2, 7pm". 
+Output
+Output will be the shortened string, e.g., "Friday May 2".   */
+
+  // const shortenToDate = longDate => longDate.split(', ')[0];
+
+  const shortenToDate = longDate => {
+    const [date] = longDate.split(', ');
+    return date;
+  };
+
+  // const shortenToDate = longDate => {
+  //   const [day, month, date] = longDate.split(' ');
+  //   return `${day} ${month} ${date}`.slice(0, -1);;
+  // };
+
+  // const shortenToDate = longDate => longDate.replace(/,\s\w*$/, '');
+  // console.log(shortenToDate("Friday May 2, 9am"));
+}
