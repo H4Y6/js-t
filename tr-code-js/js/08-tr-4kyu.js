5dfca286d1976ae35bc128591bc18fec59b55182
h4y6 <h4y6@i.ua>
2024-07-04 19:07:04 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4451,3 +4451,19 @@ remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"         */
   const remove = (s, n) => [...s].filter(e => e !== '!' || n-- < 1).join('');
   // console.log(remove("Hi!!!", 1));
 }
+
+{ /** 7 kyu
+  V A P O R C O D E 
+  ASC Week 1 Challenge 4 (Medium #1)
+Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+Note that spaces should be ignored in this case.
+Examples
+"Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+"Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"        */
+
+  const vaporcode = string => [...string.match(/[^ ]/g)].join('  ').toUpperCase();
+  // const vaporcode = string => [...string.match(/\S/g)].join('  ').toUpperCase();
+  // const vaporcode = string => string.replaceAll(' ', '').split('').join('  ').toUpperCase();
+  console.log(vaporcode("Lets go to the movies!"));
+
+}
