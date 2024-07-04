2ff26e96eb8504bf11caf87555e3d56c8f0fad74
h4y6 <h4y6@i.ua>
2024-07-04 20:00:37 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4477,3 +4477,22 @@ Calculate the distance between the first and the last pillar in centimeters (wit
   const pillars = (numPill, dist, width) => numPill > 1 ? (numPill - 1) * (dist * 100 + width) - width : 0;
   // console.log(pillars(12, 15, 30));
 }
+
+{ /** 7 kyu  Find the vowels
+  We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+So given a string "super", we should return a list of [2, 4].
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+Vowels in this context refers to: a e i o u y (including upper case)
+This is indexed from [1..n] (not zero indexed!)   */
+
+  // const vowelIndices = word => [...word].reduce((res, e, i) => res = e.match(/[aeiouy]/i) ? [...res, i + 1] : res, []);
+
+  const vowelIndices = (word, res = []) => [...word].map((e, i) => res = e.match(/[aeiouy]/i) ? [...res, i + 1] : res).reverse()[0];
+  console.log(vowelIndices('YoMama'));
+
+}
