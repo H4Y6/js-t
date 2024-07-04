568868f55c55bfeb76985cae77faa4a012dfad7a
h4y6 <h4y6@i.ua>
2024-07-04 09:39:27 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4333,5 +4333,22 @@ The solution would work like the following:
 getEvenNumbers([2,4,5,6]) // should == [2,4,6]  */
 
   const getEvenNumbers = numbersArray => numbersArray.filter(e => !(e % 2));
-  console.log(getEvenNumbers([2, 4, 5, 6]));
+  // console.log(getEvenNumbers([2, 4, 5, 6]));
+}
+
+{ /**8 kyu  Grasshopper - Array Mean
+  To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+
+For an example list of 1, 3, 5, 7
+
+1. Add all of the numbers
+
+1+3+5+7 = 16
+2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+
+16/4 = 4
+3. The mean (or average) of this list is 4      */
+
+  const findAverage = nums => nums.reduce((sum, e) => sum + e, 0) / nums.length;
+  // console.log(findAverage([1, 3, 5, 7]));
 }
