ba5caf6d72d0139c72e13b9ec17b067416ccbefe
h4y6 <h4y6@i.ua>
2024-07-04 15:49:14 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4398,14 +4398,21 @@ var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
 aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
 aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'   */
 
-  function aliasGen(s1, s2) {
-    let firstName = { A: 'Alpha', B: 'Beta', C: 'Cache', D: 'Data', E: 'Energy', F: 'Function', G: 'Glitch', H: 'Half-life', I: 'Ice', J: 'Java', K: 'Keystroke', L: 'Logic', M: 'Malware', N: 'Nagware', O: 'OS', P: 'Phishing', Q: 'Quantum', R: 'RAD', S: 'Strike', T: 'Trojan', U: 'Ultraviolet', V: 'Vanilla', W: 'WiFi', X: 'Xerox', Y: 'Y', Z: 'Zero' };
 
-    let surname = { A: 'Analogue', B: 'Bomb', C: 'Catalyst', D: 'Discharge', E: 'Electron', F: 'Faraday', G: 'Gig', H: 'Hacker', I: 'IP', J: 'Jabber', K: 'Killer', L: 'Lazer', M: 'Mike', N: 'n00b', O: 'Overclock', P: 'Payload', Q: 'Quark', R: 'Roy', S: 'Spy', T: 'T-Rex', U: 'Unit', V: 'Virus', W: 'Worm', X: 'X', Y: 'Yob', Z: 'Zombie' };
+  let firstName = { A: 'Alpha', B: 'Beta', C: 'Cache', D: 'Data', E: 'Energy', F: 'Function', G: 'Glitch', H: 'Half-life', I: 'Ice', J: 'Java', K: 'Keystroke', L: 'Logic', M: 'Malware', N: 'Nagware', O: 'OS', P: 'Phishing', Q: 'Quantum', R: 'RAD', S: 'Strike', T: 'Trojan', U: 'Ultraviolet', V: 'Vanilla', W: 'WiFi', X: 'Xerox', Y: 'Y', Z: 'Zero' };
 
-    s1 = s1[0].toUpperCase();
-    s2 = s2[0].toUpperCase();
-    return /[A-Z]/.test((s1)) && /[A-Z]/.test((s2)) ? firstName[s1] + ' ' + surname[s2] : "Your name must start with a letter from A - Z.";
+  let surname = { A: 'Analogue', B: 'Bomb', C: 'Catalyst', D: 'Discharge', E: 'Electron', F: 'Faraday', G: 'Gig', H: 'Hacker', I: 'IP', J: 'Jabber', K: 'Killer', L: 'Lazer', M: 'Mike', N: 'n00b', O: 'Overclock', P: 'Payload', Q: 'Quark', R: 'Roy', S: 'Spy', T: 'T-Rex', U: 'Unit', V: 'Virus', W: 'Worm', X: 'X', Y: 'Yob', Z: 'Zombie' };
+
+  // function aliasGen(s1, s2) {
+  //   s1 = s1[0].toUpperCase();
+  //   s2 = s2[0].toUpperCase();
+  //   return /[A-Z]/.test((s1)) && /[A-Z]/.test((s2)) ? firstName[s1] + ' ' + surname[s2] : "Your name must start with a letter from A - Z.";
+  // }
+
+  function aliasGen() {
+    const [s1] = arguments[0].toUpperCase();
+    const [s2] = arguments[1].toUpperCase();
+    return /[A-Z]/.test((s1)) && /[A-Z]/.test((s2)) ? `${firstName[s1]} ${surname[s2]}` : "Your name must start with a letter from A - Z.";
   }
   // console.log(aliasGen("Mike", "Millington"));
 }
