6aeb46f41292c4fb2136a7b2a1586f07180880d6
h4y6 <h4y6@i.ua>
2024-07-05 14:33:32 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4504,3 +4504,15 @@ This is indexed from [1..n] (not zero indexed!)   */
   // const cubeOdd = arr => arr.filter(e => isNaN(e)).length ? undefined : arr.filter(e => e % 2).reduce((sum, e) => sum + e ** 3, 0);
   // console.log(cubeOdd([1, 'e', 4, 5, 4, 3]));
 }
+
+{ /** 7 kyu  Simple beads count
+  Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.*/
+
+  const countRedBeads = n => n ? (n - 1) * 2 : 0;
+  // console.log(countRedBeads(-10));
+}
