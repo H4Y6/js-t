f4eb996d6d4fb35740a12a6769b32c902d9eab04
h4y6 <h4y6@i.ua>
2024-07-05 19:25:20 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -4524,3 +4524,18 @@ If there are less than 2 blue beads return 0.*/
   const reverse = str => str.split(' ').map((e, i, ar) => i % 2 ? [...e].reverse().join('') : e).join(' ').trim();
   // console.log(reverse("I really don't like reversing strings!"));
 }
+
+{ /** 7 kyu  Alphabet symmetry
+  Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+Input will consist of alphabet characters, both uppercase and lowercase. No spaces. */
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxwz';
+  const solve = arr => arr.map(e => [...e].filter((el, i, ar) => i === alphabet.indexOf(el.toLowerCase())).length);
+  // const solve = arr => arr.map(e => [...e].reduce((count, el, i, ar) => i === alphabet.indexOf(el.toLowerCase()) ? count + 1 : count, 0));
+  // console.log(solve(["abode", "ABc", "xyzD"]));
+  // console.log(solve(["abide", "ABc", "xyz"]));
+  // console.log(solve(["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"]));
+  // console.log(solve(["encode", "abc", "xyzD", "ABmD"]));
+}
