6391c7969795d62625cc50b4412faee57ce05950
h4y6 <h4y6@i.ua>
2024-07-13 17:34:04 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -5149,5 +5149,20 @@ function should be case insensitive to pass the tests          */
     return /hello|ciao|salut|hallo|hola|ahoj|czesc/i.test(greetings);
     // return /hello|ciao|salut|hallo|hola|ahoj|czesc/.test(greetings.toLowerCase());
   }
-  console.log(validateHello('a Hallo'));
+  // console.log(validateHello('a Hallo'));
+}
+
+{ /** 8 kyu  Leonardo Dicaprio and Oscars 
+  You have to write a function that describe Leo:
+
+function leo(oscar) {
+
+}
+if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+if it was over 88 you should return "Leo got one already!"           */
+
+  const leo = oscar => oscar === 88 ? "Leo finally won the oscar! Leo is happy" : oscar === 86 ? "Not even for Wolf of wallstreet?!" : oscar < 88 ? "When will you give Leo an Oscar?" : "Leo got one already!";
+  // console.log(leo(62)); "Leo finally won the oscar! Leo is happy";
 }
