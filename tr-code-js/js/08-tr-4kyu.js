e728fd8d9dde8c5a5e2c8086341aeedffbe16089
h4y6 <h4y6@i.ua>
2024-07-14 18:15:14 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -5191,3 +5191,22 @@ All inputs will be valid.     / failed */
   // console.log(last('man i need a taxi up to ubud'));
   // console.log(last('what time are we climbing up the volcano'));
 }
+
+{ /** 7 kyu  Deodorant Evaporator 
+  We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+The program reports the nth day (as an integer) on which the evaporator will be out of use.
+Example:
+evaporator(10, 10, 5) -> 29
+Note:
+Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.     */
+
+  function evaporator(content, evap_per_day, threshold, duration = 0) {
+    const mlThreshold = content / 100 * threshold;
+    while (content >= mlThreshold) {
+      content -= content / 100 * evap_per_day;
+      duration++;
+    }
+    return duration;
+  }
+  // console.log(evaporator(10, 10, 5));
+}
