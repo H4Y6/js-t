e3a11ad5482e52120b7197b28bbc804d7a083ae8
h4y6 <h4y6@i.ua>
2024-07-15 20:37:07 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -5210,3 +5210,42 @@ Content is in fact not necessary in the body of the function "evaporator", you c
   }
   // console.log(evaporator(10, 10, 5));
 }
+
+{ /** 6 kyu   +1 Array
+  Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+the array can't be empty
+only non-negative, single digit integers are allowed
+Return nil (or your language's equivalent) for invalid inputs.
+Examples
+Valid arrays
+[4, 3, 2, 5] would return [4, 3, 2, 6]
+[1, 2, 3, 9] would return [1, 2, 4, 0]
+[9, 9, 9, 9] would return [1, 0, 0, 0, 0]
+[0, 1, 3, 7] would return [0, 1, 3, 8]
+Invalid arrays
+[1, -9] is invalid because -9 is not a non-negative integer
+[1, 2, 33] is invalid because 33 is not a single-digit integer          */
+
+  const upArray = arr => {
+    if (arr.some(e => ('' + e).length > 1 || e < 0) || arr.length === 0) return null;
+    let end = arr.map(e => '' + e).join('').match(/[9]+$/);
+
+    if (end === null) {
+      arr[arr.length - 1] += 1;
+      return arr;
+    }
+    const endLength = end[0].length;
+    end = Array(endLength).fill(0);
+    const start = arr.slice(0, - endLength);
+
+    if (start.length === 0) return [1, ...end];
+
+    start[start.length - 1] += 1;
+    return [...start, ...end];
+  };
+
+  // console.log(upArray([0, 0, 9, 9, 9, 9, 9]));
+  // console.log(upArray([2, 3, 9, 9]));
+  // console.log(upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+  // console.log(upArray([0, 2, 6, 3, 7, 2, 1, 6, 0, 3, 3, 4, 3, 7, 9, 0, 4, 8, 2, 8, 2]));
+}
