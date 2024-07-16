4c69cf669c7c63a6b6e3dc3be962f4d43267a550
h4y6 <h4y6@i.ua>
2024-07-16 22:53:11 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -5378,3 +5378,13 @@ Length should be between 4 and 16 characters (both included).   */
   const validateUsr = username => /^[a-z\d_]{4,16}$/.test(username);
   // console.log(validateUsr('jrs5tp_'));
 }
+
+{ /** 7 kyu  Digits explosion 
+  Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+Examples
+"312" should return "333122"
+"102269" should return "12222666666999999999"   */
+
+  const explode = s => s.split('').map(e => e.repeat(+e)).join('');
+  console.log(explode('102269'));
+}
