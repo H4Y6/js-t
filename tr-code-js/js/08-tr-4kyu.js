9a35318b2c3c71d2cccb87ff42371067ec3d6512
h4y6 <h4y6@i.ua>
2024-07-16 14:11:00 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -5262,8 +5262,19 @@ Invalid arrays
     return [...start, ...end];
   };
 
-  console.log(upArray([0, 0, 9, 9, 9, 9, 9]));
-  console.log(upArray([2, 3, 9, 9]));
-  console.log(upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
-  console.log(upArray([0, 2, 6, 3, 7, 2, 1, 6, 0, 3, 3, 4, 3, 7, 9, 0, 4, 8, 2, 8, 2]));
+  // console.log(upArray([0, 0, 9, 9, 9, 9, 9]));
+  // console.log(upArray([2, 3, 9, 9]));
+  // console.log(upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+  // console.log(upArray([0, 2, 6, 3, 7, 2, 1, 6, 0, 3, 3, 4, 3, 7, 9, 0, 4, 8, 2, 8, 2]));
+}
+
+{ /** 8 kyu  How old will I be in 2099? 
+  Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+"..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.*/
+
+  const calculateAge = (born, curr) => curr - born > 1 ? `You are ${curr - born} years old.` : curr - 1 === born ? `You are ${curr - born} year old.`
+    : curr === born ? "You were born this very year!" : born - curr === 1 ? `You will be born in ${born - curr} year.` : `You will be born in ${born - curr} years.`;
+  // console.log(calculateAge(1989, 1989));
 }
