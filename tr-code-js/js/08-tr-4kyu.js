6607c7ce97aad84f1f42b6eeb219e43040caa539
h4y6 <h4y6@i.ua>
2024-07-19 22:46:44 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -5514,3 +5514,39 @@ abbreviate("elephant-rides are really fun!")
   // console.log(abbreviate('internationalization'));
   // console.log(abbreviate('You need, need not want, to complete this code-wars mission'));
 }
+
+{ /** 7 kyu  Basic Sequence Practice
+  A sequence or a series, in mathematics, is a string of objects, like numbers, that follow a particular pattern. The individual elements in a sequence are called terms. A simple example is 3, 6, 9, 12, 15, 18, 21, ..., where the pattern is: "add 3 to the previous term".
+In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28, ... This sequence is generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
+[ 0,  1,    3,      6,   ...]
+  0  0+1  0+1+2  0+1+2+3
+Your Task
+Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic series explained above. Whenn < 0 return the sequence with negative terms.
+Examples
+ 5  -->  [0,  1,  3,  6,  10,  15]
+-5  -->  [0, -1, -3, -6, -10, -15]
+ 7  -->  [0,  1,  3,  6,  10,  15,  21,  28]      */
+
+  // const sumOfN = n => {
+  //   let arr = [];
+  //   if (!n) [0];
+  //   if (n > 0) {
+  //     for (let i = 0, res = 0; i <= n; i++) {
+  //       arr = [...arr, res += i];
+  //     }
+  //   }
+  //   for (let i = 0, res = 0; i >= n; i--) {
+  //     arr = [...arr, res += i];
+  //   }
+  //   return arr;
+  // };
+
+  const sumOfN = n => {
+    let arr = [0];
+    for (let i = 1, res = 0; i <= Math.abs(n); i++) {
+      arr = [...arr, res += i];
+    }
+    return n < 0 ? arr.map(e => -e) : arr;
+  };
+  // console.log(sumOfN(3));
+}
