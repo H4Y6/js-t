7d6e560c39a4dbe667ce9b62ae589f73e5629c59
h4y6 <h4y6@i.ua>
2024-07-20 15:53:01 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -5597,3 +5597,41 @@ Since , 51 + 62 + 43 = 105 != 564 , thus output is "Not !!"         */
   const disariumNumber = n => (n + '').split('').reduce((sum, e, i) => sum + e ** (i + 1), 0) === n ? "Disarium !!" : "Not !!";
   // console.log(disariumNumber(879));
 }
+
+{ /**7 kyu  Jumping Number
+  Jumping number is the number that All adjacent digits in it differ by 1.
+Task
+Given a number, Find if it is Jumping or not .
+Notes
+Number passed is always Positive .
+Return the result as String .
+The difference between ‘9’ and ‘0’ is not considered as 1 .
+All single digit numbers are considered as Jumping numbers.
+Input >> Output Examples
+jumpingNumber(9) ==> return "Jumping!!"
+Explanation:
+It's single-digit number
+jumpingNumber(79) ==> return "Not!!"
+Explanation:
+Adjacent digits don't differ by 1
+jumpingNumber(23) ==> return "Jumping!!"
+Explanation:
+Adjacent digits differ by 1
+jumpingNumber(556847) ==> return "Not!!"
+Explanation:
+Adjacent digits don't differ by 1
+jumpingNumber(4343456) ==> return "Jumping!!"
+Explanation:
+Adjacent digits differ by 1
+jumpingNumber(89098) ==> return "Not!!"
+Explanation:
+Adjacent digits don't differ by 1
+jumpingNumber(32) ==> return "Jumping!!"
+Explanation:
+Adjacent digits differ by 1 */
+
+  const jumpingNumber = n => (n + '').length === 1 ? "Jumping!!"
+    : (n + '').split('').map((e, i, ar) => Math.abs(ar[i + 1] - e)).slice(0, -1).every(e => e === 1) ? "Jumping!!" : "Not!!";
+  console.log(jumpingNumber(98789876));
+  console.log(jumpingNumber(6));
+}
