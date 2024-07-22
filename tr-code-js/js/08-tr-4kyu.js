4fdda254cacb605cd1b3b79de9166d20227000c1
h4y6 <h4y6@i.ua>
2024-07-22 19:34:20 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -5796,5 +5796,19 @@ The number's digits {2 , 3, 3, 5} are in non-Decreasing Order , Note 3 <= 3     
   //   }
   //   return res;
   // }
-  console.log(numPrimorial(3));
+
+  // console.log(numPrimorial(3));
+}
+
+{ /** 7 kyu
+  Convert an array of strings to array of numbers
+  Oh no!
+  Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+    You need to cast the whole array to the correct type.
+    Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+    ie:["1", "2", "3"] to [1, 2, 3]
+    Note that you can receive floats as well.   */
+
+  const toNumberArray = stringArray => stringArray.map(e => +e);
+  console.log(toNumberArray(["1.1", "2.2", "3.3"]));
 }
