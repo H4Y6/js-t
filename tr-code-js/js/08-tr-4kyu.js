b766b400f2717bbbdcf37335e9ddc98bea3296bb
h4y6 <h4y6@i.ua>
2024-07-27 13:48:54 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6181,3 +6181,84 @@ Your function should be case insensitive to the vowels. */
   }
   // console.log(vowel2index('Codewars is the best site in the world'));
 }
+
+{ /** 5 kyu  Best travel 
+  John and Mary want to travel between a few towns A, B, C ... Mary has on a sheet of paper a list of distances between these towns. ls = [50, 55, 57, 58, 60]. John is tired of driving and he says to Mary that he doesn't want to drive more than t = 174 miles and he will visit only 3 towns.
+Which distances, hence which towns, they will choose so that the sum of the distances is the biggest possible to please Mary and John?
+Example:
+With list ls and 3 towns to visit they can make a choice between: [50,55,57],[50,55,58],[50,55,60],[50,57,58],[50,57,60],[50,58,60],[55,57,58],[55,57,60],[55,58,60],[57,58,60].
+The sums of distances are then: 162, 163, 165, 165, 167, 168, 170, 172, 173, 175.
+The biggest possible sum taking a limit of 174 into account is then 173 and the distances of the 3 corresponding towns is [55, 58, 60].
+The function chooseBestSum (or choose_best_sum or ... depending on the language) will take as parameters t (maximum sum of distances, integer >= 0), k (number of towns to visit, k >= 1) and ls (list of distances, all distances are positive or zero integers and this list has at least one element). The function returns the "best" sum ie the biggest possible sum of k distances less than or equal to the given limit t, if that sum exists, or otherwise nil, null, None, Nothing, depending on the language. In that case with C, C++, D, Dart, Fortran, F#, Go, Julia, Kotlin, Nim, OCaml, Pascal, Perl, PowerShell, Reason, Rust, Scala, Shell, Swift return -1.
+Examples:
+ts = [50, 55, 56, 57, 58] choose_best_sum(163, 3, ts) -> 163
+xs = [50] choose_best_sum(163, 3, xs) -> nil (or null or ... or -1 (C++, C, D, Rust, Swift, Go, ...)
+ys = [91, 74, 73, 85, 73, 81, 87] choose_best_sum(230, 3, ys) -> 228
+Notes:
+try not to modify the input list of distances ls    */
+
+  function chooseBestSum(t, k, ls) {
+    if (k > ls.length) return null;
+    const arr = [];
+
+    for (let i = 0; i < ls.length; i++) {
+      let sum = 0;
+      if (k === 1) {
+        sum += ls[i];
+        if (sum <= t) arr.push(sum);
+      }
+
+      for (let j = 0; j < ls.length; j++) {
+        if (j === i) { continue; }
+        if (k === 2) {
+          sum = ls[i] + ls[j];
+          if (sum <= t) arr.push(sum);
+        };
+
+        for (let m = 0; m < ls.length; m++) {
+          if (m === i || m === j) { continue; }
+          if (k === 3) {
+            sum = ls[i] + ls[j] + ls[m];
+            if (sum <= t) arr.push(sum);
+          };
+
+          for (let n = 0; n < ls.length; n++) {
+            if (n === i || n === j || n === m) { continue; }
+            if (k === 4) {
+              sum = ls[i] + ls[j] + ls[m] + ls[n];
+              if (sum <= t) arr.push(sum);
+            };
+
+
+            for (let p = 0; p < ls.length; p++) {
+              if (p === i || p === j || p === m || p === n) { continue; }
+              if (k === 5) {
+                sum = ls[i] + ls[j] + ls[m] + ls[n] + ls[p];
+                if (sum <= t) arr.push(sum);
+              };
+
+              for (let q = 0; q < ls.length; q++) {
+                if (q === i || q === j || q === m || q === n || q === p) { continue; }
+                if (k === 6) {
+                  sum = ls[i] + ls[j] + ls[m] + ls[n] + ls[p] + ls[q];
+                  if (sum <= t) arr.push(sum);
+                };
+              }
+            }
+          }
+        }
+      }
+    }
+    return arr.length ? Math.max(...arr) : null;
+  }
+
+
+  // console.log(chooseBestSum(163, 3, [50, 55, 56, 57, 58]));
+  // console.log(chooseBestSum(163, 3, [50]));
+  // console.log(chooseBestSum(430, 5, [100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34, 89]));  /** Maximum call stack size exceeded */
+  // console.log(chooseBestSum(700, 6, [91, 74, 73, 85, 73, 81, 87]));
+  // console.log(chooseBestSum(331, 4, [91, 74, 73, 85, 73, 81, 87]));
+  // console.log(chooseBestSum(331, 5, [91, 74, 73, 85, 73, 81, 87]));
+  // console.log(chooseBestSum(331, 2, [91, 74, 73, 85, 73, 81, 87]));
+  // console.log(chooseBestSum(230, 3, [91, 74, 73, 85, 73, 81, 87]));
+}
