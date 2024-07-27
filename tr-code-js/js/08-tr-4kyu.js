a619b84224d8c40e1c9a36c24613dbd546dd8fc1
h4y6 <h4y6@i.ua>
2024-07-27 17:11:34 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6318,3 +6318,21 @@ Your function should return the 20 year estimate of the stairs climbed using the
 
   // console.log(stairsIn20(stairs));
 }
+
+{ /** 7 kyu  Word values
+  Given a string "abc" and assuming that each letter in the string has a value equal to its position in the alphabet, our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ....z = 26.
+
+You will be given a list of strings and your task will be to return the values of the strings as explained above multiplied by the position of that string in the list. For our purpose, position begins with 1.
+
+nameValue ["abc","abc abc"] should return [6,24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
+
+"abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied by 1 while the value at position 2 is multiplied by 2.
+
+Input will only contain lowercase characters and spaces.  */
+
+  function wordValue(a) {
+    const alph = 'abcdefghijklmnopqrstuvwxyz';
+    return a.map((e, i) => e.split('').map(el => alph.indexOf(el) + 1).reduce((sum, e) => sum + e, 0) * (i + 1));
+  }
+  console.log(wordValue(["codewars", "abc", "xyz"]));
+}
