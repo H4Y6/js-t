eb5ac9940bbdb5f88844058e7cf761765f25a100
h4y6 <h4y6@i.ua>
2024-07-29 12:49:15 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6442,6 +6442,14 @@ Encode("masterpiece",1939);  ==>  [ 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8] */
     // return [...str].map((e, i) => e.charCodeAt() - 96 + +('' + n)[i % ('' + n).length]);
     return Array.from(str, (e, i) => e.charCodeAt() - 96 + +('' + n)[i % ('' + n).length]);
   }
-  console.log(encode("masterpiece", 1939));
-  'abcdefghijklmnopqrstuvwxyz';
+  // console.log(encode("masterpiece", 1939));
+}
+
+{ /** 7 kyu  Sum of integers in string
+  Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+Note: only positive integers will be tested.    */
+
+  const sumOfIntegersInString = s => s.split(/\D/).reduce((sum, e) => sum + +e, 0);
+  console.log(sumOfIntegersInString('The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog'));
 }
