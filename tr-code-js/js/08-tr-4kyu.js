116ec5375c1f644252fecdbc53092650dde01987
h4y6 <h4y6@i.ua>
2024-07-29 12:09:08 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6437,7 +6437,8 @@ Encode("scout",1939);  ==>  [ 20, 12, 18, 30, 21]
 Encode("masterpiece",1939);  ==>  [ 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8] */
 
   function encode(str, n) {
-    return [...str].map((e, i) => 'abcdefghijklmnopqrstuvwxyz'.indexOf(e) + +n.toString()[i % n.toString().length] + 1);
+    // return [...str].map((e, i) => 'abcdefghijklmnopqrstuvwxyz'.indexOf(e) + +n.toString()[i % n.toString().length] + 1);
+    return [...str].map((e, i) => e.charCodeAt() - 96 + +n.toString()[i % n.toString().length]);
   }
   console.log(encode("masterpiece", 1939));
   'abcdefghijklmnopqrstuvwxyz';
