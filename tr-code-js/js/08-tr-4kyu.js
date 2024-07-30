47cfe35a5299e65714900026f11c06f17aeccc2c
h4y6 <h4y6@i.ua>
2024-07-30 13:46:20 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6547,5 +6547,13 @@ unluckyDays(1986) == 1     */
   // }
 
   const unluckyDays = year => [...Array(12)].map((e, i) => new Date(year, i, 13)).reduce((count, e) => count + (e.getDay() === 5), 0);
-  console.log(unluckyDays(2015));
+  // console.log(unluckyDays(2015));
+}
+
+{ /** 7 kyu  Numbers to Letters
+  Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+All inputs will be valid.   */
+
+  const switcher = x => x.map(e => ' zyxwvutsrqponmlkjihgfedcba!? '[e]).join('');
+  console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8']));
 }
