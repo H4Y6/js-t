656fe3cbbade9bc248b9802350764ca972bc2969
h4y6 <h4y6@i.ua>
2024-07-30 19:59:06 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6627,3 +6627,21 @@ mineLocation( [ [0, 0, 0], [0, 0, 0], [0, 1, 0] ] ) => returns [2, 1]      */
   // console.log(mineLocation([[1, 0], [0, 0]]));
   // console.log(mineLocation([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]));
 }
+
+{ /** 6 kyu  Highest Scoring Word
+  Given a string of words, you need to find the highest scoring word.
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
+You need to return the highest scoring word as a string.
+If two words score the same, return the word that appears earliest in the original string.
+All letters will be lowercase and all inputs will be valid.  */
+
+  const high = x => {
+    const sumArr = x.split(' ').map(e => [...e].reduce((sum, e) => sum + ' abcdefghijklmnopqrstuvwxyz'.indexOf(e), 0));
+    const max = Math.max(...sumArr);
+    const i = sumArr.indexOf(max);
+    return x.split(' ')[i];
+  };
+  // console.log(high('man i need a taxi up to ubud'));
+  // console.log(high("blccpyikz peomzxjse ndhdidbuf tgdworlty"));
+}
