669ae859eb27f582e196c77865b55426d3921b29
h4y6 <h4y6@i.ua>
2024-07-30 18:10:23 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6572,3 +6572,40 @@ More in this series:  */
   const meeting = (x, room = x.indexOf('O')) => room >= 0 ? room : 'None available!';
   // console.log(meeting(['X', 'X', 'X']));
 }
+
+{ /** 6 kyu  Find Cracker.
+   Someone was hacking the score. Each student's record is given as an array The objects in the array are given again as an array of scores for each name and total score. ex> */
+  var array = [
+    ["name1", 445, ["B", "A", "A", "C", "A", "A"]],
+    ["name2", 110, ["B", "A", "A", "A"]],
+    ["name3", 200, ["B", "A", "A", "C"]],
+    ["name4", 200, ["A", "A", "A", "A", "A", "A", "A"]]
+  ];
+
+  /* The scores for each grade is:
+    A: 30 points
+  B: 20 points
+  C: 10 points
+  D: 5 points
+  Everything else: 0 points
+  If there are 5 or more courses and all courses has a grade of B or above, additional 20 points are awarded. After all the calculations, the total score should be capped at 200 points.
+    Returns the name of the hacked name as an array when scoring with this rule.*/
+  var array = [
+    ["name1", 445, ["B", "A", "A", "C", "A", "A", "T"]],     // name1 total point is over 200 => hacked
+    ["name2", 110, ["B", "A", "A", "A"]],               // name2 point is right
+    ["name3", 200, ["B", "A", "A", "C"]],               // name3 point is 200 but real point is 90 => hacked
+
+    ["name4", 200, ["A", "A", "A", "A", "A", "A", "A"]] // name4 point is right
+  ];
+  // return ["name1", "name3"]; 
+
+  function findHack(arr) {
+    const scores = { A: 30, B: 20, C: 10, D: 5 };
+
+    return arr.map((e, i, ar) =>
+      e[2].reduce((sum, e) => {
+        return sum + (scores[e] ? scores[e] : 0);
+      }, e[2].filter(e => scores[e] >= 20).length > 4 ? + 20 : +0) < e[1] ? e[0] : "").filter(e => e);
+  }
+  console.log(findHack(array));
+}
