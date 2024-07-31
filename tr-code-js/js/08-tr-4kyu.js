83cc6cf1200abf71835ad7cbc7b4d3aa5e7f1639
h4y6 <h4y6@i.ua>
2024-07-31 23:56:29 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6771,3 +6771,18 @@ Maximum sum after some operations(modulo 1 000 000 007)     */
   //   ['XXXXXXXX', 7]
   // ], 2));
 }
+
+{ /** 6 kyu  String transformer 
+  Given a string, return a new string that has transformed based on the input:
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+For example:
+"Example Input" ==> "iNPUT eXAMPLE"
+You may assume the input only contain English alphabet and spaces.   */
+
+  function stringTransformer(str) {
+    return str.split(' ').reverse().map(e => e.split('').map(e => e === e.toLowerCase() ? e.toUpperCase() : e.toLowerCase())).map(e => e.join('')).join(' ');
+  }
+  // console.log(stringTransformer("Example Input"));
+}
