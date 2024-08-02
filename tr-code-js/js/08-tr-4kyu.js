ee2f6a80e0925d8ea19825bc454c026229aa8ed5
h4y6 <h4y6@i.ua>
2024-08-02 21:57:04 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -6930,5 +6930,23 @@ All inputs will be in the correct format. Strings on tickets are not always the 
   // console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2));
   // console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1));
   // console.log(bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3));
-  console.log(bingo([['QM', 77], ['OC', 76], ['FUWLEDKQ', 70], ['NC', 76], ['ESFBWGH', 69], ['IQN', 81]], 2));
+  // console.log(bingo([['QM', 77], ['OC', 76], ['FUWLEDKQ', 70], ['NC', 76], ['ESFBWGH', 69], ['IQN', 81]], 2));
+}
+
+{ /** 8 kyu  Age Range Compatibility Equation
+  Everybody knows the classic "half your age plus seven" dating rule that a lot of people follow (including myself). It's the 'recommended' age range in which to date someone.
+minimum age <= your age <= maximum age
+Task
+Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.
+This equation doesn't work when the age <= 14, so use this equation instead:
+min = age - 0.10 * age
+max = age + 0.10 * age
+You should floor all your answers so that an integer is given instead of a float (which doesn't represent age). Return your answer in the form [min]-[max]
+##Examples:
+age = 27   =>   20-40
+age = 5    =>   4-5
+age = 17   =>   15-20       */
+
+  const datingRange = age => age > 14 ? [~~(age / 2 + 7), ~~(age * 2 - 14)].join('-') : [~~(.9 * age), ~~(1.1 * age)].join('-');
+  console.log(datingRange(15));
 }
