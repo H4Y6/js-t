2501bd21efdf4fda46eacef0db38118261675060
h4y6 <h4y6@i.ua>
2024-08-04 12:11:38 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -7140,3 +7140,16 @@ if the given string is "" you will return "" */
   // console.log(sumPairs([1, 4, 8, 7, 3, 15], 8));
   // console.log(sumPairs([20, -13, 40], -7));
 }
+
+{ /** 8 kyu  Return Two Highest Values in List
+  In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+The result should also be ordered from highest to lowest.
+Examples:
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  []              */
+
+  const twoHighest = arr => [...new Set(arr)].sort((a, b) => b - a).slice(0, 2);
+  console.log(twoHighest([4, 10, 10, 9]));
+  console.log(twoHighest([1, 1, 1]));
+}
