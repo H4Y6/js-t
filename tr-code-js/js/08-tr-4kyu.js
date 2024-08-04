7040836145f9cb69cfc88a0f78fd4899b8e37409
h4y6 <h4y6@i.ua>
2024-08-05 00:37:41 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -7214,3 +7214,18 @@ DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted 
   // console.log(discoverOriginalPrice(373.85, 11.2));
   // console.log(discoverOriginalPrice(458.2, 17.13));
 }
+
+{ /** 8 kyu  Take an Arrow to the knee, Functionally
+  Arrow style Functions
+Come here to practice the Arrow style functions Not much else to say good luck!
+Details
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+Examples
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a  */
+
+  const arrowFunc = function (arr) {
+    return arr.map(e => String.fromCharCode(e)).join('');
+  };
+  // console.log(arrowFunc([84, 101, 115, 116]));
+}
