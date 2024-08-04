402e988327e551a4a760bf2292a0b93c2c223f32
h4y6 <h4y6@i.ua>
2024-08-04 15:11:47 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -7150,6 +7150,23 @@ Examples:
 []  =>  []              */
 
   const twoHighest = arr => [...new Set(arr)].sort((a, b) => b - a).slice(0, 2);
-  console.log(twoHighest([4, 10, 10, 9]));
-  console.log(twoHighest([1, 1, 1]));
+  // console.log(twoHighest([4, 10, 10, 9]));
+  // console.log(twoHighest([1, 1, 1]));
+}
+
+{ /** 7 kyu  Sort by Last Char 
+  Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+All inputs will be valid.         */
+
+  const last = x => x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+  // const last = x => x.split(" ").sort((a, b) => a.slice(-1) === b.slice(-1) ? 0 : a.slice(-1) > b.slice(-1) ? 1 : -1);
+  // const last = x => x.split(' ').sort((a, b) => a[a.length - 1] === b[b.length - 1] ? 0 : a[a.length - 1] > b[b.length - 1] ? 1 : -1);
+  // const last = x => x.split(' ').sort((a, b) => a[a.length - 1] === b[b.length - 1] ? 0 : a[a.length - 1] > b[b.length - 1] ? 1 : -1);
+  // const last = x => x.split(' ').sort((a, b) => a[a.length - 1] === b[b.length - 1] ? 0 : a[a.length - 1] < b[b.length - 1] ? -1 : 1);
+
+  console.log(last('man i need a taxi up to ubud'));
+  console.log(last('take me to semynak'));
+  console.log(last('what time are we climbing up the volcano'));
+  // const ss = str.split(' ').map(e => e.replace(/(\d+)\|(\d+)\|(\d+)\,*/, (_, h, m, s) => h * 3600 + m * 60 + +s));
 }
