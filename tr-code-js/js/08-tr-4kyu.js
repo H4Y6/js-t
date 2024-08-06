1d57fa4171179d73d9ff79a460731d3e79e548f6
h4y6 <h4y6@i.ua>
2024-08-06 15:07:44 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -7490,3 +7490,40 @@ largest(2, [7,6,5,4,3,2,1])
   }
   // console.log(largest(0, [-3, -2, -1, 0, -9, -8, -7, -6, -4, -5]));
 }
+
+{ /** 7 kyu  Head, Tail, Init and Last
+  $ ghci
+GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+λ head [1,2,3,4,5]
+1
+λ tail [1,2,3,4,5]
+[2,3,4,5]
+λ init [1,2,3,4,5]
+[1,2,3,4]
+λ last [1,2,3,4,5]
+5
+Your job is to implement these functions in your given language. Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
+
+| HEAD | <----------- TAIL ------------> |
+[  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+| <----------- INIT ------------> | LAST |
+
+head [x] = x
+tail [x] = []
+init [x] = []
+last [x] = x
+Here's how I expect the functions to be called in your language:
+
+head([1,2,3,4,5]); => 1
+tail([1,2,3,4,5]); => [2,3,4,5] */
+
+  const head = x => x.slice(0, 1)[0];
+  const tail = x => x.slice(1);
+  const init = x => x.slice(0, x.length - 1);
+  const last = x => x.slice(-1)[0];
+
+  // console.log(head([1, 2, 3, 4, 5, 6, 7]));
+  // console.log(tail([1, 2, 3, 4, 5, 6, 7]));
+  // console.log(init([1, 2, 3, 4, 5, 6, 7]));
+  // console.log(last([1, 2, 3, 4, 5, 6, 7]));
+}
