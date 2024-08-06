f0c3e357383f5a9c16bece1713d28f2a7e09d982
h4y6 <h4y6@i.ua>
2024-08-06 17:36:55 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js,  tr-code-js/reg-exp.txt

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -7552,5 +7552,25 @@ Strings will only contain letters and spaces, with exactly 1 space between words
   // const makeString = s => s.split(' ').map(e => e.replace(/(^\w)(\w*)/g, (e, a, b) => a)).join('');
   const makeString = s => s.match(/\b\w/g).join('');
 
-  console.log(makeString("kaks de gan has a big head"));
+  // console.log(makeString("kaks de gan has a big head"));
+}
+
+{ /** 7 kyu  Split In Parts 
+  The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+Example:
+Split the below string into other strings of size #3
+'supercalifragilisticexpialidocious'
+Will return a new string
+'sup erc ali fra gil ist ice xpi ali doc iou s'
+Assumptions:
+String length is always greater than 0
+String has no spaces
+Size is always positive */
+
+  const splitInParts = function (s, partLength) {
+    const regex = new RegExp(`\\w{${partLength}}`, 'g');
+    return s.match(regex).join(' ') + (s.length % partLength ? ' ' + s.slice(-s.length % partLength) : '');
+  };
+  console.log(splitInParts('supercalifragilisticexpialidocious', 3));
+  console.log(splitInParts('HelloKata', 1));
 }
