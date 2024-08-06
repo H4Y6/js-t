168406a3e9722790b62a813fbf93848f6af7f488
h4y6 <h4y6@i.ua>
2024-08-06 12:18:37 +0300

    modified:   tr-code-js/js/08-tr-4kyu.js

--- tr-code-js/js/08-tr-4kyu.js
+++ tr-code-js/js/08-tr-4kyu.js
@@ -7386,3 +7386,32 @@ Note 2: do not modify the input array.      */
 
   // console.log(beggars([1, 2, 3, 4, 5], 3));
 }
+
+{ /** 6 kyu  The Deaf Rats of Hamelin
+  The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
+
+But some of the rats are deaf and are going the wrong way!
+
+Kata Task
+How many deaf rats are there?
+Legend
+P = The Pied Piper
+O~ = Rat going left
+~O = Rat going right
+Example
+ex1 ~O~O~O~O P has 0 deaf rats
+ex2 P O~ O~ ~O O~ has 1 deaf rat
+ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats */
+
+  const findRightPP = town => [...[town.split('P')[0]].join('').replaceAll(' ', '')].filter((e, i) => i % 2 ? e === '~' : '').length;
+  const findLeftPP = town => [...[town.split('P')[1]].join('').replaceAll(' ', '')].filter((e, i) => i % 2 ? e === 'O' : '').length;
+
+  const countDeafRats = function (town) {
+    const i = town.indexOf('P');
+    return !i ? findLeftPP(town) : i === town.length - 1 ? findRightPP(town) : findRightPP(town) + findLeftPP(town);
+  };
+
+  console.log(countDeafRats("P O~ O~ ~O O~"));
+  console.log(countDeafRats("~O~O~O~O P"));
+  console.log(countDeafRats("~O~O~O~OP~O~OO~"));
+}
