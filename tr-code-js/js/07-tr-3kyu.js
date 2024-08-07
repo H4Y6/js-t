3a8c492aba7d2feecf1066984e3dce8615596ed1
h4y6 <h4y6@i.ua>
2024-08-07 16:59:12 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -98,3 +98,15 @@ uniTotal("aaa") == 291*/
   }
   // console.log(uniTotal('aaaa'));
 }
+{/**7 kyuChanging letters
+  When provided with a String, capitalize all vowels
+For example:
+Input : "Hello World!"
+Output : "HEllO WOrld!"
+Note: Y is not a vowel in this kata. */
+
+  function swap(string) {
+    return string.replace(/[aeiou]/g, e => e.toUpperCase());
+  }
+  // console.log(swap("Hello World!"));
+}
