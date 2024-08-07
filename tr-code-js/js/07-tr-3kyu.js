c2b873cf8ee2d8a7b7f08b387d0cb283e821ee8c
h4y6 <h4y6@i.ua>
2024-08-07 16:53:08 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -87,3 +87,14 @@ movie(100, 10, 0.95) should return 24
   };
   // console.log(movie(100, 10, 0.95));
 }
+{ /** 8 kyu  ASCII Total
+  You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+Examples:
+uniTotal("a") == 97
+uniTotal("aaa") == 291*/
+
+  function uniTotal(string) {
+    return string.split('').reduce((total, e) => total + e.charCodeAt(), 0);
+  }
+  // console.log(uniTotal('aaaa'));
+}
