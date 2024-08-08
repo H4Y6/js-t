a9c1b55ca9d56f7e2d4d0e58632f68859b904d4d
h4y6 <h4y6@i.ua>
2024-08-08 23:04:10 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -186,3 +186,27 @@ In Fortran - as in any other language - the returned string is not permitted to 
   // console.log(decomp(14));
   // console.log(decomp(25));
 }
+{/** 8 kyu  Barking mad
+  Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined
+Use method prototypes to enable all Dogs to bark.*/
+
+  function Dog(breed) {
+    this.breed = breed;
+  }
+
+  Dog.prototype.bark = function () {
+    return 'Woof';
+  };
+
+  var snoopy = new Dog("Beagle");
+
+  snoopy.bark = function () {
+    return "Woof";
+  };
+
+  var scoobydoo = new Dog("Great Dane");
+
+  console.log(scoobydoo.bark());
+}
