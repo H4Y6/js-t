a750b489489597a620c000766da1719ea0b8d219
h4y6 <h4y6@i.ua>
2024-08-09 13:36:41 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -278,5 +278,12 @@ Array will always contain at least 2 floors. Random tests will contain 2-20 elem
     }
     return dist;
   };
-  console.log(elevatorDistance([7, 1, 7, 1]));
+  // console.log(elevatorDistance([7, 1, 7, 1]));
+}
+{/**7 kyu  Consecutive items 
+  You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+It is guaranteed that a and b are both present in arr.   */
+
+  const consecutive = (arr, a, b) => Math.abs(arr.indexOf(a) - arr.indexOf(b)) === 1;
+  console.log(consecutive([1, 6, 9, -3, -4, -78, 0], -3, 4));
 }
