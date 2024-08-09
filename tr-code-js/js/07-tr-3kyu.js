4dc110cfd90a518aeb4330566865a5cf83217af2
h4y6 <h4y6@i.ua>
2024-08-09 12:51:46 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -255,3 +255,19 @@ Examples:
   const typeValidation = (variable, type) => typeof variable === type;
   // console.log(typeValidation("42", "number"));
 }
+{/**7 kyu  Elevator Distance
+  Imagine you start on the 5th floor of a building, then travel down to the 2nd floor, then back up to the 8th floor. You have travelled a total of 3 + 6 = 9 floors of distance.
+
+Given an array representing a series of floors you must reach by elevator, return an integer representing the total distance travelled for visiting each floor in the array in order.
+// simple examples
+elevatorDistance([5,2,8]) = 9
+elevatorDistance([1,2,3]) = 2
+elevatorDistance([7,1,7,1]) = 18
+// if two consecutive floors are the same,
+//distance travelled between them is 0
+elevatorDistance([3,3]) = 0
+Array will always contain at least 2 floors. Random tests will contain 2-20 elements in array, and floor values between 0 and 30.  */
+
+  const elevatorDistance = array => array.map((e, i, ar) => Math.abs(e - ar[i + 1])).slice(0, -1).reduce((dist, e) => dist + e, 0);
+  console.log(elevatorDistance([7, 1, 7, 1]));
+}
