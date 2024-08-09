1bcabbff95c25f1fd1d2a5c8824728c8e43a612b
h4y6 <h4y6@i.ua>
2024-08-09 12:15:01 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -245,3 +245,13 @@ null, ""    -> equal          */
   // console.log(compare("AD", "DD"));
   // console.log(compare("zz1", ""));
 }
+{/** 8 kyu
+For Twins: 1. Types
+ Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+Examples:
+42, "number"   --> true
+"42", "number" --> false   */
+
+  const typeValidation = (variable, type) => typeof variable === type;
+  // console.log(typeValidation("42", "number"));
+}
