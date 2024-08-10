848f86b606c9ddd39b1c3ace0a8d42399d067fbb
h4y6 <h4y6@i.ua>
2024-08-10 16:49:54 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -332,5 +332,18 @@ Place all people in alphabetical order where Mothers are followed by their child
 
   const findChildren = dancingBrigade => [...dancingBrigade].filter(e => e.match(/[A-Z]/)).sort().map(e => [...e,
   ...[...dancingBrigade].filter(el => el === e.toLowerCase())]).flat().join('');
-  console.log(findChildren("uwwWUueEe"));
+  // console.log(findChildren("uwwWUueEe"));
+}
+{/**7 kyu  Count all the sheep on farm in the heights of New Zealand
+  Every Friday and Saturday night, farmer counts sheep coming back to his farm (sheep returned on Friday stay and don't leave).
+Sheep return in groups both of the days -> You will be given two arrays with these numbers (one for Friday and one for Saturday night). Array entries are integers, equal zero or higher - in case a given array is empty, farmer did not count the sheep or got sick (we treat this as no sheep returned that night).
+Farmer knows how many sheep he has in total (third parameter). You need to calculate the amount of sheep lost (not returned to the farm) after final sheep count on Saturday.
+Example 1: Input: [1, 2], [3, 4], 15 --> Output: 5
+Example 2: Input: [3, 1, 2], [4, 5], 21 --> Output: 6
+Example 3: Input: [0], [4, 15], 31 --> Output: 12
+Example 4: Input: [], [4], 15 --> Output: 11  */
+
+  const lostSheep = (friday, saturday, total) => total - friday.reduce((sum, e) => sum + e, 0) - saturday.reduce((sum, e) => sum + e, 0);
+
+  console.log(lostSheep([1, 2], [3, 4], 15));
 }
