4a99d53f92b6f7685d8d5fb9e3048b9ee87c398c
h4y6 <h4y6@i.ua>
2024-08-10 14:34:20 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -312,6 +312,7 @@ If a number is even, convert it to binary.
 If a number is odd, convert it to hex.
 Numbers will be positive. The hexadecimal string should be lowercased.*/
 
-  const evensAndOdds = num => num % 2 ? num.toString(16) : num.toString(2);
+  // const evensAndOdds = num => num % 2 ? num.toString(16) : num.toString(2);
+  const evensAndOdds = num => num.toString(num % 2 ? 16 : 2);
   console.log(evensAndOdds(62));
 }
