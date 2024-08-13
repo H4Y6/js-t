93ce5d543f4b5ef1b318a3749e711a9c7281c8e2
h4y6 <h4y6@i.ua>
2024-08-13 14:20:35 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -469,4 +469,47 @@ solution({a: 1, b: '2'}) // should return "a = 1,b = 2"*/
   const sevenAte9 = str => str.replaceAll('797', 77).replaceAll('797', 77);
   // console.log(sevenAte9('165561786121789797'));
 };
+{/** 6 kyu  Prize Draw 
+  To participate in a prize draw each one gives his/her firstname.
+Each letter of a firstname has a value which is its rank in the English alphabet. A and a have rank 1, B and b rank 2 and so on.
+The length of the firstname is added to the sum of these ranks hence a number som.
+An array of random weights is linked to the firstnames and each som is multiplied by its corresponding weight to get what they call a winning number.
+Example:
+names: "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
+weights: [1, 4, 4, 5, 2, 1]
+PauL -> som = length of firstname + 16 + 1 + 21 + 12 = 4 + 50 -> 54
+The *weight* associated with PauL is 2 so PauL's *winning number* is 54 * 2 = 108.
+Now one can sort the firstnames in decreasing order of the winning numbers. When two people have the same winning number sort them alphabetically by their firstnames.
+Task:
+parameters: st a string of firstnames, we an array of weights, n a rank
+return: the firstname of the participant whose rank is n (ranks are numbered from 1)
+Example:
+names: "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
+weights: [1, 4, 4, 5, 2, 1]
+n: 4
+The function should return: "PauL"
+Notes:
+The weight array is at least as long as the number of names, it may be longer.
+If st is empty return "No participants".
+If n is greater than the number of participants then return "Not enough participants".
+See Examples Test Cases for more examples.    */
+
+  function rank(st, we, n) {
+    const nameArr = st.split(',');
+    if (!st) {
+      return "No participants";
+    } else if (nameArr.length < n) {
+      return "Not enough participants";
+    }
+    const scores = nameArr.map((name) => [...name.toUpperCase()].reduce((sum, e, i) => sum + e.charCodeAt() - 64, 0) + name.length)
+      .map((e, i) => [nameArr[i], e * we[i]]);
+    const winnerScore = scores.sort((a, b) => b[1] - a[1])[n - 1][1];
+    return scores.findIndex(e => e[1] === winnerScore) + 1 === n
+      ? scores.filter(e => e[1] === winnerScore).sort()[0][0] : scores.filter(e => e[1] === winnerScore).sort()[1][0];
+  }
+
+  console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4));
+  console.log(rank("Addison,Ben,Sofia,Michael,Andrew,William,Willaim", [4, 2, 1, 4, 3, 2, 2], 4));
+  console.log(rank("Lagon,Lily", [1, 5], 2));
+}
 
