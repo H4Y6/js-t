3745c2d58c0802ce93d87e06760da12d0df2a373
h4y6 <h4y6@i.ua>
2024-08-14 23:39:14 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -594,5 +594,24 @@ args_count('a', 'b') --> 2   */
   // const args_count = (...args) => args.reduce((count, e) => count + 1, 0);
   // const args_count = (...args) => args.length;
   function args_count() { return arguments.length; }
-  console.log(args_count('a', 'b', 'c'));
+  // console.log(args_count('a', 'b', 'c'));
+}
+{ /** 7 kyu  Valid Spacing 
+  Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either true or false (or the corresponding value in each language).
+For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Words can be any consecutive sequence of non space characters. Below are some examples of what the function should return:
+* 'Hello world'   => true
+* ' Hello world'  => false
+* 'Hello world  ' => false
+* 'Hello  world'  => false
+* 'Hello'         => true
+Even though there are no spaces, it is still valid because none are needed:
+* 'Helloworld'    => true
+* 'Helloworld '   => false
+* ' '             => false
+* ''              => true      */
+
+  function validSpacing(s) {
+    return !/^\s|\s$|\s\S+/.test(s);
+  }
+  console.log(validSpacing(' Hello world'));
 }
