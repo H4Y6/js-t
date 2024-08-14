d82c8054ab7f7903e206fd3daeacf9d01d560cb4
h4y6 <h4y6@i.ua>
2024-08-14 22:55:57 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -570,3 +570,16 @@ Can you finish the game so all the rules are met?    */
     }
   }
 }
+{ /** 7 kyu  Band name generator
+  My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
+"dolphin" -> "The Dolphin"
+However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
+"alaska" -> "Alaskalaska"
+Complete the function that takes a noun as a string, and returns her preferred band name written as a string.*/
+
+  function bandNameGenerator(str) {
+    return str[0] === str[str.length - 1] ? str.replace(str[0], str[0].toUpperCase()) + str.slice(1) : 'The ' + str.replace(str[0], str[0].toUpperCase());
+  }
+  // console.log(bandNameGenerator('tart'));
+  // console.log(bandNameGenerator('knife'));
+}
