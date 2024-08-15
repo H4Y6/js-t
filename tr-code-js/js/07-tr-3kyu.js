04f8707ae3f95889aed008467e23cb1a39690a04
h4y6 <h4y6@i.ua>
2024-08-15 22:24:38 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -618,3 +618,23 @@ Even though there are no spaces, it is still valid because none are needed:
   }
   // console.log(validSpacing(' Hello world'));
 }
+{ /** 8 kyu  Grader
+  Create a function that takes a number as an argument and returns a grade based on that number.
+Score	Grade
+Anything greater than 1 or less than 0.6	"F"
+0.9 or greater	"A"
+0.8 or greater	"B"
+0.7 or greater	"C"
+0.6 or greater	"D"
+Examples:
+grader(0)   should be "F"
+grader(1.1) should be "F"
+grader(0.9) should be "A"
+grader(0.8) should be "B"
+grader(0.7) should be "C"
+grader(0.6) should be "D"         */
+
+  function grader(score) {
+    return score < .6 ? 'F' : score < .7 ? 'D' : score < .8 ? 'C' : score < .9 ? 'B' : score <= 1 ? 'A' : 'F';
+  }
+}
