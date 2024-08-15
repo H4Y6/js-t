c98768bc6ca69bbe4fbd4b8cd0e40eaa8b759182
h4y6 <h4y6@i.ua>
2024-08-15 23:19:04 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -639,3 +639,20 @@ grader(0.6) should be "D"         */
     return score < .6 || score > 1 ? 'F' : score < .7 ? 'D' : score < .8 ? 'C' : score < .9 ? 'B' : 'A';
   }
 }
+{ /** 7 kyu  Arithmetic progression 
+  In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+The result should be a string of numbers, separated by comma and space.
+Example
+# first element: 1, difference: 2, how many: 5
+arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"      */
+
+  const arithmeticSequenceElements = (a, d, n) => {
+    const arr = [a];
+    while (arr.length < n) {
+      arr.push(a += d);
+    }
+    return arr.join(', ');
+  };
+  console.log(arithmeticSequenceElements(1, 2, 5));
+  console.log(arithmeticSequenceElements(1, -3, 10));
+}
