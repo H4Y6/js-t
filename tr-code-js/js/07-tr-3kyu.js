ac51638ab6e9c24cb3eb5ca7e364c7f30a18a76e
h4y6 <h4y6@i.ua>
2024-08-17 18:22:21 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -694,3 +694,18 @@ Don't worry about bad input, you will always receive a finite list of integers. 
   const filterLucky = x => [...x].filter(e => [...e + ''].some(el => el === '7'));
   // console.log(filterLucky([1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17]));
 }
+{ /** 7 kyu
+  All Star Code Challenge #22 
+  Create a function that takes an integer argument of seconds and converts the value into a string describing how many hours and minutes comprise that many seconds.
+Any remaining seconds left over are ignored.
+Note:
+The string output needs to be in the specific form - "X hour(s) and X minute(s)"
+For example:
+3600 --> "1 hour(s) and 0 minute(s)"
+3601 --> "1 hour(s) and 0 minute(s)"
+3500 --> "0 hour(s) and 58 minute(s)"
+323500 --> "89 hour(s) and 51 minute(s)"  */
+
+  const toTime = seconds => `${Math.trunc(seconds / 3600)} hour(s) and ${Math.trunc(seconds % 3600 / 60)} minute(s)`;
+  console.log(toTime(3700));
+}
