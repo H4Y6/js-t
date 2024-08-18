c5feab9b765db841acdc5e3f3a71d0cc7a4cd9f0
h4y6 <h4y6@i.ua>
2024-08-18 21:36:37 +0300

    modified:   work-check-go/js/1-w.js

--- work-check-go/js/1-w.js
+++ work-check-go/js/1-w.js
@@ -13,3 +13,16 @@ function sayHi() {
   console.log("Hello, this is my function!");
 }
 sayHi();
+
+function add(a, b, c) {
+  console.log(`Addition result equals ${a + b + c}`);
+  return a + b + c;
+}
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
+add(2, 5, 8); // 15
+
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
