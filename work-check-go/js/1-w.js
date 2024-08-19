e9ff38848f0f01f1459f21f405356a2e49b00a47
h4y6 <h4y6@i.ua>
2024-08-19 12:08:48 +0300

    modified:   work-check-go/js/1-w.js

--- work-check-go/js/1-w.js
+++ work-check-go/js/1-w.js
@@ -127,3 +127,19 @@ function isNumberNotInRange(start, end, number) {
 
   return isNotInRange;
 }
+
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+  if (totalSpent < 5000) { discount = BASE_DISCOUNT; }
+  else if (totalSpent < 20000) { discount = BRONZE_DISCOUNT; }
+  else if (totalSpent < 50000) { discount = SILVER_DISCOUNT; } else {
+    discount = GOLD_DISCOUNT;
+  }
+
+  return discount;
+}
+console.log(getDiscount(137000));
