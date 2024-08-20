e51d70185a74da7298ec304304162880a6698904
h4y6 <h4y6@i.ua>
2024-08-20 13:17:51 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -762,3 +762,20 @@ If null value given then return -1     */
   }
   // console.log(excludingVatPrice(0));
 }
+{
+  /**7 kyu  Check three and two  
+   * Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran), check if the array contains three and two of the same values.
+Examples
+["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+["a", "a", "a", "a", "a"] ==> false // 5x "a"   */
+
+  function checkThreeAndTwo(array) {
+    if (new Set(array).size === 2) {
+      const res = array.reduce((res, e) => e === array[0] ? res + 1 : res, 0);
+      return res === 2 || res === 3;
+    }
+    return false;
+  }
+  console.log(checkThreeAndTwo(["a", "c", "a", "c", "i"]));
+}
