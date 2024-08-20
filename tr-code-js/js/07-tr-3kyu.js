336407ab5d3cb2ed3cd3e6d3693a291e407600f1
h4y6 <h4y6@i.ua>
2024-08-20 11:30:09 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -710,3 +710,20 @@ For example:
   const toTime = seconds => `${seconds / 3600 ^ 0} hour(s) and ${seconds % 3600 / 60 ^ 0} minute(s)`;
   // console.log(toTime(3700));
 }
+{ /** 8 kyu  Find the Integral 
+  Create a function that finds the integral of the expression passed.
+In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
+For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).
+Notes:
+The output should be a string.
+The coefficient and exponent is always a positive integer.
+Examples
+ 3, 2  -->  "1x^3"
+12, 5  -->  "2x^6"
+20, 1  -->  "10x^2"
+40, 3  -->  "10x^4"
+90, 2  -->  "30x^3"       */
+
+  const integrate = (coefficient, exponent) => [coefficient / (exponent += 1), 'x^', exponent].join('');
+  console.log(integrate(12, 5));
+}
