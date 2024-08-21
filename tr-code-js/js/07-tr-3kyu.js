5af79aa576488886548862ed9413982ff9e7b398
h4y6 <h4y6@i.ua>
2024-08-21 16:15:35 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -795,4 +795,22 @@ greet("") === null; // Return null if input is empty string
 greet(null) === null; // Return null if input is null           */
 
 const greet = name => name ? `hello ${name}!` : null;
-console.log(greet(""));
+// console.log(greet(""));
+
+/*8 kyu  Playing with cubes II
+Hey Codewarrior!You already implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code two: One taking an integer and one handling no given arguments!
+Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+The constructor taking no arguments should assign 0 to Cube's Side property.*/
+
+class Cube {
+  constructor(side) {
+    this.side = side;
+  }
+
+  getSide() {
+    return this.side;
+  }
+  setSide(n) {
+    this.side = Math.abs(n);
+  }
+}
