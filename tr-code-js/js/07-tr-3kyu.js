cb1145bf355579a06ba41bbf4054b7af4c273648
h4y6 <h4y6@i.ua>
2024-08-21 18:46:30 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -839,6 +839,6 @@ Examples
 ("jason", "samson", "s")      ==>  "jasamson"
 ("wonderful", "people", "e")  ==>  "wondeople"       */
 
-const stringMerge = (string1, string2, letter) => string1.split(letter)[0] + string2.slice(string2.indexOf(letter));
-
+// const stringMerge = (string1, string2, letter) => string1.split(letter)[0] + string2.slice(string2.indexOf(letter));
+const stringMerge = (str1, str2, letter) => str1.match(`[^${letter}]*`) + str2.match(`${letter}.*`);
 console.log(stringMerge("hello", "world", "l"));
