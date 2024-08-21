a59c338d3e4f363d885736c2f6719c8068e157e9
h4y6 <h4y6@i.ua>
2024-08-21 18:14:39 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -830,3 +830,15 @@ const longestWord = stringOfWords => stringOfWords.split(' ').sort((b, a) => b.l
 // console.log(longestWord('red blue grey'));
 // console.log(longestWord('red white blue'));
 // console.log(longestWord("brings each opportunity step going"));
+
+/** 7 kyu  String Merge!  
+ * Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+Examples
+("hello", "world", "l")       ==>  "held"
+("coding", "anywhere", "n")   ==>  "codinywhere"
+("jason", "samson", "s")      ==>  "jasamson"
+("wonderful", "people", "e")  ==>  "wondeople"       */
+
+const stringMerge = (string1, string2, letter) => string1.split(letter)[0] + string2.slice(string2.indexOf(letter));
+
+console.log(stringMerge("hello", "world", "l"));
