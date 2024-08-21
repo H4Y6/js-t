de3ad7ee0b2038d164abc7d74fae39b70068e1f8
h4y6 <h4y6@i.ua>
2024-08-21 16:03:04 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -786,3 +786,13 @@ Examples
   const sortme = names => names.sort();
   // console.log(sortme(['one', 'two', 'three']));
 }
+
+/* 7 kyu  Say hello! 
+Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+Example:
+greet("Niks") === "hello Niks!";
+greet("") === null; // Return null if input is empty string
+greet(null) === null; // Return null if input is null           */
+
+const greet = name => name ? `hello ${name}!` : null;
+console.log(greet(""));
