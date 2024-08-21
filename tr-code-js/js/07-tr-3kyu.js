7a1b746d87470a75c1048ab88c8e68da23de1457
h4y6 <h4y6@i.ua>
2024-08-21 19:41:35 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -849,7 +849,8 @@ const stringMerge = (str1, str2, letter) => (str1.split(letter)[0] + letter + st
 For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
 Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.*/
 
-const bingo = a => 'BINGO'.split('').map(e => a.map(e => String.fromCharCode(e + 64)).join('').includes(e)).every(e => e) ? 'WIN' : 'LOSE';
+// const bingo = a => 'BINGO'.split('').map(e => a.map(e => String.fromCharCode(e + 64)).join('').includes(e)).every(e => e) ? 'WIN' : 'LOSE';
+const bingo = a => 'BINGO'.split('').map(e => a.includes(e.charCodeAt() - 64)).every(e => e) ? 'WIN' : 'LOSE';
 
 console.log(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10]));
 console.log(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
