8c257f9f3795f027a76952c938a940e8c3802b97
h4y6 <h4y6@i.ua>
2024-08-24 22:44:09 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1019,8 +1019,10 @@ function newAvg(arr, newavg) {
 Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
 For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace'] */
 
-function spacey(array) {
-  let res = '';
-  return array.map((e) => res += e);
-}
+// function spacey(array) {
+//   let res = '';
+//   return array.map((e) => res += e);
+// }
+
+const spacey = (array, res = '') => array.map((e) => res += e);
 console.log(spacey(['kevin', 'has', 'no', 'space']));
