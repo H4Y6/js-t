e586742608f310631d64806ebce3a30c1c568687
h4y6 <h4y6@i.ua>
2024-08-24 18:52:56 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -923,4 +923,49 @@ function balance(left, right) {
   const diff = [...left].reduce((res, e) => res + (e === '!' ? 2 : 3), 0) - [...right].reduce((res, e) => res + (e === '!' ? 2 : 3), 0);
   return diff > 0 ? 'Left' : diff < 0 ? 'Right' : 'Balance';
 }
-console.log(balance("!!???!????", "??!!?!!!!!!!"));
+// console.log(balance("!!???!????", "??!!?!!!!!!!"));
+
+/** 7 kyu  Letterbox Paint-Squad 
+ * You and a group of friends are earning some extra money in the school holidays by re-painting the numbers on people's letterboxes for a small fee.
+
+Since there are 10 of you in the group each person just concentrates on painting one digit! For example, somebody will paint only the 1's, somebody else will paint only the 2's and so on...
+
+But at the end of the day you realise not everybody did the same amount of work.
+
+To avoid any fights you need to distribute the money fairly. That's where this Kata comes in.
+
+Kata Task
+Given the start and end letterbox numbers, write a method to return the frequency of all 10 digits painted.
+
+Example
+For start = 125, and end = 132
+
+The letterboxes are
+
+125 = 1, 2, 5
+126 = 1, 2, 6
+127 = 1, 2, 7
+128 = 1, 2, 8
+129 = 1, 2, 9
+130 = 1, 3, 0
+131 = 1, 3, 1
+132 = 1, 3, 2
+The digit frequencies are:
+
+0 is painted 1 time
+1 is painted 9 times
+2 is painted 6 times
+etc...
+and so the method would return [1,9,6,3,0,1,1,1,1,1]
+
+Notes
+0 < start <= end      */
+
+const paintLetterboxes = (start, end) => {
+  let digits = '';
+  for (let i = start; i <= end; i++) {
+    digits += i;
+  }
+  return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((e) => 0 + [...digits].filter(el => +el === e).length);
+};
+console.log(paintLetterboxes(125, 132));
