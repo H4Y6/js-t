4a15f3ad5e3ddb569072e980a6bbf091de192ace
h4y6 <h4y6@i.ua>
2024-08-25 14:45:29 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1054,3 +1054,24 @@ const deepCount = a => JSON.stringify(a).replace(/[^[,]|\[]/g, '').length;
 // console.log(deepCount(["x", "y", ["z"]]));
 // console.log(deepCount([]));
 // console.log(deepCount([26, [[[[23, 9, 0]], 11, [[19, [[]], [[]]]]]], 4]));
+
+{/** 7 kyu  By 3, or not by 3? That is the question . . .
+A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+
+Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+
+Example:
+
+"123"      -> true
+"8409"     -> true
+"100853"   -> false
+"33333333" -> true
+"7"        -> false
+Try to avoid using the % (modulo) operator. */
+
+  function divisibleByThree(str) {
+    const sum = [...str].reduce((sum, e) => sum + +e, 0) / 3;
+    return sum === parseInt(sum);
+  }
+  console.log(divisibleByThree('192541'));
+}
