79b0f707a25aff3d6fa1d07d2c6bce87ab6ac15c
h4y6 <h4y6@i.ua>
2024-08-25 18:59:01 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1132,3 +1132,18 @@ vowelOne( "aeiou, abc" ) // "1111100100" */
   }
   // console.log(vowelOne('vowelOne'));
 }
+{/** 7 kyu  Initialize my name
+  Some people just have a first name; some people have first and last names and some people have first, middle and last names.
+You task is to initialize the middle names (if there is any).
+Examples
+'Jack Ryan'                   => 'Jack Ryan'
+'Lois Mary Lane'              => 'Lois M. Lane'
+'Dimitri'                     => 'Dimitri'
+'Alice Betty Catherine Davis' => 'Alice B. C. Davis'   */
+
+  function initializeNames(name) {
+    name = name.split(' ');
+    return name.length >= 2 ? [name[0], ...name.slice(1, -1).map(e => e[0] + '.'), ...name.slice(-1)].join(' ') : name[0];
+  }
+  console.log(initializeNames('Alice Betty Catherine Davis'));
+}
