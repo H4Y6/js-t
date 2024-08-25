cfd5800a19e85658bf17c06aabfc17b1b04a8d4d
h4y6 <h4y6@i.ua>
2024-08-25 17:03:21 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1076,3 +1076,14 @@ Try to avoid using the % (modulo) operator. */
   const divisibleByThree = str => Number.isInteger([...str].reduce((sum, e) => sum + +e, 0) / 3);
   // console.log(divisibleByThree('192541'));
 }
+
+{ /** 7 kyu    Flatten 
+  Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]    */
+
+  const flatten = array => array.flat();
+  // TODO: Program me
+  console.log(flatten([[1, 2, 3], ["a", "b", "c"], [1, 2, 3]]));
+}
