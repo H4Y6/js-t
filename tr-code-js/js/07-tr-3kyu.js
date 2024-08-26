82c846e482a26c8000469ce64ef5057af1355d86
h4y6 <h4y6@i.ua>
2024-08-26 20:30:48 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1186,6 +1186,21 @@ Examples
   "!???!!"    --->   9
   "!????!!!?" --->  20 */
 
-  const product = string => [...string].filter(e => e === '!').length * [...string].filter(e => e === '?').length;
-  console.log(product('!ab? ?'));
+  // const product = string => [...string].filter(e => e === '!').length * [...string].filter(e => e === '?').length;
+  // const product = string => (string.split('!').length - 1) * (string.split('?').length - 1);
+  const product = string => (string.match(/!/g) || []).length * (string.match(/\?/g) || []).length;
+  // console.log(product('!ab? ?'));
+}
+{ /** 7 kyu  Is n divisible by (...)?
+  Create a function that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
+    Example:
+    (6,1,3)--> true because 6 is divisible by 1 and 3
+  (12,2)--> true because 12 is divisible by 2
+  (100,5,4,10,25,20)--> true
+  (12,7)--> false because 12 is not divisible by 7 */
+
+  function isDivisible() {
+    return [...arguments].map((e, i, ar) => ar[0] % e).every(e => !e);
+  }
+  console.log(isDivisible(8, 4, 2));
 }
