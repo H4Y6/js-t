1de5c5cc0eff2e52ccbd35f3f6a4f53d4d0b7cc3
h4y6 <h4y6@i.ua>
2024-08-27 23:25:43 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1231,5 +1231,22 @@ Code Examples
     if (+n !== Math.abs(parseInt(n))) { throw new TypeError(`${n} is invalid`); }
     return Array.from({ length: n }, e => v);
   }
-  console.log(prefill('1', 'er'));
+  // console.log(prefill('1', 'er'));
+}
+{/** 8 kyu  NBA full 48 minutes average 
+  An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game, subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
+Examples:
+pointsPer48(12, 20) // 28.8
+pointsPer48(10, 10) // 48 
+pointsPer48(5, 17) // 14.1 
+pointsPer48(0, 0) // 0
+Notes:
+All inputs will be either be an integer or float.  */
+
+  function pointsPer48(ppg, mpg) {
+    return !ppg ? 0 : Math.round(ppg / mpg * 480) / 10;
+  }
+  console.log(pointsPer48(12, 20));
+  console.log(pointsPer48(11.1, 32));
 }
