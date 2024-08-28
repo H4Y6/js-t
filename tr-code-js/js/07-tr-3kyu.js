da42bb130ffdf8ae794b3089304780800535c650
h4y6 <h4y6@i.ua>
2024-08-28 08:58:27 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1249,7 +1249,38 @@ All inputs will be either be an integer or float.  */
   // }
 
   const pointsPer48 = (ppg, mpg) => ppg ? Number((ppg / mpg * 48).toFixed(1)) : 0;
+  // console.log(pointsPer48(12, 20));
+  // console.log(pointsPer48(11.1, 32));
+}
+{/** 7 kyu  Drying Potatoes 
+  All we eat is water and dry matter.
 
-  console.log(pointsPer48(12, 20));
-  console.log(pointsPer48(11.1, 32));
+Let us begin with an example:
+
+John bought potatoes: their weight is 100 kilograms. Potatoes contain water and dry matter. The water content is 99 percent of the total weight. He thinks they are too wet and puts them in an oven - at low temperature - for them to lose some water.
+
+At the output the water content is only 98%.
+
+What is the total weight in kilograms (water content plus dry matter) coming out of the oven?
+
+He finds 50 kilograms and he thinks he made a mistake: "So much weight lost for such a small change in water content!"
+
+Can you help him?
+
+Task
+Write function potatoes with
+
+int parameter p0 - initial percent of water-
+int parameter w0 - initial weight -
+int parameter p1 - final percent of water -
+potatoesshould return the final weight coming out of the oven w1 truncated as an int.
+
+Example:
+potatoes(99, 100, 98) --> 50  */
+
+  function potatoes(p0, w0, p1) {
+    return parseInt(w0 * (100 - p0) / (100 - p1));
+  }
+  // console.log(potatoes(99, 100, 98));
+  // console.log(potatoes(82, 127, 80));
 }
