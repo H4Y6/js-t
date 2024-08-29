f2d56dd4626df034a181c021efbc2d222671548b
h4y6 <h4y6@i.ua>
2024-08-29 11:13:17 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1308,6 +1308,31 @@ Write a function named isNice/IsNice that returns true if its array argument is 
   // const isNice = arr => arr.length ? arr.map(e => arr.includes(e + 1) || arr.includes(e - 1)).every(e => e) : false;
   const isNice = arr => arr.length ? arr.every(e => arr.includes(e + 1) || arr.includes(e - 1)) : false;
 
-  console.log(isNice([2, 10, 9, 3]));
-  console.log(isNice([]));
+  // console.log(isNice([2, 10, 9, 3]));
+  // console.log(isNice([]));
+}
+{ /** 7 kyu  Split The Bill
+  You're in a restaurant with your friends and it's time to go, but there's still one big problem...the bill. Who will pay what? Lucky for you, you've got your computer handy! One simple function and the bill is paid——fairly, too!
+The function should take one parameter: an object/dict with two or more name-value pairs that represent the members of the group and the amount spent by each.
+Your function should return an object/dict with the same names, showing how much money the members should pay or receive.
+Further points:
+The values should be positive numbers if the person should receive money from the group, negative numbers if they owe money to the group.
+If value is a decimal, round to two decimal places.
+Translations and comments (and upvotes!) welcome.
+Example
+3 friends go out together: A spends £20, B spends £15, and C spends £10. The function should return an object/dict showing that A should receive £5, B should receive £0, and C should pay £5.
+var group = {
+    A: 20, 
+    B: 15, 
+    C: 10
+}
+splitTheBill(group) // returns {A: 5, B: 0, C: -5} */
+
+  const splitTheBill = x => {
+    const obj = { ...x };
+    let sumDiv = Object.values(x).reduce((sum, e) => sum + e, 0) / Object.keys(x).length;
+    Object.entries(x).map(e => obj[e[0]] = !(e[1] - sumDiv) % 1 ? e[1] - sumDiv : +(e[1] - sumDiv).toFixed(2));
+    return obj;
+  };
+  // console.log(splitTheBill({ A: 20, B: 14, C: 10 }));
 }
