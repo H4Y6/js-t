d6b0546bc834a1b366b3197c92c30aa85bc74b84
h4y6 <h4y6@i.ua>
2024-09-02 20:15:54 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1559,4 +1559,20 @@ createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}  */
   const createDict = (keys, values) => keys.reduce((obj, key, i) => (obj[key] = values[i] ?? null, obj), {});
   // console.log(createDict(['a', 'b', 'c', 'd'], [1, 2, 3]));
 }
+{ /** 7 kyu  Find Duplicates
+  Given an array, find the duplicates in that array, and return a new array of those duplicates. The elements of the returned array should appear in the order when they first appeared as duplicates.
+Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., "1" != 1).
+Examples
+[1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
+[0, 1, 2, 3, 4, 5]                ==>  []         */
 
+  const duplicates = arr => {
+    const dupArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arr[i + 1]) dupArr.push(arr[i]);
+
+    }
+    return dupArr;
+  };
+  console.log(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]));
+}
