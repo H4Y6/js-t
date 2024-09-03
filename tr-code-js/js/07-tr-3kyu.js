2e2794d0475981da7d163258da5949e76ee958d1
h4y6 <h4y6@i.ua>
2024-09-03 20:22:15 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1579,7 +1579,8 @@ Examples
 { /** 7 kyu  Count consonants
   Complete the function that takes a string of English-language text and returns the number of consonants in the string.
     Consonants are all letters used to write English excluding the vowels a, e, i, o, u. */
-  const consonantCount = str => [...str].map(e => /[A-z]/g.test(e) ? e : e = '').map(e => /[aeiou]/gi.test(e) ? e = '' : e).map(e => e === ' ' || e === '_' || e === '^' ? '' : e).join('').length;
+  // const consonantCount = str => [...str].map(e => /[A-z]/g.test(e) ? e : e = '').map(e => /[aeiou]/gi.test(e) ? e = '' : e).map(e => e === ' ' || e === '_' || e === '^' ? '' : e).join('').length;
+  const consonantCount = str => str.replace(/\W|\d|[_aeiou]/gi, "").length;
   console.log(consonantCount('bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'));
   console.log(consonantCount('aeiouAEIOU'));
   console.log(consonantCount('aeiou AEIOU bcdfghjklmnpqrstvwxyz BCDFGHJKLMNPQRSTVWXYZ 01234567890_ ^&$#'));
