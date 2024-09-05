31190269da3e33e6090f4f787878cedca5d77723
h4y6 <h4y6@i.ua>
2024-09-05 19:46:35 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1662,5 +1662,21 @@ First and last bits as set bits.    */
   // }
 
   const extraPerfect = n => Array.from({ length: n }, (e, i) => i + 1).filter(e => e % 2);
-  console.log(extraPerfect(29));
+  // console.log(extraPerfect(29));
+}
+{ /** 7 kyu  True for Just One?
+Create a function called one that accepts two params:
+a sequence
+a function
+and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+Example
+one([1, 3, 5, 6, 99, 1, 3], bigger_than_ten) -> true
+one([1, 3, 5, 6, 99, 88, 3], bigger_than_ten) -> false
+one([1, 3, 5, 6, 5, 1, 3], bigger_than_ten) -> false  */
+
+  const bigger_than_ten = n => n > 10;
+  function one(arr, fun) {
+    return arr.reduce((count, e) => count + (fun(e) ? 1 : 0), 0);
+  }
+  console.log(one([1, 3, 5, 6, 99, 1, 33], bigger_than_ten));
 }
