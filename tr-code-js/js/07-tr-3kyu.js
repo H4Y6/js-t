23e0e8f63164d86a2af3d6651a808dac092403ac
h4y6 <h4y6@i.ua>
2024-09-05 22:54:05 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1678,5 +1678,20 @@ one([1, 3, 5, 6, 5, 1, 3], bigger_than_ten) -> false  */
   function one(arr, fun) {
     return arr.reduce((count, e) => count + (fun(e) ? 1 : 0), 0);
   }
-  console.log(one([1, 3, 5, 6, 99, 1, 33], bigger_than_ten));
+  // console.log(one([1, 3, 5, 6, 99, 1, 33], bigger_than_ten));
+}
+{ /** 8 kyu  Find the Slope 
+  Given an array of 4 integers
+[a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
+
+For an undefined slope (division by 0), return undefined . Note that the "undefined" is case-sensitive.
+
+   a:x1
+   b:y1
+   c:x2
+   d:y2
+Assume that [a,b,c,d] and the answer are all integers (no floating numbers!). Slope: https://en.wikipedia.org/wiki/Slope*/
+
+  const slope = points => points[2] - points[0] ? (points[3] - points[1]) / (points[2] - points[0]) + '' : undefined;
+  console.log(slope([15, 45, 12, 60]));
 }
