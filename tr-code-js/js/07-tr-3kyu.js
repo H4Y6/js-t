5367ce00d1b4b4daf370c0d631f65b32d7e69151
h4y6 <h4y6@i.ua>
2024-09-05 18:10:52 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1632,3 +1632,33 @@ Since we cannot define keywords in Javascript (well, at least I don't know how t
     return a !== b;
   }
 }
+{
+  /** 7 kyu  Extra Perfect Numbers 
+   * Extra perfect number is the number that first and last bits are set bits.
+Task
+Given a positive integer N , Return the extra perfect numbers in range from 1 to N .
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+Number passed is always Positive .
+Returned array/list should contain the extra perfect numbers in ascending order from lowest to highest
+Input >> Output Examples
+extraPerfect(3)  ==>  return {1,3}
+Explanation:
+(1)10 =(1)2
+First and last bits as set bits.
+(3)10 = (11)2
+First and last bits as set bits.
+extraPerfect(7)  ==>  return {1,3,5,7}
+Explanation:
+(5)10 = (101)2
+First and last bits as set bits.
+(7)10 = (111)2
+First and last bits as set bits.    */
+
+  function extraPerfect(n) {
+    const arr = Array.from({ length: n / 2 }, (e, i) => 2 * i + 1);
+    return n % 2 ? [...arr, n] : arr;
+  }
+  console.log(extraPerfect(28));
+}
