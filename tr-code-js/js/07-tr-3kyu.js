0bc56ed645d35d11820ed72cd49af8ea53dce97f
h4y6 <h4y6@i.ua>
2024-09-06 17:35:25 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1732,3 +1732,21 @@ Examples:
   const arrCheck = value => value.every(e => e instanceof Array);
   // console.log(arrCheck([[5], [2]]));
 }
+{ /** 7 kyu  Interview Question (easy)
+  You receive the name of a city as a string, and you need to return a string that shows how many times each letter shows up in the string by using asterisks (*).
+For example:
+"Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
+As you can see, the letter c is shown only once, but with 2 asterisks.
+The return string should include only the letters (not the dashes, spaces, apostrophes, etc). There should be no spaces in the output, and the different letters are separated by a comma (,) as seen in the example above.
+Note that the return string must list the letters in order of their first appearance in the original string.
+More examples:
+"Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
+"Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"    */
+
+  const getStrings = city => {
+    const set = new Set(city.toLowerCase().replaceAll(' ', ''));
+    return [...set].map(e => [e, [...city.toLowerCase()].filter(el => el === e)]).map(e => e[0] + ':' + e[1].join('').replace(/./g, '*')).join(',');
+  };
+  console.log(getStrings("Chicago"));
+  console.log(getStrings("Dar es Salaam"));
+}
