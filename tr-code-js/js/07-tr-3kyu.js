ab615be9e76a3af90cd106959a218216082edc3c
h4y6 <h4y6@i.ua>
2024-09-08 17:54:59 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1906,5 +1906,18 @@ Result:
       }
     });
   }
-  console.log(streetFighterSelection(fighters, [0, 0], moves));
+  // console.log(streetFighterSelection(fighters, [0, 0], moves));
+}
+{ /** 7 kyu  Stanton measure
+  The Stanton measure of an array is computed as follows: count the number of occurences for value 1 in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
+Write a function which takes an integer array and returns its Stanton measure.
+Examples
+The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times in the array and 3 occurs 1 time. */
+
+  const stantonMeasure = a => {
+    const n = a.reduce((count, e) => e === 1 ? count + 1 : count, 0);
+    return a.reduce((c, e) => e === n ? c + 1 : c, 0);
+  };
+  console.log((stantonMeasure([1, 4, 3, 2, 1, 2, 3, 2])));
 }
