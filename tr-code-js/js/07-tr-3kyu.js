ee2062fe29778c6516b2a262f8804f95557f769a
h4y6 <h4y6@i.ua>
2024-09-08 19:52:56 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -1949,3 +1949,20 @@ findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]     */
   // const findAll = (array, n) => array.reduce((arr, e, i) => e === n ? [...arr, i] : arr, []);
   // console.log(findAll([20, 20, 10, 13, 15, 2, 7, 2, 20, 3, 18, 2, 3, 2, 16, 10, 9, 9, 7, 5, 15, 5], 20));
 }
+{ /** 7 kyu  Responsible Drinking 
+  Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+Your fellow coders have bought you several drinks tonight in the form of a string. Return a string suggesting how many glasses of water you should drink to not be hungover.
+Examples
+"1 beer"  -->  "1 glass of water"
+because you drank one standard drink
+"1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+because you drank ten standard drinks
+Note:
+To keep the things simple, we'll consider that any "numbered thing" in the string is a drink. Even "1 bear" -> "1 glass of water"; or "1 chainsaw and 2 pools" -> "3 glasses of water"...    */
+
+  const hydrate = s => {
+    const n = s.split(/[a-z]+./).reduce((res, e) => res + +e, 0);
+    return n > 1 ? n + ' glasses of water' : n + ' glass of water';
+  };
+  console.log(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"));
+}
