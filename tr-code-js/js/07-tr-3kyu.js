5b96cf3a34835d51bff7c391ecbb04440d7dca68
h4y6 <h4y6@i.ua>
2024-09-10 19:59:00 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2039,3 +2039,12 @@ Example:
   // console.log(countLettersAndDigits('hel2!lo'));
   // console.log(countLettersAndDigits('n!!_ice!!123'));
 }
+{ /** 7 kyu  Evens times last
+  Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL), multiplied by the integer at the last index.
+Indices in sequence start from 0.
+If the sequence is empty, you should return 0. */
+
+  const evenLast = numbers => numbers.length ? numbers.reduce((res, e, i) => res + (i % 2 ? 0 : e), 0) * numbers[numbers.length - 1] : 0;
+  console.log(evenLast([2, 3, 4, 5]));
+}
+
