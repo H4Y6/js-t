9240a064da4dd996f280b72aa1abf0d2207607f1
h4y6 <h4y6@i.ua>
2024-09-10 22:25:27 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2047,6 +2047,19 @@ If the sequence is empty, you should return 0. */
   // const evenLast = numbers => numbers.length ? numbers.reduce((res, e, i) => res + (i % 2 ? 0 : e), 0) * numbers[numbers.length - 1] : 0;
   // const evenLast = numbers => numbers.length ? numbers.reduce((res, e, i) => res + (i % 2 ? 0 : e), 0) * numbers.pop() : 0;
   const evenLast = numbers => numbers.length ? numbers.reduce((res, e, i) => res + (i % 2 ? 0 : e), 0) * numbers.slice(-1) : 0;
-  console.log(evenLast([2, 3, 4, 5]));
+  // console.log(evenLast([2, 3, 4, 5]));
+}
+{ /** 7 kyu  Find Screen Size 
+  Cheesy Cheeseman just got a new monitor! He is happy with it, but he just discovered that his old desktop wallpaper no longer fits. He wants to find a new wallpaper, but does not know which size wallpaper he should be looking for, and alas, he just threw out the new monitor's box. Luckily he remembers the width and the aspect ratio of the monitor from when Bob Mortimer sold it to him. Can you help Cheesy out?
+The Challenge
+Given an integer width and a string ratio written as WIDTH:HEIGHT, output the screen dimensions as a string written as WIDTHxHEIGHT.
+Note: The calculated height should be represented as an integer. If the height is fractional, truncate it.    */
+
+  const findScreenHeight = (width, ratio) => {
+    const [w, h] = ratio.split(':');
+    const height = width / w * h;
+    return width + "x" + height;
+  };
+  console.log(findScreenHeight(1024, "4:3"));
 }
 
