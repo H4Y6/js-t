97076dd113a215a8c1b6bedc2523f5f94a0365d1
h4y6 <h4y6@i.ua>
2024-09-12 19:11:31 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2117,3 +2117,16 @@ As the Next closest year with only distinct digits is 1023 .*/
   const isLucky = n => n % 9 === 0;
   // console.log(isLucky(1892376));
 }
+{ /** 7 kyu  Heron's formula
+  Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+    Heron's formula:
+  s ∗  (  s  −  a  )  ∗  (  s  −  b  )  ∗  (  s  −  c  )  s∗(s−a)∗(s−b)∗(s−c)   
+  where  s  =  (a  +  b  +  c) / 2     
+  Output should have 2 digits precision. */
+
+  function heron(a, b, c) {
+    const s = (a + b + c) / 2;
+    return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  }
+  console.log(heron(3, 4, 5));
+}
