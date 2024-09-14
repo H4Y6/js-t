aed5ed14d72efc1e4ab0ccb0e0b0fa5065f94aec
h4y6 <h4y6@i.ua>
2024-09-14 21:40:53 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2292,3 +2292,21 @@ The questions array is already defined for you and is not the same as the one in
   }
   // console.log(questions);
 }
+{ /** 7 kyu  How many consecutive numbers are needed?
+  Write a function that takes an array of unique integers and returns the minimum number of integers needed to make the values of the array consecutive from the lowest number to the highest number.
+
+Example
+[4, 8, 6] --> 2
+Because 5 and 7 need to be added to have [4, 5, 6, 7, 8]
+
+[-1, -5] --> 3
+Because -2, -3, -4 need to be added to have [-5, -4, -3, -2, -1]
+
+[1] --> 0
+[]  --> 0      */
+
+  // const consecutive = array => array.length ? Math.abs(Math.max(...array) - Math.min(...array)) + 1 - array.length : 0;
+  const consecutive = array => Math.abs(Math.max(...array) - Math.min(...array)) + 1 - array.length | 0;
+
+  console.log(consecutive([]));
+}
