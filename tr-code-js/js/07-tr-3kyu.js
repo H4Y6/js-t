81d474a9fcbf29558272ee754fdafd7ad77199c7
h4y6 <h4y6@i.ua>
2024-09-16 13:49:11 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2393,3 +2393,15 @@ If the input-string is null or empty return exactly this value! (empty string fo
     ? new Date(new Date().setHours(...timestring.split(`:`))).toTimeString().slice(0, 8) : null;
   // console.log(timeCorrect("52:01:01"));
 }
+{ /** 7 kyu Frequency sequence
+  Your task is to return an output string that translates an input string s by replacing each character in s with a number representing the number of times that character occurs in s and separating each number with the sep character(s).
+    Example (s, sep --> Output)
+    "hello world", "-" --> "1-1-3-3-2-1-1-2-1-3-1"
+  "19999999"   , ":" --> "1:7:7:7:7:7:7:7"
+  "^^^**$"     , "x" --> "3x3x3x2x2x1"        */
+
+  function freqSeq(str, sep) {
+    return [...str].reduce((res, e, i, ar) => res + [...str].filter(el => el === e).length + sep, '').slice(0, -1);
+  }
+  console.log(freqSeq('hello world', '-'));
+}
