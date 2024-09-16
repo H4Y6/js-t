5fc306be209787207d3fe19329004cf8e341fac8
h4y6 <h4y6@i.ua>
2024-09-16 15:35:55 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2406,3 +2406,34 @@ If the input-string is null or empty return exactly this value! (empty string fo
   }
   // console.log(freqSeq('hello world', '-'));
 }
+{ /** 7 kyu  Number-Star ladder
+  Using n as a parameter in the function pattern, where n>0, complete the codes to get the pattern (take the help of examples):
+    Note: There is no newline in the end (after the pattern ends)
+    Examples
+  pattern(3) should return "1\n1*2\n1**3", e.g. the following:
+  
+  1
+  1*2
+  1**3
+  pattern(10): should return the following:
+  
+  1
+  1*2
+  1**3
+  1***4
+  1****5
+  1*****6
+  1******7
+  1*******8
+  1********9
+  1*********1   */
+
+  function pattern(n) {
+    let output = "";
+    for (let i = 0; i < n; i++) {
+      output += 1 + '*'.repeat(i) + (+i + 1) + (i < n - 1 ? '\n' : '');
+    }
+    return output.slice(1);
+  }
+  console.log(pattern(4));
+}
