3e41b9d8fa30741d56ec9a92daaa7ca2ad3b9310
h4y6 <h4y6@i.ua>
2024-09-18 15:52:44 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2475,13 +2475,40 @@ Input will be a string with length > 0*/
   const moveTen = s => s.replace(/[a-z]/g, e => String.fromCharCode(97 + (e.charCodeAt() - 87) % 26));
   // console.log(moveTen("exampletesthere"));
 }
-{
+{/** 7 kyu  Dot Calculator 
+  You have to write a calculator that receives strings for input. The dots will represent the number in the equation. There will be dots on one side, an operator, and dots again after the operator. The dots and the operator will be separated by one space.
+
+Here are the following valid operators :
+
++ Addition
+- Subtraction
+* Multiplication
+// Integer Division
+Your Work (Task)
+You'll have to return a string that contains dots, as many the equation returns. If the result is 0, return the empty string. When it comes to subtraction, the first number will always be greater than or equal to the second number.
+
+Examples (Input => Output)
+* "..... + ..............." => "...................."
+* "..... - ..."             => ".."
+* "..... - ."               => "...."
+* "..... * ..."             => "..............."
+* "..... * .."              => ".........."
+* "..... // .."             => ".."
+* "..... // ."              => "....."
+* ". // .."                 => ""
+* ".. - .."                 =>   */
+
+  // function dotCalculator(equation) {
+  //   const [a, sign, b] = equation.split(' ');
+  //   if (sign === '+') return '.'.repeat(a.length + b.length);
+  //   if (sign === '-') return '.'.repeat(a.length - b.length);
+  //   if (sign === '//') return '.'.repeat(a.length / b.length);
+  //   if (sign === '*') return '.'.repeat(a.length * b.length);
+  // }
+
   function dotCalculator(equation) {
     const [a, sign, b] = equation.split(' ');
-    if (sign === '+') return '.'.repeat(a.length + b.length);
-    if (sign === '-') return '.'.repeat(a.length - b.length);
-    if (sign === '//') return '.'.repeat(a.length / b.length);
-    if (sign === '*') return '.'.repeat(a.length * b.length);
+    return '.'.repeat(eval([a.length, sign.slice(-1), b.length].join('')));
   }
   console.log(dotCalculator("..... + ..............."));
   console.log(dotCalculator("..... * .."));
