182bb530152da174ebb0ae61266b626b15ce83a3
h4y6 <h4y6@i.ua>
2024-09-19 16:24:45 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2542,3 +2542,27 @@ Examples (Input => Output)
 
   // console.log(prevMultOfThree(34));
 }
+{ /** 7 kyu  Who is the killer?
+  Who is the killer?
+  Some people have been killed!
+  You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+  
+  Task.
+  Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+  
+  {'James': ['Jacob', 'Bill', 'Lucas'],
+   'Johnny': ['David', 'Kyle', 'Lucas'],
+   'Peter': ['Lucy', 'Kyle']}
+  and also a list of the names of the dead people:
+  
+  ['Lucas', 'Bill']
+  return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill' */
+
+  function killer(suspectInfo, dead) {
+    const keys = Object.keys(suspectInfo);
+    for (const key of keys) {
+      if (suspectInfo[key].includes(...dead)) return key;
+    }
+  }
+  console.log(killer({ 'James': ['Jacob', 'Bill', 'Lucas'], 'Johnny': ['David', 'Kyle', 'Lucas'], 'Peter': ['Lucy', 'Kyle'] }, ['Lucas', 'Bill']));
+}
