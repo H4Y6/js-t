3f4b5a66bc4a89203ba377e95b3d0d910057f601
h4y6 <h4y6@i.ua>
2024-09-19 15:51:43 +0300

    new file:   js-2-2/js/12-examples.js, modified:   js-2-2/index.html

---
+++ js-2-2/js/12-examples.js
@@ -0,0 +1,39 @@
+const wordsA = ['kodje', 'jow', 'geomy', 'plevs'];
+const wordsB = ['woodjic', 'joke', 'glemmy'];
+
+const findLongest = function () {
+  let long = '';
+
+  for (const el of arguments[0]) {
+    long = el.length > long.length ? el : long;
+  }
+
+  return long;
+};
+console.log(findLongest([...wordsA, ...wordsB]));
+
+
+const array = ['dolt', 'golf', 'lef', 'folk', 'dell', 'pig'];
+const arr = ['golf', 'folk', 'bill', 'dell', 'pig', 'well'];
+
+const getCommon = function (arr1, arr2) {
+  const common = [];
+
+  for (const str of arr1) {
+    if (arr2.includes(str)) common.push(str);
+  }
+
+  return common;
+};
+console.log(getCommon(array, arr));
+
+const getAverage = function (arr) {
+  let sum = 0;
+
+  for (const el of arr) {
+    sum += el;
+  }
+
+  return sum / arr.length;
+};
+console.log(getAverage([18, 16, 22, 29, 41, 16, 22, 23]));
