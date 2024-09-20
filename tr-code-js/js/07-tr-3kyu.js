d9dc51d5cdc178ae0511074c40c87599109307ff
h4y6 <h4y6@i.ua>
2024-09-20 13:53:22 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2605,3 +2605,17 @@ function gordon(a) {
   return a.toUpperCase().replace(/(\w+)/g, '$&!!!!').replace(/A/g, '@').replace(/[EIOU]/g, '*');
 }
 // console.log(gordon('What feck damn cake'));
+
+{ /** 7 kyu  Return substring instance count
+  Complete the solution so that it returns the number of times the search_text is found within the full_text. Overlap is not permitted : "aaa" contains 1 instance of "aa", not 2.
+    Usage example:
+    full_text = "aa_bb_cc_dd_bb_e", search_text = "bb"
+      ---> should return 2 since "bb" shows up twice
+    
+  full_text = "aaabbbcccc", search_text = "bbb"
+      ---> should return 1    */
+
+  const solution = (fullText, searchText) => fullText.split(searchText).length - 1;
+
+  console.log(solution('abcdeb', 'b'));
+}
