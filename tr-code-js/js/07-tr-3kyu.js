ed65fc13382d94cbc742f33eee715f17d3ddd335
h4y6 <h4y6@i.ua>
2024-09-22 23:04:30 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2954,3 +2954,18 @@ Examples
 
   // console.log(numObj([118, 117, 120]));
 }
+{ /** 7 kyu  Exclamation marks series #5: Remove all exclamation marks from the end of words
+  Remove all exclamation marks from the end of words. Words are separated by a single space. There are no exclamation marks within a word.
+    Examples
+  remove("Hi!") === "Hi"
+  remove("Hi!!!") === "Hi"
+  remove("!Hi") === "!Hi"
+  remove("!Hi!") === "!Hi"
+  remove("Hi! Hi!") === "Hi Hi"
+  remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi" */
+
+  function remove(string) {
+    return string.replace(/(\w+)(!*)/g, (_, a, b) => a);
+  }
+  console.log(remove('!!!Hi !!hi!!! !hi'));
+}
