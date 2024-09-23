5de48997ce7decaa64c86551964963b88830d8fc
h4y6 <h4y6@i.ua>
2024-09-23 19:57:10 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -2994,7 +2994,8 @@ Examples
 
   // const rotate = str => [...str].map((e, i, arr) => str = str.slice(1) + arr[i]);
   // const rotate = str => [...str].map(e => str = str.slice(1) + str.slice(0, 1));
-  const rotate = str => [...str].map(() => str = str.slice(1) + str.slice(0, 1));
+  // const rotate = str => [...str].map(() => str = str.slice(1) + str.slice(0, 1));
+  const rotate = str => [...str].map((e, i) => str.slice(i += 1) + str.slice(0, i));
 
-  console.log(rotate('Hello'));
+  // console.log(rotate('Hello'));
 }
