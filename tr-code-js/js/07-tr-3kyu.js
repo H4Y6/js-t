d17fc6aa089665968873b3cb8aa079ad5c35996e
h4y6 <h4y6@i.ua>
2024-09-24 18:09:27 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3039,5 +3039,51 @@ Examples
   const looseChange = cents => (e => ({ Quarters: e / 25 ^ 0, Dimes: e % 25 / 10 ^ 0, Nickels: e % 25 % 10 / 5 ^ 0, Pennies: e % 5 ^ 0, }))
     (Math.sign(cents) + 1 ? cents : 0);
 
-  console.log(looseChange(66));
+  // console.log(looseChange(66));
+}
+{ /** 7 kyu  Moves in squared strings (I)
+  This kata is the first of a sequence of four about "Squared Strings".
+  
+  You are given a string of n lines, each substring being n characters long: For example:
+  
+  s = "abcd\nefgh\nijkl\nmnop"
+  
+  We will study some transformations of this square of strings.
+  
+  Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+  vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+  Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+   hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+  or printed:
+  
+  vertical mirror   |horizontal mirror   
+  abcd --> dcba     |abcd --> mnop 
+  efgh     hgfe     |efgh     ijkl 
+  ijkl     lkji     |ijkl     efgh 
+  mnop     ponm     |mnop     abcd 
+  Task:
+  Write these two functions
+  and
+  
+  high-order function oper(fct, s) where
+  
+  fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+  
+  Examples:
+  s = "abcd\nefgh\nijkl\nmnop"
+  oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+  oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+  Note:
+  The form of the parameter fct in oper changes according to the language. You can see each form according to the language in "Sample Tests". */
+
+  function vertMirror(strng) {
+    return strng.split('\n').map(e => [...e].reverse().join('')).join('\n');
+  }
+  function horMirror(strng) {
+    return strng.split('\n').reverse().join('\n');
+  }
+  function oper(fct, s) {
+    return fct(s);
+  }
+  console.log(oper(vertMirror, "abcd\nefgh\nijkl\nmnop"));
 }
