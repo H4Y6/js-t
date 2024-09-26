1c9052d52ab71a6e3c4bc04f562428d04057e573
h4y6 <h4y6@i.ua>
2024-09-26 21:50:11 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3162,5 +3162,28 @@ Examples
   // const rakeGarden = garden => garden.split(' ').map(e => e !== 'rock' ? 'gravel' : e).join(' ');
   const rakeGarden = garden => garden.replace(/(?!rock\b)\b\w+/g, 'gravel');
 
-  console.log(rakeGarden('gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel'));
+  // console.log(rakeGarden('gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel'));
+}
+{ /** 7 kyu  "Very Even" Numbers.
+  Write a function that returns true if the number is a "Very Even" number.
+  
+  If a number is a single digit, then it is simply "Very Even" if it itself is even.
+  
+  If it has 2 or more digits, it is "Very Even" if the sum of its digits is "Very Even".
+  
+  Examples
+  number = 88 => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd 
+  
+  number = 222 => returns true -> 2 + 2 + 2 = 6 => 6 is even
+  
+  number = 5 => returns false
+  
+  number = 841 => returns true -> 8 + 4 + 1 = 13 -> 1 + 3 => 4 is even
+  Note: The numbers will always be 0 or positive integers!    */
+
+  function isVeryEvenNumber(n) {
+    if ((n + '').length < 2) return !(n % 2);
+    return isVeryEvenNumber((n + '').split('').reduce((sum, e) => sum + +e, 0));
+  }
+  console.log(isVeryEvenNumber(7775));
 }
