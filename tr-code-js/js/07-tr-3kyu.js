0e909dc55cdddf7a65130cabdc8ea6912b098c6e
h4y6 <h4y6@i.ua>
2024-09-27 17:57:44 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3379,5 +3379,23 @@ Do not expect any negative or invalid inputs. */
   }
 
   const block = new Block([2, 4, 6]);
-  console.log(block.getWidth());
+  // console.log(block.getWidth());
+}
+{ /** 7 kyu  Unique Sum
+  Given a list of integers values, your job is to return the sum of the values; however, if the same integer value appears multiple times in the list, you can only count it once in your sum.
+  
+  For example:
+  
+  [ 1, 2, 3] ==> 6
+  
+  [ 1, 3, 8, 1, 8] ==> 12
+  
+  [ -1, -1, 5, 2, -7] ==> -1
+  
+  [] ==> null */
+
+  function uniqueSum(arr) {
+    return [...new Set(arr)].reduce((sum, e) => sum + e, 0);
+  }
+  console.log(uniqueSum([-1, -1, 5, 2, -7]));
 }
