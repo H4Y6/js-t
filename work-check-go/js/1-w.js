7e77fdb4d99e602dc478c7846445510f95191578
h4y6 <h4y6@i.ua>
2024-09-28 16:09:00 +0300

    modified:   work-check-go/js/1-w.js

--- work-check-go/js/1-w.js
+++ work-check-go/js/1-w.js
@@ -256,3 +256,145 @@ function checkForName(fullName, name) {
 function checkForSpam(message) {
   return /sale|spam/gi.test(message);
 }
+
+{
+  const bookShelf = {
+    authors: ["Bernard Cornwell", "Robert Sheckley"],
+    getAuthors() { return this.authors; },
+    addAuthor(authorName) { this.authors.push(authorName); },
+  };
+  // console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley"]
+  bookShelf.addAuthor("Tanith Lee");
+  // console.log(bookShelf.getAuthors());
+
+  const pizzaPalace = {
+    pizzas: ["Supercheese", "Smoked", "Four meats"],
+    checkPizza(pizzaName) {
+      return this.pizzas.includes(pizzaName);
+    },
+    order(pizzaName) {
+      const isPizzaAvailable = this.checkPizza(pizzaName);
+
+      if (!isPizzaAvailable) {
+        return `Sorry, there is no pizza named «${pizzaName}»`;
+      }
+
+      return `Order accepted, preparing «${pizzaName}» pizza`;
+    },
+  };
+
+  // console.log(pizzaPalace.checkPizza('Smoked'));
+  // console.log(pizzaPalace.order('Four meats'));
+}
+{
+  const customer = {
+    username: "Mango",
+    balance: 24000,
+    discount: 0.1,
+    orders: ["Burger", "Pizza", "Salad"],
+    getBalance() {
+      return this.balance;
+    },
+    getDiscount() {
+      return this.discount;
+    },
+    setDiscount(value) {
+      this.discount = value;
+    },
+    getOrders() {
+      return this.orders;
+    },
+    addOrder(cost, order) {
+      this.balance -= cost - cost * this.discount;
+      this.orders.push(order);
+    },
+  };
+
+  customer.setDiscount(0.15);
+  // console.log(customer.getDiscount()); // 0.15
+  customer.addOrder(5000, "Steak");
+  // console.log(customer.getBalance()); // 19750
+  // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+}
+{
+  const historyService = {
+    orders: [
+      { email: "jacob@hotmail.com", dish: "Burrito" },
+      { email: "solomon@topmail.net", dish: "Burger" },
+      { email: "artemis@coldmail.net", dish: "Pizza" },
+      { email: "solomon@topmail.net", dish: "Apple pie" },
+      { email: "jacob@hotmail.com", dish: "Taco" },
+    ],
+    getOrdersLog() {
+      return this.orders
+        .map(order => `email: ${order.email} dish: ${order.dish}`)
+        .join(" - ");
+    },
+    getEmails() {
+      const emails = this.orders.map(order => order.email);
+      const uniqueEmails = new Set(emails);
+      return [...uniqueEmails];
+    },
+    getOrdersByEmail(email) {
+      return this.orders.filter(order => order.email === email);
+    },
+  };
+}
+{
+  const animal = { legs: 4, };
+  const dog = Object.create(animal); dog.name = "Mango";
+  // console.log(dog);
+  // { name: 'Mango', __proto__: animal }
+  // console.log(animal.isPrototypeOf(dog));
+
+  // console.log(dog.hasOwnProperty("name"));   // true
+  // console.log(dog.name); // 'Mango' 
+  // console.log(dog.hasOwnProperty("legs")); // false console.log(dog.legs); // 4
+
+  const parent = {
+    name: "Stacey",
+    surname: "Moore",
+    age: 54,
+    heritage: "Irish",
+  };
+
+  const child = Object.create(parent);
+  child.name = "Jason";
+  child.age = 27;
+  // console.log(child);
+}
+{
+  const ancestor = {
+    name: "Paul",
+    age: 83,
+    surname: "Dawson",
+    heritage: "Irish",
+  };
+
+  const parent = Object.create(ancestor);
+  parent.name = "Stacey";
+  parent.surname = "Moore";
+  parent.age = 54;
+
+  const child = Object.create(parent);
+  child.name = "Jason";
+  child.age = 27;
+}
+{
+  class User {
+    constructor(name, email) {
+      this.name = name;
+      this.email = email;
+    }
+  }
+  const mango = new User("Mango", "mango@mail.com");
+  // console.log(mango);
+
+  class Car {
+    constructor(brand, model, price) {
+      this.brand = brand;
+      this.model = model;
+      this.price = price;
+    }
+  }
+}
