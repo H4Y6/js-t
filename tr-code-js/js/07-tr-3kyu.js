a2557f1cc7b113ca7a6ed49722a0daadef03eaa5
h4y6 <h4y6@i.ua>
2024-10-01 19:37:19 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3686,3 +3686,20 @@ Do not expect any negative or invalid inputs. */
 
   // console.log(wordSearch("ab", ["za", "ab", "abc", "zab", "zc"]));
 }
+{ /** 8 kyu  Training JS #16: Methods of String object--slice(), substring() and substr()
+  Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+The first mission: Traversing arr, find the shortest string length.
+
+The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+for example:
+cutIt(["ab","cde","fgh"]) should return ["ab","cd","fg"]
+cutIt(["abc","defgh","ijklmn"]) should return ["abc","def","ijk"]
+cutIt(["codewars","javascript","java"]) should return ["code","java","java"]           */
+
+  const cutIt = arr => arr.map(e => e.substr(0, Math.min(...arr.map(e => e.length))));
+
+  console.log(cutIt(["abc", "defgh", "ijklmn"]));
+}
+
