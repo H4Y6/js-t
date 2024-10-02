215a28878af148dbd1d5e12514caf5a98e2c1ed5
h4y6 <h4y6@i.ua>
2024-10-02 22:33:01 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3781,3 +3781,16 @@ cutIt(["codewars","javascript","java"]) should return ["code","java","java"]    
   // console.log(dog);
   // console.log(cat.introduce());
 }
+{ /** 7 kyu
+  For every good kata idea there seem to be quite a few bad ones!
+  
+  In this kata you need to check the provided 2 dimensional array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+  
+  The sub arrays may not be the same length.
+  
+  The solution should be case insensitive (ie good, GOOD and gOOd all count as a good idea). All inputs may not be strings. */
+
+  const well = x => (length => !length ? 'Fail!' : length > 2 ? 'I smell a series!' : 'Publish!')(x.flat().filter(e => /good/gi.test(e)).length);
+
+  console.log(well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']]));
+}
