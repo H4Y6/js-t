118f695ed96b8a46d178bdf9a1441684bc928015
h4y6 <h4y6@i.ua>
2024-10-03 15:47:18 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3825,3 +3825,20 @@ chain(2, [add, mult]);
 
   // console.log(chain(2, [add, mult]));
 }
+{ /** 7 kyu  Substring fun
+  Complete the function that takes an array of words.
+  
+  You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+  
+  For example:
+  
+  ["yoda", "best", "has"]  -->  "yes"
+    ^        ^        ^
+    n=0     n=1     n=2
+  Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters. */
+
+  function nthChar(words) {
+    return words.map((e, i, ar) => e[i]).join('');
+  }
+  console.log(nthChar(['yoda', 'best', 'has']));
+}
