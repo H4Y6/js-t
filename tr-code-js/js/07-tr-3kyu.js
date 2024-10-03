a3ebbb304e88bbaa2e24c08c57dd50ec63827d83
h4y6 <h4y6@i.ua>
2024-10-03 15:42:48 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3796,3 +3796,32 @@ cutIt(["codewars","javascript","java"]) should return ["code","java","java"]    
 
   // console.log(well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']]));
 }
+{
+  /** 7 kyu  Chain me
+Write a generic function chainer
+Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+
+function add(num) {
+ return num + 1;
+}
+
+function mult(num) {
+ return num * 30;
+}
+
+chain(2, [add, mult]);
+// returns 90; */
+
+  function add(num) {
+    return num + 1;
+  }
+
+  function mult(num) {
+    return num * 30;
+  }
+  const chain = (input, fs) => fs.reduce((res, func) => func(res), input);
+
+  // console.log(chain(2, [add, mult]));
+}
