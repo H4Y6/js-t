4cc7ffccdbda113ca98acc164c18343a2cf2111d
h4y6 <h4y6@i.ua>
2024-10-04 23:23:22 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3871,3 +3871,10 @@ chain(2, [add, mult]);
 
   // console.log(mean(["u", "6", "d", "1", "i", "w", "6", "s", "t", "4", "a", "6", "g", "1", "2", "w", "8", "o", "2", "0"]));
 }
+{ /** 7 kyu  Reverse list
+  Write reverseList function that simply reverses lists. */
+
+  function reverseList(arr) {
+    return arr.reverse();
+  }
+}
