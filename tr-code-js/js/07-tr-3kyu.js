a633590e9e4bda9c9e1c0b9d5a7e027f2c9f3f83
h4y6 <h4y6@i.ua>
2024-10-04 16:18:23 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -3854,3 +3854,16 @@ chain(2, [add, mult]);
   };
   // console.log(String.prototype.isLetter());
 }
+{ /** 7 kyu  Calculate mean and concatenate string
+  You will be given an array which will include both integers and characters.
+  
+  Return an array of length 2 with a[0] representing the mean of the ten integers as a floating point number. There will always be 10 integers and 10 characters. Create a single string with the characters and return it as a[1] while maintaining the original order.
+  
+  lst = ['u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0']
+  Here is an example of your return  
+  [3.6, "udiwstagwo"] */
+
+  const mean = lst => [lst.reduce((sum, e) => sum + (+e ? +e : 0), 0) / 10, lst.reduce((s, e) => s + e).replace(/\d/g, '')];
+
+  console.log(mean(["u", "6", "d", "1", "i", "w", "6", "s", "t", "4", "a", "6", "g", "1", "2", "w", "8", "o", "2", "0"]));
+}
