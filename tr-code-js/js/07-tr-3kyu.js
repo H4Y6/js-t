faba1591044adba0a9edbf7e9064019136721a76
h4y6 <h4y6@i.ua>
2024-10-09 17:55:11 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -4249,9 +4249,19 @@ chain(2, [add, mult]);
   // console.log(mango.isBlacklisted("poly@mail.com")); // true
 }
 
-{
-  function evenChars(string) {
-    return (string.length < 2 || string.length > 100) ? "invalid string" : string.split('').filter((e, i, ar) => i % 2);
-  }
+{ /** 7 kyu  Return a string's even characters.
+  Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
+  
+  For example:
+  
+  "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+  "a"             --> "invalid string" */
+
+  // function evenChars(string) {
+  //   return (string.length < 2 || string.length > 100) ? "invalid string" : string.split('').filter((e, i, ar) => i % 2);
+  // }
+
+  const evenChars = string => (string.length < 2 || string.length > 100) ? "invalid string" : string.split('').filter((e, i) => i % 2);
+
   console.log(evenChars("abcdefghijklm"));
 }
