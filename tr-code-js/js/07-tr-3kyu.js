5a1d49d2ad2e490d5bb6431bd68b81a3b7fc10f7
h4y6 <h4y6@i.ua>
2024-10-10 17:41:50 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -4344,5 +4344,37 @@ chain(2, [add, mult]);
     m < 10 ? m = '0' + m : m;
     return h + ':' + m;
   };
-  console.log(whatTimeIsIt(31));
+  // console.log(whatTimeIsIt(31));
+}
+{ /** 7 kyu  What dominates your array?
+  A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value that occurs in more than half of the elements of arr.
+  For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+  The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+  Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers, returns the dominator of arr. The function should return −1 if array does not have a dominator. All values in arr will be >=0.   */
+
+  // function dominator(arr) {
+  //   const res = [...new Set(arr)].map(e => [e, arr.filter(el => el === e).length]);
+  //   const max = Math.max(...res.map(e => e[1]));
+  //   return max > arr.length / 2 ? res.find(e => e[1] === max)[0] : -1;
+  // } /** time out */
+
+  // function dominator(arr) {
+  //   const res = [...new Set(arr)].map(e => [e, arr.filter(el => el === e).length]).sort((a, b) => b[1] - a[1])[0];
+  //   return res[1] > arr.length / 2 ? res[0] : -1;
+  // } /** time out */
+  function dominator(arr) { }
+  // console.log(dominator([3, 4, 3, 2, 3, 1, 3, 3]));
+}
+{
+  class Animal {
+    name = 'Coppy';
+  }
+
+  class Cat extends Animal {
+    speak() {
+      return this.name + ` meows.`;
+    }
+  }
+  const cat = new Cat;
+  // console.log(cat.speak());
 }
