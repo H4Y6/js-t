981ee45146435012c86ebfca7a5b42fcdf50c628
h4y6 <h4y6@i.ua>
2024-10-26 18:02:07 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -5103,10 +5103,17 @@ Notes:
       0,
     );
 
-  console.log(countDevelopers(list1));
+  // console.log(countDevelopers(list1));
 }
 
 {
+  /** 7 kyu    Coding Meetup #2 - Higher-Order Functions Series - Greet developer
+   You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+
+Hi < firstName here >, what do you like the most about < language here >?   */
+
   const list1 = [
     {
       firstName: 'Sofia',
@@ -5153,5 +5160,12 @@ Notes:
         }),
     );
 
-  console.log(greetDevelopers(list1));
+  // console.log(greetDevelopers(list1));
+
+  /**  */
+
+  function isRubyComing(list) {
+    return Boolean(list.find(d => d.language === 'Ruby'));
+  }
+  console.log(isRubyComing(list1));
 }
