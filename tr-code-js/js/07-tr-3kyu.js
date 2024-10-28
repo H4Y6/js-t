891893cdbc5cc4e44e0fca39b7f27a517d137235
h4y6 <h4y6@i.ua>
2024-10-28 17:40:39 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -5729,3 +5729,91 @@ Notes:
 
   // console.log(getAverageAge(list1));
 }
+{
+  /** 
+  6 kyu  Coding Meetup #13 - Higher-Order Functions Series - Is the meetup language-diverse?
+  You will be given an array of objects representing data about developers who have signed up to attend the next web development meetup that you are organising. Three programming languages will be represented: Python, Ruby and JavaScript.
+  
+  Your task is to return either:
+  
+      true if the number of meetup participants representing any of the three programming languages is ** at most 2 times higher than the number of developers representing any of the remaining programming languages**; or
+      false otherwise.  
+
+  your function should return false as the number of JavaScript developers (3) is 3 times higher than the number of Python developers (1). It can't be more than 2 times higher to be regarded as language-diverse.
+  
+  Notes:
+  
+      The strings representing all three programming languages will always be formatted in the same way (e.g. 'JavaScript' will always be formatted with upper-case 'J' and 'S'.
+      The input array will always be valid and formatted as in the example above.
+      Each of the 3 programming languages will always be represented. */
+
+  const list1 = [
+    {
+      firstName: 'Daniel',
+      lastName: 'J.',
+      country: 'Aruba',
+      continent: 'Americas',
+      age: 42,
+      language: 'Python',
+    },
+    {
+      firstName: 'Kseniya',
+      lastName: 'T.',
+      country: 'Belarus',
+      continent: 'Europe',
+      age: 22,
+      language: 'Ruby',
+    },
+    {
+      firstName: 'Sou',
+      lastName: 'B.',
+      country: 'Japan',
+      continent: 'Asia',
+      age: 43,
+      language: 'Ruby',
+    },
+    {
+      firstName: 'Hanna',
+      lastName: 'L.',
+      country: 'Hungary',
+      continent: 'Europe',
+      age: 95,
+      language: 'JavaScript',
+    },
+    {
+      firstName: 'Jayden',
+      lastName: 'P.',
+      country: 'Jamaica',
+      continent: 'Americas',
+      age: 18,
+      language: 'JavaScript',
+    },
+    {
+      firstName: 'Joao',
+      lastName: 'D.',
+      country: 'Portugal',
+      continent: 'Europe',
+      age: 25,
+      language: 'JavaScript',
+    },
+  ];
+
+  function isLanguageDiverse(list) {
+    const set = list
+      .map(d => d.language)
+      .filter((l, i, arr) => arr.indexOf(l) === i);
+
+    const max = Math.max(
+      ...set.map(l => list.filter(e => e.language === l).length),
+    );
+    return (
+      Math.max(
+        ...set
+          .map(l => list.filter(e => e.language === l).length)
+          .map(e => max / e),
+      ) <= 2
+    );
+  }
+
+  console.log(isLanguageDiverse(list1));
+}
