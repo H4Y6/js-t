a2a7ae182c9416fbaa32c52e3d104f9cbb7893af
h4y6 <h4y6@i.ua>
2024-10-28 16:42:25 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -5689,3 +5689,37 @@ Notes:
 
   // console.log(addUsername(list1));
 }
+{
+  /**  7 kyu  Coding Meetup #11 - Higher-Order Functions Series - Find the average age
+You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising. 
+write a function that returns the average age of developers (rounded to the nearest integer). In the example above your function should return 50 (number).
+
+Notes:
+    The input array will always be valid and formatted as in the example above.
+    Age is represented by a number which can be any positive integer. */
+
+  const list1 = [
+    {
+      firstName: 'Maria',
+      lastName: 'Y.',
+      country: 'Cyprus',
+      continent: 'Europe',
+      age: 30,
+      language: 'Java',
+    },
+    {
+      firstName: 'Victoria',
+      lastName: 'T.',
+      country: 'Puerto Rico',
+      continent: 'Americas',
+      age: 70,
+      language: 'Python',
+    },
+  ];
+
+  function getAverageAge(list) {
+    return Math.round(list.reduce((av, e) => av + e.age, 0) / list.length);
+  }
+
+  console.log(getAverageAge(list1));
+}
