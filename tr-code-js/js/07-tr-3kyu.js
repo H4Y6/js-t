d4807736fcf05242cb6628ca6642903b7b127c34
h4y6 <h4y6@i.ua>
2024-11-02 13:52:07 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6207,3 +6207,20 @@ The array elements will all be numbers. The numbers will also all be unique and 
 
   // console.log(neutralise('-++-', '-+-+'));
 }
+{
+  /** 7 kyu  Multiples!
+ Make a program that takes a value (x) and returns "Bang" if the number is divisible by 3, "Boom" if it is divisible by 5, "BangBoom" if it divisible by 3 and 5, and "Miss" if it isn't divisible by any of them. Note: Your program should only return one value
+Ex: Input: 105 --> Output: "BangBoom" Ex: Input: 9 --> Output: "Bang" Ex:Input: 25 --> Output: "Boom"   */
+
+  function multiple(x) {
+    return !(x % 3) && !(x % 5)
+      ? 'BangBoom'
+      : !(x % 3)
+      ? 'Bang'
+      : !(x % 5)
+      ? 'Boom'
+      : 'Miss';
+  }
+
+  console.log(multiple(33));
+}
