b25f6c4bf991f40fdd43796341a69ad6105e6373
h4y6 <h4y6@i.ua>
2024-11-02 17:45:00 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6244,16 +6244,42 @@ Ex: Input: 105 --> Output: "BangBoom" Ex: Input: 9 --> Output: "Bang" Ex:Input: 
   // console.log(multiple(130));
 }
 {
+  /** 
+  7 kyu  Merge two arrays
+  Write a function that combines two arrays by alternatingly taking elements from each array in turn.
+  
+  Examples:
+  
+  [a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+  
+  [1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+  
+  Points:
+  
+      The arrays may be of different lengths, with at least one character/digit.
+      One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).   */
+
+  // function mergeArrays(a, b) {
+  //   return a.length < b.length
+  //     ? a
+  //         .map((e, i) => [e, b[i]])
+  //         .flat()
+  //         .concat(b.slice(a.length))
+  //     : a
+  //         .slice(0, b.length)
+  //         .map((e, i) => [e, b[i]])
+  //         .flat()
+  //         .concat(a.slice(b.length));
+  // }
+
   function mergeArrays(a, b) {
-    return a.length < b.length
-      ? a
-          .map((e, i) => [e, b[i]])
-          .flat()
-          .concat(b.slice(a.length))
+    return a.length === b.length
+      ? a.reduce((res, e, i) => [...res, e, b[i]], [])
+      : a.length < b.length
+      ? a.reduce((res, e, i) => [...res, e, b[i]], []).concat(b.slice(a.length))
       : a
           .slice(0, b.length)
-          .map((e, i) => [e, b[i]])
-          .flat()
+          .reduce((res, e, i) => [...res, e, b[i]], [])
           .concat(a.slice(b.length));
   }
 
