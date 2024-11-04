20df342d358060858cf60f5be57133539a50c0bd
h4y6 <h4y6@i.ua>
2024-11-04 21:38:30 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6346,3 +6346,29 @@ Ex: Input: 105 --> Output: "BangBoom" Ex: Input: 9 --> Output: "Bang" Ex:Input: 
 
   // console.log(calcType(1, 2, -1));
 }
+{
+  /** 7 kyuFix My Phone Numbers!
+Fix My Phone Numbers
+Oh thank goodness you're here! The last intern has completely ruined everything!
+
+All of our customer's phone numbers have been scrambled, and we need those phone numbers to annoy them with endless sales calls!
+
+The Format
+Phone numbers are stored as strings and comprise 11 digits, eg '02078834982' and must always start with a 0.
+
+However, something strange has happened and now all of the phone numbers contain lots of random characters, whitespace and some are not phone numbers at all!
+
+For example, '02078834982' has somehow become 'efRFS:)0207ERGQREG88349F82!' and there are lots more lines that we need to check.
+
+The Task
+Given a string, you must decide whether or not it contains a valid phone number. If it does, return the corrected phone number as a string ie. '02078834982' with no whitespace or special characters, else return "Not a phone number".    */
+
+  function isItANum(str) {
+    return (e =>
+      e.length === 11 && e.charAt() === '0' ? e : 'Not a phone number')(
+      str.replace(/[^0-9]/g, ''),
+    );
+  }
+
+  console.log(isItANum('S:)0207ERGQREG88349F82!efRF)'));
+}
