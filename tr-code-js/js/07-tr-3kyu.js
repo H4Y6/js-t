7f4d1ab93d0236a190d3c184fd93344fab10e76e
h4y6 <h4y6@i.ua>
2024-11-04 21:42:41 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6363,12 +6363,17 @@ For example, '02078834982' has somehow become 'efRFS:)0207ERGQREG88349F82!' and 
 The Task
 Given a string, you must decide whether or not it contains a valid phone number. If it does, return the corrected phone number as a string ie. '02078834982' with no whitespace or special characters, else return "Not a phone number".    */
 
-  function isItANum(str) {
-    return (e =>
-      e.length === 11 && e.charAt() === '0' ? e : 'Not a phone number')(
+  // function isItANum(str) {
+  //   return (e =>
+  //     e.length === 11 && e.charAt() === '0' ? e : 'Not a phone number')(
+  //     str.replace(/[^0-9]/g, ''),
+  //   );
+  // }
+
+  const isItANum = str =>
+    (e => (e.length === 11 && e.charAt() === '0' ? e : 'Not a phone number'))(
       str.replace(/[^0-9]/g, ''),
     );
-  }
 
   console.log(isItANum('S:)0207ERGQREG88349F82!efRF)'));
 }
