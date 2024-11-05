d265542921d39bdd621e2c8cf922c47e32b8cad8
h4y6 <h4y6@i.ua>
2024-11-05 14:54:07 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6388,3 +6388,22 @@ Given a string, you must decide whether or not it contains a valid phone number.
 
   // console.log(isItANum('S:)0207ERGQREG88349F82!efRF)'));
 }
+{
+  /** 6 kyu  Round by 0.5 steps
+Round any given number to the closest 0.5 step
+I.E.
+solution(4.2) = 4
+solution(4.3) = 4.5
+solution(4.6) = 4.5
+solution(4.8) = 5
+
+Round up if number is as close to previous and next 0.5 steps.
+
+solution(4.75) == 5   */
+
+  function solution(n) {
+    return Math.round((n % Math.trunc(n)) * 2) / 2 + Math.trunc(n);
+  }
+
+  console.log(solution(4.25));
+}
