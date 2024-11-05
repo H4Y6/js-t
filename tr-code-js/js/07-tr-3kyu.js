a4f7dce50ebabe94e6eecdcd3da447c67351e3a8
h4y6 <h4y6@i.ua>
2024-11-05 16:32:13 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6433,3 +6433,25 @@ Note : zero to the power of zero equals one in this kata */
 
   // console.log(power(-1, 40));
 }
+{
+  /**  7 kyu Vampire Numbers
+Vampire Numbers
+
+Our loose definition of Vampire Numbers can be described as follows:
+
+6 * 21 = 126
+# 6 and 21 would be valid 'fangs' for a vampire number as the 
+# digits 6, 1, and 2 are present in both the product and multiplicands
+
+10 * 11 = 110
+# 110 is not a vampire number since there are three 1's in the
+# multiplicands, but only two 1's in the product
+
+Create a function that can receive two 'fangs' and determine if the product of the two is a valid vampire number.  */
+
+  const vampireTest = (a, b) =>
+    [...String(a * b)].sort().join('') ===
+    [...(a.toString() + b)].sort().join('');
+
+  console.log(vampireTest(6, 21));
+}
