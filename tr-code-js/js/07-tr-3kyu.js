82c4e5b6921932f7865e55bfd89896154902d251
h4y6 <h4y6@i.ua>
2024-11-08 21:52:43 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6485,3 +6485,18 @@ function tankvol(h, d, vt, r = d / 2) {
 
   // console.log(containAllRots("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]))
 }
+{
+  /** 7 kyuFind the index of the second occurrence of a letter in a string
+In this kata, you need to write a function that takes a string and a letter as input and then returns the index of the second occurrence of that letter in the string. If there is no such letter in the string, then the function should return -1. If the letter occurs only once in the string, then -1 should also be returned.
+
+Examples:
+
+secondSymbol('Hello world!!!','l')  --> 3
+secondSymbol('Hello world!!!', 'A') --> -1   */
+
+  function secondSymbol(s, symbol) {
+    return s.replace(symbol, '_').indexOf(symbol);
+  }
+
+  console.log(secondSymbol('Hello world!!!', 'l'));
+}
