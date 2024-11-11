5ae19dea921572b0941601dd8499935dd611def8
h4y6 <h4y6@i.ua>
2024-11-11 21:07:44 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6706,3 +6706,17 @@ counterEffect("0000") // [[0],[0],[0],[0]]   */
 
   // console.log(counterEffect('1250'));
 }
+{
+  /** 7 kyu  How many times should I go?
+Lot of museum allow you to be a member, for a certain amount amount_by_year you can have unlimitted acces to the museum.
+
+In this kata you should complete a function in order to know after how many visit it will be better to take an annual pass. The function take 2 arguments annual_price and individual_price.    */
+
+  function howManyTimes(annualPrice, individualPrice) {
+    return (annualPrice / individualPrice) % 1
+      ? (1 + annualPrice / individualPrice) ^ 0
+      : annualPrice / individualPrice;
+  }
+
+  console.log(howManyTimes(80, 15));
+}
