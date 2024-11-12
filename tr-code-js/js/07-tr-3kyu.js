e37e25b2340b8a4e2781e884505f1ad9cb948906
h4y6 <h4y6@i.ua>
2024-11-12 16:51:13 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6807,5 +6807,40 @@ function Dog(name, breed, sex, age){
       .map((e, i) => String.fromCharCode(97 + i))
       .join();
 
-  console.log(createalpabet());
+  // console.log(createalpabet());
+
+  const repeatStr = (str, n) => Array(n + 1).join(str);
+
+  // console.log(repeatStr('tj ', 5));
+
+  /**Task
+Coding in function bigToSmall. function accept 1 parameter arr(2D number array).
+
+Your task is: First to all, refer to the example above, flat output arr to a one-dimensional array.
+
+And then sort array in descending order.
+
+Finally, use the separator ">" to connect the elements into a string.
+
+Don't complain about the situation like 1>1 is not reasonable, it is just a separator.
+
+Some example:
+
+bigToSmall([[1,2],[3,4],[5,6]]) should return "6>5>4>3>2>1"
+bigToSmall([[1,3,5],[2,4,6]]) should return "6>5>4>3>2>1"
+bigToSmall([[1,1],[1],[1,1]]) should return "1>1>1>1>1"   */
+
+  const bigToSmall = arr =>
+    []
+      .concat(...arr)
+      .sort((a, b) => b - a)
+      .join('>');
+
+  // console.log(
+  //   bigToSmall([
+  //     [1, 2],
+  //     [3, 4],
+  //     [5, 6],
+  //   ]),
+  // );
 }
