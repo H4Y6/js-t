8660eae2b49f18e3829cf9a309a8fe61c2f25793
h4y6 <h4y6@i.ua>
2024-11-12 14:59:56 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -6727,3 +6727,32 @@ In this kata you should complete a function in order to know after how many visi
 
   // console.log(howManyTimes(80, 15));
 }
+{
+  /** 7 kyu  Find sum of top-left to bottom-right diagonals
+Given a square matrix (i.e. an array of subarrays), find the sum of values from the first value of the first array, the second value of the second array, the third value of the third array, and so on...
+
+Examples
+array = [[1, 2],
+         [3, 4]]
+
+diagonal sum: 1 + 4 = 5
+array = [[5, 9, 1, 0],
+         [8, 7, 2, 3],
+         [1, 4, 1, 9],
+         [2, 3, 8, 2]]
+
+diagonal sum: 5 + 7 + 1 + 2 = 15   */
+
+  function diagonalSum(matrix) {
+    return matrix.reduce((sum, e, i) => sum + e[i], 0);
+  }
+
+  console.log(
+    diagonalSum([
+      [5, 9, 1, 0],
+      [8, 7, 2, 3],
+      [1, 4, 1, 9],
+      [2, 3, 8, 2],
+    ]),
+  );
+}
