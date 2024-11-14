383e0b0c895d523cd04a12b2c7f7ad636bb1f954
h4y6 <h4y6@i.ua>
2024-11-14 23:12:21 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7075,8 +7075,12 @@ getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88] */
   //   return a.map((e, i) => (e > b[i] ? e : (e = b[i])));
   // }
 
-  const getLargerNumbers = (a, b) =>
-    a.map((e, i) => (e > b[i] ? e : (e = b[i])));
+  // const getLargerNumbers = (a, b) =>
+  //   a.map((e, i) => (e > b[i] ? e : (e = b[i])));
+
+  // const getLargerNumbers = (a, b) => a.map((e, i) => (e > b[i] ? e : b[i]));
+
+  const getLargerNumbers = (a, b) => a.map((e, i) => Math.max(e, b[i]));
 
   console.log(getLargerNumbers([13, 64, 15, 17, 88], [23, 14, 53, 17, 80]));
 }
