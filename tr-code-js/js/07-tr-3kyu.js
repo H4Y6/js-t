e1a1de70e5de28614c36a65157e32066f816bd9a
h4y6 <h4y6@i.ua>
2024-11-14 18:28:40 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7043,9 +7043,20 @@ The brute force approach would be to generate all the permutations of the string
   // console.log(permuteAPalindrome('aedamm'));
 }
 {
-  function keysAndValues(data) {
-    return [Object.keys(data), Object.values(data)];
-  }
+  /**7 kyu Get key/value pairs as arrays
+Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
 
-  console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
+Example:
+
+keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). For extra style, can you get your method to check for objects that extend their prototype?
+
+ */
+  // function keysAndValues(data) {
+  //   return [Object.keys(data), Object.values(data)];
+  // }
+
+  const keysAndValues = data => [Object.keys(data), Object.values(data)];
+
+  // console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
 }
