f7eee3124faeb33286ea3ed3c7d1a71fc0df8b27
h4y6 <h4y6@i.ua>
2024-11-15 16:11:47 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7082,5 +7082,35 @@ getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88] */
 
   const getLargerNumbers = (a, b) => a.map((e, i) => Math.max(e, b[i]));
 
-  console.log(getLargerNumbers([13, 64, 15, 17, 88], [23, 14, 53, 17, 80]));
+  // console.log(getLargerNumbers([13, 64, 15, 17, 88], [23, 14, 53, 17, 80]));
+}
+{
+  /**6 kyu  More Zeros than Ones
+Create a moreZeros function which will receive a string for input, and return an array (or null terminated string in C) containing only the characters from that string whose binary representation of its ASCII value consists of more zeros than ones.
+
+You should remove any duplicate characters, keeping the first occurrence of any such duplicates, so they are in the same order in the final array as they first appeared in the input string.
+
+Examples
+
+'abcde' === ["1100001", "1100010", "1100011", "1100100", "1100101"]
+               True       True       False      True       False
+                   
+        --> ['a','b','d']
+    
+'DIGEST'--> ['D','I','E','T']
+All input will be valid strings of length > 0. Leading zeros in binary should not be counted. */
+
+  function moreZeros(s) {
+    return [...new Set(s)]
+      .map((e, i, ar) =>
+        [...e.charCodeAt().toString(2)].filter(e => e === '1').length /
+          [...e.charCodeAt().toString(2)].length <
+        0.5
+          ? ar[i]
+          : '',
+      )
+      .filter(e => e);
+  }
+
+  console.log(moreZeros('abcdeabcde'));
 }
