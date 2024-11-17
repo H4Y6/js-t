d2b50f34209e748f1ef75e29cb5695ee31a108e6
h4y6 <h4y6@i.ua>
2024-11-17 22:05:58 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7302,3 +7302,19 @@ NOTICE:
   // console.log(multiples(12, 3, 60));
   // console.log(multiples(30, 16, 1399));
 }
+{
+  /** 
+7 kyu  Borrower Speak
+The borrowers are tiny tiny fictional people. As tiny tiny people they have to be sure they aren't spotted, or more importantly, stepped on.
+
+As a result, the borrowers talk very very quietly. They find that capitals and punctuation of any sort lead them to raise their voices and put them in danger.
+
+The young borrowers have begged their parents to stop using caps and punctuation.
+
+Change the input text s to new borrower speak. */
+
+  function borrow(s) {
+    return s.toLowerCase().replace(/[^a-z]/g, '');
+  }
+  console.log(borrow('WhAt! FiCK! DaMn CAke?'));
+}
