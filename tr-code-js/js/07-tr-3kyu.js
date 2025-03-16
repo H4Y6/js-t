c1969024300b704813d3c712ef56f1a972cc2e3c
h4y6 <h4y6@i.ua>
2025-03-16 23:30:42 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7573,11 +7573,33 @@ roundIt(34.56) should return 35   */
   // console.log(howManyTimes(40, 25));
 }
 {
+  /** 7 kyu	What dominates your array?
+	A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value that occurs in more than half of the elements of arr.
+	For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+	The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+	Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers, returns the dominator of arr. The function should return −1 if array does not have a dominator. All values in arr will be >=0. */
+
+  // function dominator(arr) {
+  //   return (ar =>
+  //     Math.max(...ar) > arr.length / 2 ? arr[ar.indexOf(Math.max(...ar))] : -1)(
+  //     arr.map(e => arr.filter(el => el === e).length),
+  //   );
+  // }  /** This code timed out: */
+
   function dominator(arr) {
-    return (ar =>
-      Math.max(...ar) > arr.length / 2 ? arr[ar.indexOf(Math.max(...ar))] : -1)(
-      arr.map(e => arr.filter(el => el === e).length),
-    );
+    const freqMap = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+      freqMap.set(arr[i], (freqMap.get(arr[i]) || 0) + 1);
+    }
+
+    for (let [key, value] of freqMap) {
+      if (value > arr.length / 2) {
+        return key;
+      }
+    }
+
+    return -1;
   }
 
   console.log(dominator([3, 4, 3, 2, 3, 1, 3, 3]));
