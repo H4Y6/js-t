263f1a8882f17b265b4b4ace2d17d53e7f748d9e
h4y6 <h4y6@i.ua>
2025-03-25 22:34:10 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7656,35 +7656,45 @@ roundIt(34.56) should return 35   */
   // console.log(replaceAll('erty ploh', 'r', 'o'));
 }
 {
+  //   function decode(code, n) {
+  //     const alphabet = [
+  //       'a',
+  //       'b',
+  //       'c',
+  //       'd',
+  //       'e',
+  //       'f',
+  //       'g',
+  //       'h',
+  //       'i',
+  //       'j',
+  //       'k',
+  //       'l',
+  //       'm',
+  //       'n',
+  //       'o',
+  //       'p',
+  //       'q',
+  //       'r',
+  //       's',
+  //       't',
+  //       'u',
+  //       'v',
+  //       'w',
+  //       'x',
+  //       'y',
+  //       'z',
+  //     ];
+  //
+  //     const nStr = ('' + n).split('');
+  //
+  //     return code
+  //       .map((e, i, ar) => alphabet[e - nStr[i % nStr.length] - 1])
+  //       .join('');
+  //   }
+
   function decode(code, n) {
-    const alphabet = [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h',
-      'i',
-      'j',
-      'k',
-      'l',
-      'm',
-      'n',
-      'o',
-      'p',
-      'q',
-      'r',
-      's',
-      't',
-      'u',
-      'v',
-      'w',
-      'x',
-      'y',
-      'z',
-    ];
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
     const nStr = ('' + n).split('');
 
