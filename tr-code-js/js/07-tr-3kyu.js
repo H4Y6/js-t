67a7491ccbb3b10ff8072debce86f2817b00798f
h4y6 <h4y6@i.ua>
2025-03-25 22:53:48 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7656,6 +7656,39 @@ roundIt(34.56) should return 35   */
   // console.log(replaceAll('erty ploh', 'r', 'o'));
 }
 {
+  /**  7 kyu  Digital cypher vol 2
+
+	Digital Cypher assigns to each letter of the alphabet unique number. For example:
+	
+	 a  b  c  d  e  f  g  h  i  j  k  l  m
+	 1  2  3  4  5  6  7  8  9 10 11 12 13
+	 n  o  p  q  r  s  t  u  v  w  x  y  z
+	14 15 16 17 18 19 20 21 22 23 24 25 26
+	Instead of letters in encrypted word we write the corresponding number, eg. The word scout:
+	
+	 s  c  o  u  t
+	19  3 15 21 20
+	Then we add to each obtained digit consecutive digits from the key. For example. In case of key equal to 1939 :
+	
+		 s  c  o  u  t
+		19  3 15 21 20
+	 + 1  9  3  9  1
+	 ---------------
+		20 12 18 30 21
+		
+		 m  a  s  t  e  r  p  i  e  c  e
+		13  1 19 20  5 18 16  9  5  3  5
+	+  1  9  3  9  1  9  3  9  1  9  3
+		--------------------------------
+		14 10 22 29  6 27 19 18  6  12 8
+	Task
+	Write a function that accepts an array of integers code and a key number. As the result, it should return string containg a decoded message from the code.
+	
+	Example
+	decode([ 20, 12, 18, 30, 21],1939);  ==> "scout"
+	decode([ 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8],1939);  ==>  "masterpiece"
+	 */
+
   //   function decode(code, n) {
   //     const alphabet = [
   //       'a',
@@ -7713,14 +7746,22 @@ roundIt(34.56) should return 35   */
   //       .join('');
   //   }
 
-  function decode(code, n) {
-    return code
+  // function decode(code, n) {
+  //   return code
+  //     .map(
+  //       (e, i) =>
+  //         'abcdefghijklmnopqrstuvwxyz'[e - ('' + n)[i % ('' + n).length] - 1],
+  //     )
+  //     .join('');
+  // }
+
+  const decode = (code, n) =>
+    code
       .map(
         (e, i) =>
           'abcdefghijklmnopqrstuvwxyz'[e - ('' + n)[i % ('' + n).length] - 1],
       )
       .join('');
-  }
 
-  console.log(decode([20, 12, 18, 30, 21], 1939));
+  // console.log(decode([20, 12, 18, 30, 21], 1939));
 }
