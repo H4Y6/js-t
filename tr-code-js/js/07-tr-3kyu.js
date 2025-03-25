759dc5bf07807d3cd4b7085cdcd80b94ca346a9e
h4y6 <h4y6@i.ua>
2025-03-25 22:29:30 +0200

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7655,3 +7655,42 @@ roundIt(34.56) should return 35   */
   // console.log(replaceAll([1, 2, 2], 1, 2));
   // console.log(replaceAll('erty ploh', 'r', 'o'));
 }
+{
+  function decode(code, n) {
+    const alphabet = [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z',
+    ];
+
+    const nStr = ('' + n).split('');
+
+    return code
+      .map((e, i, ar) => alphabet[e - nStr[i % nStr.length] - 1])
+      .join('');
+  }
+  console.log(decode([20, 12, 18, 30, 21], 1939));
+}
