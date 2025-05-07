98f34353101bdf1b8bfbcfefd4b58ef3476be7c0
h4y6 <h4y6@i.ua>
2025-05-07 22:05:45 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7919,3 +7919,31 @@ For example:
 
   // console.log(aspectRatio(640, 480));
 }
+
+{
+  /**  7 kyu  Possibilities Array
+A non-empty array a of length n is called an array of all possibilities if it contains all numbers between 0 and a.length - 1 (both inclusive).
+
+Write a function that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+Examples:
+
+[1,2,0,3] => True
+# Includes all numbers between 0 and a.length - 1 (4 - 1 = 3)
+
+[0,1,2,2,3] => False
+# Doesn't include all numbers between 0 and a.length - 1 (5 - 1 = 4)
+
+[0] => True
+# Includes all numbers between 0 and a.length - 1 (1 - 1 = 0).    */
+
+  function isAllPossibilities(x) {
+    return (
+      Math.min(...x) === 0 &&
+      Math.max(...x) === x.length - 1 &&
+      [...new Set(x)].length === x.length
+    );
+  }
+
+  console.log(isAllPossibilities([1, 2, 0, 3]));
+}
