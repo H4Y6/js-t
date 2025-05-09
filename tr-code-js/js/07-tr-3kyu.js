9b7d4023564786ec9754d6b5b0bfab849ae4e19e
h4y6 <h4y6@i.ua>
2025-05-09 17:40:48 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -7957,3 +7957,21 @@ Examples:
 
   // console.log(isAllPossibilities([1, 2, 0, 3]));
 }
+
+{
+  /**  7 kyu  Sort with a sorting array
+Sort an array according to the indices in another array.
+It is guaranteed that the two arrays have the same size, and that the sorting array has all the required indices.
+
+    sort(['x', 'y', 'z'], [1, 2, 0]) => ['z', 'x', 'y']
+    
+    sort(['z', 'x', 'y'], [0, 2, 1]) => ['z', 'y', 'x']    */
+
+  function sort(initialArray, sortingArray) {
+    const sortedArray = [];
+    sortingArray.map((e, i) => (sortedArray[e] = initialArray[i]));
+    return sortedArray;
+  }
+
+  console.log(sort(['x', 'y', 'z'], [1, 2, 0]));
+}
