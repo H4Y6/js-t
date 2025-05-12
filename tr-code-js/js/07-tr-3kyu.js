182c5fa29bba45de407af6f32e670124fd27ce15
h4y6 <h4y6@i.ua>
2025-05-12 17:24:25 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -8177,9 +8177,5 @@ all((1, 2, 3, 4, 5), less_than_9)    -> True   */
 
   const all = (arr, fun) => arr.every(fun);
 
-  console.log(
-    all([1, 2, 3, 4, 5], function (v) {
-      return v > 9;
-    }),
-  );
+  // console.log(all([1, 2, 3, 4, 5], function (v) { return v > 9}));
 }
