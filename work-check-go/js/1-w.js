8382f9a1a26a75e4b373b7d87fbe34cf4f161471
h4y6 <h4y6@i.ua>
2025-09-02 17:05:13 +0300

    modified:   work-check-go/js/1-w.js

--- work-check-go/js/1-w.js
+++ work-check-go/js/1-w.js
@@ -1202,5 +1202,147 @@ function checkForSpam(message) {
     return changed;
   }
 
-  console.log(changeEven([1, 2, 3, 4, 5], 10));
+  // console.log(changeEven([1, 2, 3, 4, 5], 10));
+
+  const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
+  const planetsLengths = planets.map(pl => pl.length);
+  // console.log(planetsLengths);
+
+  const books = [
+    {
+      title: 'The Last Kingdom',
+      author: 'Bernard Cornwell',
+      rating: 8.38,
+    },
+    {
+      title: 'Beside Still Waters',
+      author: 'Robert Sheckley',
+      rating: 8.51,
+    },
+    {
+      title: 'The Dream of a Ridiculous Man',
+      author: 'Fyodor Dostoevsky',
+      rating: 7.75,
+    },
+    { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+    { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+  ];
+
+  const titles = books.map(({ title }) => title);
+  const genres = books.flatMap(({ genres }) => genres);
+}
+{
+  const users = [
+    {
+      name: 'Moore Hensley',
+      email: 'moorehensley@indexia.com',
+      eyeColor: 'blue',
+      friends: ['Sharron Pace'],
+      isActive: false,
+      balance: 2811,
+      skills: ['ipsum', 'lorem'],
+      gender: 'male',
+      age: 37,
+    },
+    {
+      name: 'Sharlene Bush',
+      email: 'sharlenebush@tubesys.com',
+      eyeColor: 'blue',
+      friends: ['Briana Decker', 'Sharron Pace'],
+      isActive: true,
+      balance: 3821,
+      skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+      gender: 'female',
+      age: 34,
+    },
+    {
+      name: 'Ross Vazquez',
+      email: 'rossvazquez@xinware.com',
+      eyeColor: 'green',
+      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+      isActive: false,
+      balance: 3793,
+      skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+      gender: 'male',
+      age: 24,
+    },
+    {
+      name: 'Elma Head',
+      email: 'elmahead@omatom.com',
+      eyeColor: 'green',
+      friends: ['Goldie Gentry', 'Aisha Tran'],
+      isActive: true,
+      balance: 2278,
+      skills: ['adipisicing', 'irure', 'velit'],
+      gender: 'female',
+      age: 21,
+    },
+    {
+      name: 'Carey Barr',
+      email: 'careybarr@nurali.com',
+      eyeColor: 'blue',
+      friends: ['Jordan Sampson', 'Eddie Strong'],
+      isActive: true,
+      balance: 3951,
+      skills: ['ex', 'culpa', 'nostrud'],
+      gender: 'male',
+      age: 27,
+    },
+    {
+      name: 'Blackburn Dotson',
+      email: 'blackburndotson@furnigeer.com',
+      eyeColor: 'brown',
+      friends: ['Jacklyn Lucas', 'Linda Chapman'],
+      isActive: false,
+      balance: 1498,
+      skills: ['non', 'amet', 'ipsum'],
+      gender: 'male',
+      age: 38,
+    },
+    {
+      name: 'Sheree Anthony',
+      email: 'shereeanthony@kog.com',
+      eyeColor: 'brown',
+      friends: ['Goldie Gentry', 'Briana Decker'],
+      isActive: true,
+      balance: 2764,
+      skills: ['lorem', 'veniam', 'culpa'],
+      gender: 'female',
+      age: 39,
+    },
+  ];
+
+  const getUserNames = users => users.map(({ name }) => name);
+  const getUserEmails = users => users.map(({ email }) => email);
+
+  const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+  const evenNumbers = numbers.filter(num => !(num % 2));
+  const oddNumbers = numbers.filter(num => num % 2);
+
+  const books = [
+    {
+      title: 'The Last Kingdom',
+      author: 'Bernard Cornwell',
+      genres: ['adventure', 'history'],
+    },
+    {
+      title: 'Beside Still Waters',
+      author: 'Robert Sheckley',
+      genres: ['fiction', 'mysticism'],
+    },
+    {
+      title: 'Redder Than Blood',
+      author: 'Tanith Lee',
+      genres: ['horror', 'mysticism', 'adventure'],
+    },
+  ];
+  const allGenres = books.flatMap(({ genres }) => genres);
+  const uniqueGenres = allGenres.filter((el, i, arr) => i === arr.indexOf(el));
+
+  const MIN_RATING = 8;
+  const AUTHOR = 'Bernard Cornwell';
+
+  const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING);
+  const booksByAuthor = books.filter(({ author }) => author === AUTHOR);
 }
