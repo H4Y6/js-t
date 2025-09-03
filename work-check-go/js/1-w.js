4e8fca02ab35ace9c3cb51acebe2330adec6b0c3
h4y6 <h4y6@i.ua>
2025-09-03 11:09:15 +0300

    modified:   work-check-go/js/1-w.js

--- work-check-go/js/1-w.js
+++ work-check-go/js/1-w.js
@@ -1356,3 +1356,116 @@ function checkForSpam(message) {
   const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING);
   const booksByAuthor = books.filter(({ author }) => author === AUTHOR);
 }
+{
+  const users = [
+    {
+      name: 'Moore Hensley',
+      email: 'moorehensley@indexia.com',
+      eyeColor: 'blue',
+      friends: ['Sharron Pace'],
+      isActive: false,
+      balance: 2811,
+      gender: 'male',
+      age: 37,
+    },
+    {
+      name: 'Sharlene Bush',
+      email: 'sharlenebush@tubesys.com',
+      eyeColor: 'blue',
+      friends: ['Briana Decker', 'Sharron Pace'],
+      isActive: true,
+      balance: 3821,
+      gender: 'female',
+      age: 34,
+    },
+    {
+      name: 'Ross Vazquez',
+      email: 'rossvazquez@xinware.com',
+      eyeColor: 'green',
+      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+      isActive: false,
+      balance: 3793,
+      gender: 'male',
+      age: 24,
+    },
+    {
+      name: 'Elma Head',
+      email: 'elmahead@omatom.com',
+      eyeColor: 'green',
+      friends: ['Goldie Gentry', 'Aisha Tran'],
+      isActive: true,
+      balance: 2278,
+      gender: 'female',
+      age: 21,
+    },
+    {
+      name: 'Carey Barr',
+      email: 'careybarr@nurali.com',
+      eyeColor: 'blue',
+      friends: ['Jordan Sampson', 'Eddie Strong'],
+      isActive: true,
+      balance: 3951,
+      gender: 'male',
+      age: 27,
+    },
+    {
+      name: 'Blackburn Dotson',
+      email: 'blackburndotson@furnigeer.com',
+      eyeColor: 'brown',
+      friends: ['Jacklyn Lucas', 'Linda Chapman'],
+      isActive: false,
+      balance: 1498,
+      gender: 'male',
+      age: 38,
+    },
+    {
+      name: 'Sheree Anthony',
+      email: 'shereeanthony@kog.com',
+      eyeColor: 'brown',
+      friends: ['Goldie Gentry', 'Briana Decker'],
+      isActive: true,
+      balance: 2764,
+      gender: 'female',
+      age: 39,
+    },
+  ];
+
+  const getFriends = users =>
+    users
+      .flatMap(({ friends }) => friends)
+      .filter((fr, i, arr) => i === arr.indexOf(fr));
+  // console.log(getFriends(users));
+
+  const getActiveUsers = users => users.filter(({ isActive }) => isActive);
+  // console.log(getActiveUsers(users));
+
+  const getInactiveUsers = users => users.filter(({ isActive }) => !isActive);
+}
+{
+  const books = [
+    {
+      title: 'The Last Kingdom',
+      author: 'Bernard Cornwell',
+      rating: 8.38,
+    },
+    {
+      title: 'Beside Still Waters',
+      author: 'Robert Sheckley',
+      rating: 8.51,
+    },
+    {
+      title: 'The Dream of a Ridiculous Man',
+      author: 'Fyodor Dostoevsky',
+      rating: 7.75,
+    },
+    { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  ];
+  const BOOK_TITLE = 'The Dream of a Ridiculous Man';
+  const AUTHOR = 'Robert Sheckley';
+
+  const bookWithTitle = books.find(({ title }) => title === BOOK_TITLE);
+  const bookByAuthor = books.find(({ author }) => author === AUTHOR);
+
+  // console.log(bookWithTitle);
+  // console.log(bookByAuthor);
+}
