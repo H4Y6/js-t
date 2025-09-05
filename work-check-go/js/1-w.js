ad7a56ae251b80f10f56fd0d9545b038519ca614
h4y6 <h4y6@i.ua>
2025-09-05 16:30:50 +0300

    modified:   work-check-go/js/1-w.js

--- work-check-go/js/1-w.js
+++ work-check-go/js/1-w.js
@@ -1773,8 +1773,33 @@ function checkForSpam(message) {
   };
 
   customer.setDiscount(0.15);
-  console.log(customer.getDiscount()); // 0.15
+  // console.log(customer.getDiscount()); // 0.15
   customer.addOrder(5000, 'Steak');
-  console.log(customer.getBalance()); // 19750
-  console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+  // console.log(customer.getBalance()); // 19750
+  // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+  const historyService = {
+    orders: [
+      { email: 'jacob@hotmail.com', dish: 'Burrito' },
+      { email: 'solomon@topmail.net', dish: 'Burger' },
+      { email: 'artemis@coldmail.net', dish: 'Pizza' },
+      { email: 'solomon@topmail.net', dish: 'Apple pie' },
+      { email: 'jacob@hotmail.com', dish: 'Taco' },
+    ],
+
+    getOrdersLog() {
+      return this.orders
+        .map(order => `email: ${order.email} dish: ${order.dish}`)
+        .join(' - ');
+    },
+    getEmails() {
+      const emails = this.orders.map(order => order.email);
+      const uniqueEmails = new Set(emails);
+      return [...uniqueEmails];
+    },
+    getOrdersByEmail(email) {
+      return this.orders.filter(order => order.email === email);
+    },
+  };
+  // console.log(historyService.getOrdersLog());
 }
