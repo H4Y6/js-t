8b6c9bba8a39f722995137a5c1215413addb5a2e
h4y6 <h4y6@i.ua>
2025-09-08 22:14:41 +0300

    modified:   work-check-go/js/1-w.js

--- work-check-go/js/1-w.js
+++ work-check-go/js/1-w.js
@@ -1955,3 +1955,34 @@ function checkForSpam(message) {
     }
   }
 }
+{
+  class Car {
+    #brand;
+    #model;
+    #price;
+    constructor({ brand, model, price }) {
+      this.#brand = brand;
+      this.#model = model;
+      this.#price = price;
+    }
+    get brand() {
+      return this.#brand;
+    }
+    set brand(newBrand) {
+      this.#brand = newBrand;
+    }
+
+    get model() {
+      return this.#model;
+    }
+    set model(newModel) {
+      this.#model = newModel;
+    }
+    get price() {
+      return this.#price;
+    }
+    set price(newPrice) {
+      this.#price = newPrice;
+    }
+  }
+}
