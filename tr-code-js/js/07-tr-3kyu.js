4df5b89b356ceb99261cfb3a1b2d8d07c4f7e9c0
h4y6 <h4y6@i.ua>
2025-09-12 11:45:19 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -8224,3 +8224,61 @@ Do not use loops. Do not modify input array.
 
   // console.log(remove('Hi! Hi!! Hi!'));
 }
+{
+  /**  
+8 kyu   For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+This is a beginner friendly kata especially for UFC/MMA fans.
+
+It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden.
+ Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner
+  will make his legendary statement. It's your job to return the right statement depending on
+   the winner!
+
+If the winner is George Saint Pierre he will obviously say:
+
+    "I am not impressed by your performance."
+
+If the winner is Conor McGregor he will most undoubtedly say:
+
+    "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+
+The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". 
+Your solution should treat both as the same thing (case-insensitive).
+ */
+
+  // function quote(fighter) {
+  //   return fighter.includes('m') || fighter.includes('M')
+  //     ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  //     : 'I am not impressed by your performance.';
+  // }
+
+  // const quote = fighter =>
+  //   fighter.includes('m') || fighter.includes('M')
+  //     ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  //     : 'I am not impressed by your performance.';
+
+  //   const quote = fighter =>
+  //     fighter.toLowerCase().includes('m')
+  //       ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  //       : 'I am not impressed by your performance.';
+
+  // const quote = fighter =>
+  //   fighter.toLowerCase().match(/m/gi)
+  //     ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  //     : 'I am not impressed by your performance.';
+
+  // const quote = fighter =>
+  //   fighter.toLowerCase().match(/c/g)
+  //     ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  //     : 'I am not impressed by your performance.';
+
+  // const quote = fighter =>
+  //   /c/i.test(fighter)
+  //     ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  //     : 'I am not impressed by your performance.';
+
+  const quote = fighter =>
+    fighter.match(/c/i)
+      ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+      : 'I am not impressed by your performance.';
+}
