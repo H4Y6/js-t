b979da24e2a53db2167135f6c4630c384283cff9
h4y6 <h4y6@i.ua>
2025-09-13 12:38:44 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -8282,3 +8282,33 @@ Your solution should treat both as the same thing (case-insensitive).
       ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
       : 'I am not impressed by your performance.';
 }
+{
+  /**  
+8 kyu
+Add Length
+What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+Example(Input --> Output)
+
+"apple ban" --> ["apple 5", "ban 3"]
+"you will win" -->["you 3", "will 4", "win 3"]
+
+Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+Note: String will have at least one element; words will always be separated by a space.
+ */
+
+  // function addLength(str) {
+  //   return str.split(' ').map(e => e + ` ${e.length}`);
+  // }
+
+  // const addLength = str => str.split(' ').map(e => e + ` ${e.length}`);
+
+  // const addLength = str =>
+  //   str.split(' ').reduce((res, e) => [...res, `${e} ${e.length}`], []);
+
+  const addLength = str =>
+    str.split(' ').reduce((res, e) => [...res, e + ` ${e.length}`], []);
+
+  // console.log(addLength('you will win'));
+}
