14336a17da47ff51f0a0e72f671ab8148592a2a0
h4y6 <h4y6@i.ua>
2025-09-14 11:37:55 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -8444,3 +8444,34 @@ Notes:
   // console.log(mean('London', data));
   // console.log(variance('London', data));
 }
+{
+  /**  
+7 kyu
+Password validator
+Your job is to write a simple password validation function, as seen on many websites.
+The rules for a valid password are as follows:
+
+    There needs to be at least 1 uppercase letter.
+    There needs to be at least 1 lowercase letter.
+    There needs to be at least 1 number.
+    The password needs to be at least 8 characters long.
+
+Your function takes a string argument and returns whether it is a valid password, as a boolean.
+Examples:
+
+"Abcd1234" ===> true
+"Abcd123" ===> false
+"abcd1234" ===> false
+"AbcdefGhijKlmnopQRsTuvwxyZ1234567890" ===> true
+"ABCD1234" ===> false
+"Ab1!@#$%^&*()-_+={}[]|\:;?/>.<," ===> true;
+"!@#$%^&*()-_+={}[]|\:;?/>.<," ===> false; */
+
+  const password = str =>
+    /[A-Z]/g.test(str) &&
+    /[a-z]/g.test(str) &&
+    /\d/g.test(str) &&
+    str.length > 7;
+
+  console.log(password('Abcd1234'));
+}
