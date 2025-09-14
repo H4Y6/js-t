66e4b306f01ff9adb25fb21c1842b54cda826930
h4y6 <h4y6@i.ua>
2025-09-14 18:01:23 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -8486,3 +8486,31 @@ Examples:
 
   // console.log(password('Abcd1234'));
 }
+{
+  function animal(obj) {
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+  }
+
+  // console.log(animal({ name: 'rabbit', legs: 4, color: 'gray' }));
+
+  function howManydays(month) {
+    let days;
+    switch (month) {
+      case 2:
+        days = 28;
+        break;
+
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        days = 30;
+        break;
+      default:
+        days = 31;
+    }
+    return days;
+  }
+
+  // console.log(howManydays(11));
+}
