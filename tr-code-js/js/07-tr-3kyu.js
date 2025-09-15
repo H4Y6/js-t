23514ca1146df2a646b9f71b99bc28a29b265db0
h4y6 <h4y6@i.ua>
2025-09-15 19:32:00 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -8611,3 +8611,52 @@ You should use for..in in your code, otherwise your solution may not pass this k
 
   // console.log(explode([4, 'u']));
 }
+{
+  /**  
+7 kyu
+Sort the Vowels!
+In this kata, we want to sort the vowels in a special format.
+Task
+
+Write a function which takes a input string s and return a string in the following way:
+
+   
+                  C|                          R|
+                  |O                          n|
+                  D|                          d|
+   "CODEWARS" =>  |E       "Rnd Te5T"  =>      |
+                  W|                          T|
+                  |A                          |e
+                  R|                          5|
+                  S|                          T|
+
+Notes:
+
+    List all the Vowels on the right side of |
+    List every character except Vowels on the left side of |
+    for the purpose of this kata, the vowels are : a e i o u
+    Return every character in its original case
+    Each line is seperated with \n
+    Invalid input ( undefined / null / integer ) should return an empty string
+ */
+
+  // function sortVowels(s) {
+  //   return typeof s === 'string'
+  //     ? s
+  //         .split('')
+  //         .map(e => (e.match(/[aeoui]/i) ? '|' + e : e + '|'))
+  //         .join('\n')
+  //     : '';
+  // }
+
+  const sortVowels = s =>
+    typeof s === 'string'
+      ? s
+          .split('')
+          .map(e => (e.match(/[aeoui]/i) ? '|' + e : e + '|'))
+          .join('\n')
+      : '';
+
+  // console.log(sortVowels('Codewars'));
+  // console.log(sortVowels(333));
+}
