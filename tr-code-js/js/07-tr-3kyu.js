b4834523e893c4bf5970f6aec585e768ddf9fcb0
h4y6 <h4y6@i.ua>
2025-09-19 21:12:28 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -8869,3 +8869,29 @@ There is no 13:20, instead it is 01:20. */
 
   // console.log(WhatIsTheTime('11:59'));
 }
+
+{
+  /** 
+7 kyu
+Alphabet symmetry
+Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+
+See test cases for more examples.
+
+Input will consist of alphabet characters, both uppercase and lowercase. No spaces.  */
+
+  function solve(arr) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const count = 0;
+    return arr.map(
+      e =>
+        [...e.toLowerCase()].filter((e, i) => i === alphabet.indexOf(e)).length,
+    );
+  }
+
+  console.log(solve(['IAMDEFANDJKL', 'thedefgh', 'xyzDEFghijabc']));
+}
