8c5e0f54c732f72abfefcbd082f51c06f4ce8afd
h4y6 <h4y6@i.ua>
2025-09-20 16:01:00 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -8970,8 +8970,14 @@ Your 'search' function should return the stores that sell coffee within your bud
 The search function should return a string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order.
  */
 
+  // const search = (budget, prices) =>
+  //   prices.filter(p => p <= budget).sort((a, b) => a - b) + '';
+
   const search = (budget, prices) =>
-    prices.filter(p => p <= budget).sort((a, b) => a - b) + '';
+    prices
+      .filter(p => p <= budget)
+      .sort((a, b) => a - b)
+      .join(',');
 
   console.log(search(14, [7, 3, 23, 9, 14, 20, 7]));
 }
