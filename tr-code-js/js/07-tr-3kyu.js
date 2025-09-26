40023bb21ec3b9feea9ad12cd875d116400f8e36
h4y6 <h4y6@i.ua>
2025-09-26 22:07:12 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -9649,7 +9649,9 @@ Examples
 
   //  const collinearity=(x1, y1, x2, y2)=> (!x1 && !x2) || (!y1 && !y2) || (!x1 && !y1) || (!x2 && !y2) || (x1 / x2 === y1 / y2) ? true : false;
 
-  const collinearity = (x1, y1, x2, y2) => x1 * y2 === y1 * x2;
+  // const collinearity = (x1, y1, x2, y2) => x1 * y2 === y1 * x2;
+
+  const collinearity = (x1, y1, x2, y2) => !(x1 * y2 - y1 * x2);
 
   console.log(collinearity(4, 0, 11, 0));
 }
