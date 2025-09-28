0f45b39f2567b0f0792f64563091d3c8f7c0e263
h4y6 <h4y6@i.ua>
2025-09-28 14:22:38 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -9700,10 +9700,16 @@ Output:
   //     .flatMap(e => Object.values(e))
   //     .join(' ');
 
+  // const sentence = arrayOfObjects =>
+  //   arrayOfObjects
+  //     .sort((a, b) => Object.keys(a) - Object.keys(b))
+  //     .map(e => Object.values(e))
+  //     .join(' ');
+
   const sentence = arrayOfObjects =>
     arrayOfObjects
-      .sort((a, b) => Object.keys(a) - Object.keys(b))
-      .map(e => Object.values(e))
+      .sort((a, b) => Object.entries(a)[0][0] - Object.entries(b)[0][0])
+      .flatMap(e => Object.values(e))
       .join(' ');
 
   // console.log(sentence([{ 4: 'dog' }, { 2: 'took' }, { 3: 'his' }, { '-2': 'Vatsan' }, { 5: 'for' },{ 6: 'a' }, { 12: 'spin' }, ]), );
