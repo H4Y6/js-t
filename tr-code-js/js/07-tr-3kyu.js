3bfb1430647ef85bc95e5dc58f1c1f70fc06f71a
h4y6 <h4y6@i.ua>
2025-09-29 15:34:39 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -9777,9 +9777,13 @@ Write a function calculating distance between Point a and Point b.
 Input coordinates fit in range −50⩽x,y⩽50 -50 \leqslant x,y \leqslant 50 −50⩽x,y⩽50. Tests compare expected result and actual answer with tolerance of 1e-6.
  */
 
-  function distanceBetweenPoints(a, b) {
-    return Math.round(((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5 * 1e6) / 1e6;
-  }
+  // function distanceBetweenPoints(a, b) {
+  //   return Math.round(((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5 * 1e6) / 1e6;
+  // }
+
+  const distanceBetweenPoints = (a, b) =>
+    Math.round(((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5 * 1e6) / 1e6;
+
   console.log(
     distanceBetweenPoints({ x: -10.2, y: 12.5 }, { x: 0.3, y: 14.7 }),
   );
