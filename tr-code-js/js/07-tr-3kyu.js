5115bd58cac27c969dbecf0c9e9a9ad196d8cfa8
h4y6 <h4y6@i.ua>
2025-09-30 23:59:26 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -9969,3 +9969,19 @@ firstToLast("ababc","d") should return -1     */
 
   // console.log(firstToLast('ababc', 'd'));
 }
+{
+  /** Task
+
+Coding in function countAnimals. function accept two parameters: animals, a string contains some animals; count, an array list of which animals we should count.
+ The result should be a number array.
+ Examples
+countAnimals("dog,cat",["dog","cat"]); //=> [1,1]
+countAnimals("dog,cat",["dog","cat","pig"]); //=> [1,1,0]
+countAnimals("dog,dog,cat",["dog","cat"]); //=> [2,1]
+countAnimals("dog,dog,cat",["pig","cow"]); //=> [0,0]           */
+
+  const countAnimals = (animals, count) =>
+    count.map(e => (animals.match(new RegExp(e), 'g') ?? []).length);
+
+  console.log(countAnimals('dog,cat', ['dog', 'cat', 'pig']));
+}
