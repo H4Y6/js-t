e508af799bab2d34e5d5b4baa47b276a99d4fafb
h4y6 <h4y6@i.ua>
2025-10-02 00:22:34 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -10035,5 +10035,43 @@ Using string template and ternary operator can make your work easier. */
         : 'fake'
     } array`;
 
-  console.log(blackAndWhite([8, 5, 13, 9]));
+  // console.log(blackAndWhite([8, 5, 13, 9]));
+}
+
+{
+  /** 7 kyu   Training JS #30: methods of arrayObject---reduce() and reduceRight() 
+   * Task
+
+Coding in function tailAndHead. function accept 1 parameter arr(a number array).
+
+We use an example to explain this task:
+
+You got an array [123,456,789,12,34,56,78].
+
+First, from left to right, the tail of an element and the head of the next element are added together. The results are put into an array. like this:
+
+[123,456,789,1 2,3 4,5 6,78]
+   3+4 6+7 9+1 2+3 4+5 6+7  
+    |   |   |   |   |   |
+   [7 ,13 ,10 , 5 , 9 ,13]
+
+And then, Calculate their product and return it(7x13x10x5x9x13=532350)
+
+So, tailAndHead([123,456,789,12,34,56,78]) should return 532350 
+
+All elements of arr are positive integer. If a number is less than 10, its head and tail are the same. Please try to use reduce() solve this kata.
+Examples
+
+tailAndHead([1,2,3,4,5]) should return 945
+tailAndHead([111,2345,66,78,900]) should return 7293
+tailAndHead([35456,782,569,2454,875]) should return 12012          */
+
+  function tailAndHead(arr) {
+    const ar = arr.map((e, i) => [('' + e)[0], ('' + e)[('' + e).length - 1]]);
+    return ar
+      .slice(0, -1)
+      .map((e, i) => eval([e[1], ar.slice(1)[i][0]].join('+')))
+      .reduce((pr, e) => pr * e);
+  }
+  console.log(tailAndHead([123, 456, 789, 12, 34, 56, 78]));
 }
