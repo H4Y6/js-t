caeb24c4f3ad6cde6670906f236d4b985542a96d
h4y6 <h4y6@i.ua>
2025-10-01 22:53:13 +0300

    modified:   tr-code-js/js/07-tr-3kyu.js

--- tr-code-js/js/07-tr-3kyu.js
+++ tr-code-js/js/07-tr-3kyu.js
@@ -9983,5 +9983,41 @@ countAnimals("dog,dog,cat",["pig","cow"]); //=> [0,0]           */
   const countAnimals = (animals, count) =>
     count.map(e => (animals.match(new RegExp(e), 'g') ?? []).length);
 
-  console.log(countAnimals('dog,cat', ['dog', 'cat', 'pig']));
+  // console.log(countAnimals('dog,cat', ['dog', 'cat', 'pig']));
+}
+
+{
+  /**  Task
+
+Coding in function blackAndWhite. function accept 1 parameter arr(a number array).
+
+If arr is not an array, function should return:
+
+"It's a fake array"
+
+If arr contains elements 5 and 13, function should return:
+
+"It's a black array"
+
+If arr contains neither 5 nor 13, function should return:
+
+"It's a white array"
+
+Examples
+
+blackAndWhite(5,13) should return "It's a fake array"
+blackAndWhite([5,13]) should return "It's a black array"
+blackAndWhite([5,12]) should return "It's a white array" 
+
+Using string template and ternary operator can make your work easier. */
+
+  function blackAndWhite(arr) {
+    return Array.isArray(arr)
+      ? arr.indexOf(5) < 0 || arr.indexOf(13) < 0
+        ? "It's a white array"
+        : "It's a black array"
+      : "It's a fake array";
+  }
+
+  console.log(blackAndWhite([8, 5, 13, 9]));
 }
